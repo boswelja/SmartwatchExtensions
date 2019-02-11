eef4d6fca5fcbc4043dea4ad9d6041403e68bde1
Jack Boswell <boswelja@outlook.com>
2019-02-11 14:50:56 +1300

    Fix layer-list scaling on Wear OS complications * Also revert colour changes

--- wear/src/main/java/com/boswelja/devicemanager/complications/PhoneBatteryComplicationProvider.kt
+++ wear/src/main/java/com/boswelja/devicemanager/complications/PhoneBatteryComplicationProvider.kt
@@ -9,6 +9,8 @@ package com.boswelja.devicemanager.complications
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.drawable.Icon
 import android.os.Build
 import android.preference.PreferenceManager
@@ -47,11 +49,11 @@ class PhoneBatteryComplicationProvider : ComplicationProviderService() {
         }
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val percent = prefs.getInt(References.BATTERY_PERCENT_KEY, 0)
+        val percent = prefs.getInt(References.BATTERY_PERCENT_KEY, -1)
         val text = if (percent > -1) String.format(getString(R.string.phone_battery_percent), percent) else getString(R.string.phone_battery_unknown_short)
         val data = ComplicationData.Builder(type)
                 .setShortText(ComplicationText.plainText(text))
-                .setIcon(Icon.createWithResource(this, CommonUtils.getPhoneBatteryIndicator(percent)))
+                .setIcon(createIcon(percent))
                 .setTapAction(pendingIntent)
         if (type == ComplicationData.TYPE_RANGED_VALUE) {
             data.setMaxValue(1.0f)
@@ -59,5 +61,14 @@ class PhoneBatteryComplicationProvider : ComplicationProviderService() {
                     .setValue((percent.toFloat()) / 100)
         }
         return data.build()
+    }
+
+    private fun createIcon(percent: Int) : Icon {
+        val drawable = getDrawable(CommonUtils.getPhoneBatteryIndicator(percent))!!
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return Icon.createWithBitmap(bitmap)
     }
 }
