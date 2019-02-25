50d56f349e2ad03c794c3b9f71479c307b41e55f
Jack Boswell <boswelja@outlook.com>
2019-02-25 17:39:56 +1300

    Delete useless method * Also make sure we're using anxdroidx core-ktx

--- common/src/main/java/com/boswelja/devicemanager/common/Compat.kt
+++ common/src/main/java/com/boswelja/devicemanager/common/Compat.kt
@@ -48,12 +48,4 @@ object Compat {
             }
         }
     }
-
-    fun htmlFormat(text: String) : Spanned {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
-        } else {
-            Html.fromHtml(text)
-        }
-    }
 }
