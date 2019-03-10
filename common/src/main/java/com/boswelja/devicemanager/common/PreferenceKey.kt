496ae795648b753c5f99c993297df524d53b0d56
Jack Boswell <boswelja@outlook.com>
2019-03-11 00:22:38 +1300

    Reorganise PreferenceKey.kt

--- common/src/main/java/com/boswelja/devicemanager/common/PreferenceKey.kt
+++ common/src/main/java/com/boswelja/devicemanager/common/PreferenceKey.kt
@@ -10,19 +10,25 @@ package com.boswelja.devicemanager.common
 object PreferenceKey {
 
     const val HIDE_APP_ICON_KEY = "hide_app_icon"
+    const val NOTIFICATION_SETTINGS_KEY = "notification_settings"
+    const val BATTERY_OPT_KEY = "battery_optimisation"
+    const val DAYNIGHT_SWITCH_KEY = "daynight_switch"
+
+    const val LOCK_PHONE_ENABLED = "lock_phone_enabled"
+
     const val BATTERY_SYNC_ENABLED_KEY = "battery_sync_enabled"
     const val BATTERY_SYNC_INTERVAL_KEY = "battery_sync_interval"
     const val BATTERY_SYNC_NOW_KEY = "battery_sync_now"
     const val BATTERY_PHONE_FULL_CHARGE_NOTI_KEY = "battery_phone_full_charge"
     const val BATTERY_FULL_CHARGE_NOTI_KEY = "battery_full_charge"
+
+    const val BATTERY_CHARGED_NOTI_SENT = "battery_charged_notified"
+    const val BATTERY_PERCENT_KEY = "battery_percent"
+
     const val DND_SYNC_SEND_KEY = "dnd_sync_send"
     const val DND_SYNC_RECEIVE_KEY = "dnd_sync_receive"
-    const val LOCK_PHONE_ENABLED = "lock_phone_enabled"
-    const val NOTIFICATION_SETTINGS_KEY = "notification_settings"
-    const val BATTERY_OPT_KEY = "battery_optimisation"
-    const val DAYNIGHT_SWITCH_KEY = "daynight_switch"
+
     const val DND_ACCESS_STATUS_KEY = "dnd_access"
-    const val BATTERY_CHARGED_NOTI_SENT = "battery_charged_notified"
+
     const val DONATE_KEY = "pref_donate"
-    const val BATTERY_PERCENT_KEY = "battery_percent"
 }
