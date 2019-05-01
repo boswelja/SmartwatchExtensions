3767bd74813927b3d77ba43e5eba120b37469659
Jack Boswell <boswelja@outlook.com>
2019-05-01 14:13:29 +1200

    Auto-bullet changelog * Makes it easier to add changes

--- mobile/src/main/java/com/boswelja/devicemanager/ui/version/ChangelogDialogFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/version/ChangelogDialogFragment.kt
@@ -8,10 +8,24 @@ import com.boswelja.devicemanager.ui.base.BaseDialogFragment
 
 class ChangelogDialogFragment : BaseDialogFragment() {
 
+    private lateinit var changelog: Array<CharSequence>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val rawChangelog = context?.resources?.getStringArray(R.array.version_changelog)!!
+        val bullet = context?.getString(R.string.bullet)!!
+        val processedChangelog = ArrayList<CharSequence>()
+        for (change in rawChangelog) {
+            processedChangelog.add("$bullet $change")
+        }
+        changelog = processedChangelog.toTypedArray()
+
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(context!!)
                 .setPositiveButton(R.string.dialog_button_close) { dialog, _ -> dialog.dismiss() }
-                .setItems(R.array.version_changelog, null)
+                .setItems(changelog, null)
                 .setTitle(R.string.dialog_changelog_title)
                 .create()
     }
