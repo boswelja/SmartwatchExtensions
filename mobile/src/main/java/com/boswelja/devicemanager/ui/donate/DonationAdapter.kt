dbfee980254a999c9ff6472337bf2d48e8636296
Jack Boswell <boswelja@outlook.com>
2019-05-11 16:07:33 +1200

    Update DonationDialog styling * Fix Cancel button padding * Show SKU title and price

--- mobile/src/main/java/com/boswelja/devicemanager/ui/donate/DonationAdapter.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/donate/DonationAdapter.kt
@@ -10,30 +10,35 @@ package com.boswelja.devicemanager.ui.donate
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.billingclient.api.SkuDetails
 import com.boswelja.devicemanager.R
 
-class DonationAdapter(private val skus: List<SkuDetails>, private val dialogFragment: DonationDialogFragment) : RecyclerView.Adapter<DonationAdapter.ViewHolder>() {
+class DonationAdapter(private val skus: List<SkuDetails>, private val dialogFragment: DonationDialogFragment) : RecyclerView.Adapter<DonationAdapter.DonationViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DonationViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_single_line_text, parent, false)
-        return ViewHolder(view)
+        val view = inflater.inflate(R.layout.dialog_donations_item, parent, false)
+        return DonationViewHolder(view)
     }
 
     override fun getItemCount(): Int = skus.count()
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: DonationViewHolder, position: Int) {
         val sku = skus[position]
-        holder.text.text = sku.price
+        var title = sku.title
+        val stringEnd = title.indexOf("(")
+        title = title.substring(0, stringEnd).trim()
+        holder.donationName.text = title
+        holder.donationAmount.text = sku.price
         holder.itemView.setOnClickListener {
             dialogFragment.launchBillingFlow(sku)
         }
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val text: TextView = itemView.findViewById(R.id.text)
+    inner class DonationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val donationName: AppCompatTextView = itemView.findViewById(R.id.donation_name)
+        val donationAmount: AppCompatTextView = itemView.findViewById(R.id.donation_amount)
     }
 }
