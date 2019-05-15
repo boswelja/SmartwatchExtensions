5115f48736160befef9934125406a71df19fbdf2
Jack Boswell <boswelja@outlook.com>
2019-05-15 22:07:32 +1200

    Spotless

--- mobile/src/main/java/com/boswelja/devicemanager/ui/appmanager/AppManagerFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/appmanager/AppManagerFragment.kt
@@ -1,3 +1,10 @@
+/* Copyright (C) 2019 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
 package com.boswelja.devicemanager.ui.appmanager
 
 import android.os.Bundle
@@ -34,7 +41,6 @@ class AppManagerFragment : Fragment() {
                 }
             }
             AppManagerReferences.GET_ALL_PACKAGES -> {
-
             }
         }
     }
