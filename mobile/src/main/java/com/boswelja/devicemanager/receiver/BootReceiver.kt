fce3f1194df5f11ac7f181f82969f041f60efe50
Jack Boswell <boswelja@outlook.com>
2019-05-25 22:13:46 +1200

    Ensure BatteryUpdateJob is started on boot

--- mobile/src/main/java/com/boswelja/devicemanager/receiver/BootReceiver.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/receiver/BootReceiver.kt
@@ -9,19 +9,22 @@ package com.boswelja.devicemanager.receiver
 
 import android.content.Context
 import android.content.Intent
+import com.boswelja.devicemanager.BatteryUpdateJob
 import com.boswelja.devicemanager.common.BaseBootReceiver
 import com.boswelja.devicemanager.common.Compat
+import com.boswelja.devicemanager.common.PreferenceKey.BATTERY_SYNC_ENABLED_KEY
 import com.boswelja.devicemanager.common.PreferenceKey.INTERRUPT_FILTER_SYNC_TO_PHONE_KEY
 import com.boswelja.devicemanager.service.InterruptFilterLocalChangeListener
 
 class BootReceiver : BaseBootReceiver() {
 
     override fun onBootCompleted(context: Context?) {
-        if (isInterruptFilterSyncSending()) startInterruptFilterSyncService(context)
-    }
-
-    private fun isInterruptFilterSyncSending(): Boolean {
-        return sharedPreferences.getBoolean(INTERRUPT_FILTER_SYNC_TO_PHONE_KEY, false)
+        if (sharedPreferences.getBoolean(INTERRUPT_FILTER_SYNC_TO_PHONE_KEY, false)) {
+            startInterruptFilterSyncService(context)
+        }
+        if (sharedPreferences.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)) {
+            BatteryUpdateJob.startJob(context!!)
+        }
     }
 
     private fun startInterruptFilterSyncService(context: Context?) {
