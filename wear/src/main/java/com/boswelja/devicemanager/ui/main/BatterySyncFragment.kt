373652815f229204471009347709b7d323229044
Jack Boswell (boswelja) <boswelja@outlook.com>
2019-06-04 14:58:02 +1200

    Compact phone battery info when it's disabled * Animate adding and removing fragments from the controls view

--- wear/src/main/java/com/boswelja/devicemanager/ui/main/BatterySyncFragment.kt
+++ wear/src/main/java/com/boswelja/devicemanager/ui/main/BatterySyncFragment.kt
@@ -14,7 +14,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import android.widget.RelativeLayout
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import com.boswelja.devicemanager.R
@@ -27,30 +26,36 @@ import com.boswelja.devicemanager.ui.base.BaseSharedPreferenceFragment
 class BatterySyncFragment : BaseSharedPreferenceFragment() {
 
     private val batteryInfoChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
-        when (key) {
-            BATTERY_SYNC_ENABLED_KEY,
-            BATTERY_PERCENT_KEY -> {
-                updatePhoneBatteryInfo()
-            }
+        if (key == BATTERY_PERCENT_KEY) {
+            updatePhoneBatteryInfo()
         }
     }
 
-    private lateinit var phoneBatteryProgressBar: ProgressBar
-    private lateinit var phoneBatteryIndicatorIconView: AppCompatImageView
-    private lateinit var phoneBatteryIndicatorTextView: AppCompatTextView
-    private lateinit var phoneBatteryTapToRefresh: AppCompatTextView
+    private var phoneBatteryProgressBar: ProgressBar? = null
+    private var phoneBatteryIndicatorIconView: AppCompatImageView? = null
+    private var phoneBatteryIndicatorTextView: AppCompatTextView? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?  {
+        return if (sharedPreferences.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)) {
             inflater.inflate(R.layout.fragment_battery_sync, container, false)
+        } else {
+            inflater.inflate(R.layout.fragment_battery_sync_disabled, container, false)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        phoneBatteryProgressBar = view.findViewById(R.id.phone_battery_progress_bar)
-        phoneBatteryIndicatorIconView = view.findViewById(R.id.phone_battery_indicator_icon)
-        phoneBatteryIndicatorTextView = view.findViewById(R.id.phone_battery_indicator_text_view)
-        phoneBatteryTapToRefresh = view.findViewById(R.id.phone_battery_indicator_tap_to_refresh_label)
-        view.findViewById<RelativeLayout>(R.id.phone_battery_view).setOnClickListener {
+        if (sharedPreferences.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)) {
+            phoneBatteryProgressBar = view.findViewById(R.id.phone_battery_progress_bar)
+            phoneBatteryIndicatorIconView = view.findViewById(R.id.phone_battery_indicator_icon)
+            phoneBatteryIndicatorTextView = view.findViewById(R.id.phone_battery_indicator_text_view)
+        } else {
+            phoneBatteryProgressBar = null
+            phoneBatteryIndicatorIconView = null
+            phoneBatteryIndicatorTextView = null
+        }
+        view.setOnClickListener {
             val intent = Intent(context, ActionService::class.java)
             intent.putExtra(ActionService.EXTRA_ACTION, References.REQUEST_BATTERY_UPDATE_PATH)
             context?.startService(intent)
@@ -72,15 +77,9 @@ class BatterySyncFragment : BaseSharedPreferenceFragment() {
         val batterySyncEnabled = sharedPreferences.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)
         if (batterySyncEnabled) {
             val batteryPercent = sharedPreferences.getInt(BATTERY_PERCENT_KEY, 0)
-            phoneBatteryProgressBar.progress = batteryPercent
-            phoneBatteryIndicatorIconView.drawable.level = batteryPercent
-            phoneBatteryIndicatorTextView.text = getString(R.string.phone_battery_percent).format(batteryPercent)
-            phoneBatteryTapToRefresh.visibility = View.VISIBLE
-        } else {
-            phoneBatteryProgressBar.progress = 0
-            phoneBatteryIndicatorIconView.drawable.level = 0
-            phoneBatteryIndicatorTextView.text = getString(R.string.battery_sync_disabled)
-            phoneBatteryTapToRefresh.visibility = View.GONE
+            phoneBatteryProgressBar?.progress = batteryPercent
+            phoneBatteryIndicatorIconView?.drawable?.level = batteryPercent
+            phoneBatteryIndicatorTextView?.text = getString(R.string.phone_battery_percent).format(batteryPercent)
         }
     }
 }
--- wear/src/main/java/com/boswelja/devicemanager/ui/main/MainFragment.kt
+++ wear/src/main/java/com/boswelja/devicemanager/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@
  */
 package com.boswelja.devicemanager.ui.main
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,38 +21,82 @@ import com.boswelja.devicemanager.ui.base.BaseSharedPreferenceFragment
 
 class MainFragment : BaseSharedPreferenceFragment() {
 
+    private var batterySyncFragment: BatterySyncFragment? = null
+    private var lockPhoneFragment: LockPhoneFragment? = null
+
+    private val preferenceChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == PreferenceKey.BATTERY_SYNC_ENABLED_KEY) {
+            recreateView()
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_main, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recreateView()
+    }
 
-        if (!sharedPreferences.getBoolean(PreferenceKey.BATTERY_SYNC_ENABLED_KEY, false)) {
-            addFragment(LockPhoneFragment())
-            addFragment(BatterySyncFragment())
-            padContentTop()
-        } else {
-            addFragment(BatterySyncFragment())
-            addFragment(LockPhoneFragment())
-            padContentBottom()
-        }
+    override fun onResume() {
+        super.onResume()
+        sharedPreferences.registerOnSharedPreferenceChangeListener(preferenceChangeListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(preferenceChangeListener)
     }
 
     private fun addFragment(fragment: Fragment) {
         fragmentManager?.beginTransaction()!!
+                .setCustomAnimations(R.anim.slide_in_from_left, R.anim.slide_out_to_right)
                 .add(R.id.content, fragment)
                 .commit()
     }
 
-    private fun padContentTop() {
+    private fun padContentTop(clearOtherPadding: Boolean) {
+        val paddingDp =  Utils.complexTypeDp(resources, 72.0f).toInt()
         view?.findViewById<LinearLayout>(R.id.content)!!.apply {
-            setPadding(0, Utils.complexTypeDp(resources, 72.0f).toInt(), 0, 0)
+            if (clearOtherPadding) {
+                setPadding(0, paddingDp, 0, 0)
+            } else {
+                setPadding(paddingLeft, paddingDp, paddingRight, paddingBottom)
+            }
         }
     }
 
-    private fun padContentBottom() {
+    private fun padContentBottom(clearOtherPadding: Boolean) {
+        val paddingDp =  Utils.complexTypeDp(resources, 72.0f).toInt()
         view?.findViewById<LinearLayout>(R.id.content)!!.apply {
-            setPadding(0, 0, 0, Utils.complexTypeDp(resources, 72.0f).toInt())
+            if (clearOtherPadding) {
+                setPadding(0, 0, 0, paddingDp)
+            } else {
+                setPadding(paddingLeft, paddingTop, paddingRight, paddingDp)
+            }
+        }
+    }
+
+    private fun recreateView() {
+        if (batterySyncFragment != null &&
+                lockPhoneFragment != null) {
+            fragmentManager?.beginTransaction()!!
+                    .setCustomAnimations(R.anim.slide_in_from_left, R.anim.slide_out_to_right)
+                    .remove(batterySyncFragment!!)
+                    .remove(lockPhoneFragment!!)
+                    .commit()
+        }
+        batterySyncFragment = BatterySyncFragment()
+        lockPhoneFragment = LockPhoneFragment()
+        if (!sharedPreferences.getBoolean(PreferenceKey.BATTERY_SYNC_ENABLED_KEY, false)) {
+            padContentTop(false)
+            padContentBottom(false)
+            addFragment(lockPhoneFragment!!)
+            addFragment(batterySyncFragment!!)
+        } else {
+            padContentBottom(true)
+            addFragment(batterySyncFragment!!)
+            addFragment(lockPhoneFragment!!)
         }
     }
 }
