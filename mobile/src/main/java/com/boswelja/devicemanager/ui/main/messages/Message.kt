eb4fc3a03829894d8b3f999855b3e82a265bac4a
Jack Boswell <boswelja@outlook.com>
2019-06-10 17:28:49 +1200

    SpotlessApply

--- mobile/src/main/java/com/boswelja/devicemanager/ui/main/messages/Message.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/main/messages/Message.kt
@@ -1,3 +1,10 @@
+/* Copyright (C) 2019 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
 package com.boswelja.devicemanager.ui.main.messages
 
 import com.boswelja.devicemanager.R
