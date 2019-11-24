2d40fa02d1bee61fc820de0e47a10382df70988d
Jack Boswell (boswelja) <boswelja@outlook.com>
2019-11-25 00:07:35 +1300

    SpotlessApply

--- mobile/src/main/java/com/boswelja/devicemanager/watchconnectionmanager/WatchDao.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/watchconnectionmanager/WatchDao.kt
@@ -8,10 +8,8 @@
 package com.boswelja.devicemanager.watchconnectionmanager
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 
 @Dao
 interface WatchDao {
