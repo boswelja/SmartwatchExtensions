cd50ebb330009006995043addccddedfa91025da
Jack Boswell (boswelja) <boswelja@outlook.com>
2019-12-05 22:31:17 +1300

    Add connected field to Watch.kt

--- mobile/src/main/java/com/boswelja/devicemanager/watchconnectionmanager/Watch.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/watchconnectionmanager/Watch.kt
@@ -20,12 +20,17 @@ data class Watch(
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "battery_sync_job_id") val batterySyncJobId: Int,
     @Ignore val hasApp: Boolean,
+    @Ignore val connected: Boolean,
     @Ignore val intPrefs: HashMap<String, Int>,
     @Ignore val boolPrefs: HashMap<String, Boolean>
 ) {
+    constructor (id: String, name: String, batterySyncJobId: Int, hasApp: Boolean, connected: Boolean) : this(id, name, batterySyncJobId, hasApp, connected, HashMap(), HashMap())
+
     constructor (id: String, name: String, batterySyncJobId: Int, hasApp: Boolean) : this(id, name, batterySyncJobId, hasApp, HashMap(), HashMap())
 
     constructor (id: String, name: String, batterySyncJobId: Int) : this(id, name, batterySyncJobId, false, HashMap(), HashMap())
+
+    constructor(node: Node, hasApp: Boolean, connected: Boolean) : this(node.id, node.displayName, Random.nextInt(100000, 999999), hasApp)
 
     constructor(node: Node, hasApp: Boolean) : this(node.id, node.displayName, Random.nextInt(100000, 999999), hasApp)
 
