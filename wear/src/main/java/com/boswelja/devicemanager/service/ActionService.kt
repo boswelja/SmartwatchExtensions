3f544563f503b089a6d02d0655633a26ff10552e
Jack Boswell (boswelja) <boswelja@outlook.com>
2019-12-31 21:26:18 +1300

    Update ActionService.kt to not rely on CapabilityClient Also improve verbosity of ConfirmationActivity messages

--- wear/src/main/java/com/boswelja/devicemanager/service/ActionService.kt
+++ wear/src/main/java/com/boswelja/devicemanager/service/ActionService.kt
@@ -14,62 +14,65 @@ import android.support.wearable.complications.ProviderUpdateRequester
 import androidx.preference.PreferenceManager
 import com.boswelja.devicemanager.ConfirmationActivityHandler
 import com.boswelja.devicemanager.R
-import com.boswelja.devicemanager.Utils
 import com.boswelja.devicemanager.common.PreferenceKey
 import com.boswelja.devicemanager.common.PreferenceKey.BATTERY_SYNC_ENABLED_KEY
 import com.boswelja.devicemanager.common.References.LOCK_PHONE_PATH
 import com.boswelja.devicemanager.common.batterysync.References.REQUEST_BATTERY_UPDATE_PATH
 import com.boswelja.devicemanager.complication.PhoneBatteryComplicationProvider
-import com.google.android.gms.wearable.Node
+import com.boswelja.devicemanager.phoneconnectionmanager.References.PHONE_ID_KEY
+import com.google.android.gms.wearable.MessageClient
 import com.google.android.gms.wearable.Wearable
 
 class ActionService : IntentService("ActionService") {
 
-    private lateinit var action: String
+    private lateinit var messageClient: MessageClient
 
-    override fun onHandleIntent(intent: Intent?) {
-        action = intent?.getStringExtra(EXTRA_ACTION)!!
-
-        Utils.getCompanionNode(this)
-                .addOnCompleteListener {
-                    if (it.isSuccessful && !it.result?.nodes.isNullOrEmpty()) {
-                        val node = it.result?.nodes?.last()
-                        val prefs = PreferenceManager.getDefaultSharedPreferences(this@ActionService)
-                        when (action) {
-                            LOCK_PHONE_PATH -> {
-                                if (prefs.getBoolean(PreferenceKey.PHONE_LOCKING_ENABLED_KEY, false)) {
-                                    sendMessage(node)
-                                } else {
-                                    ConfirmationActivityHandler.failAnimation(this@ActionService, getString(R.string.lock_phone_disabled_message))
-                                }
-                                val providerUpdateRequester = ProviderUpdateRequester(this@ActionService, ComponentName(packageName, PhoneBatteryComplicationProvider::class.java.name))
-                                providerUpdateRequester.requestUpdateAll()
-                            }
-                            REQUEST_BATTERY_UPDATE_PATH -> {
-                                if (prefs.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)) {
-                                    sendMessage(node)
-                                } else {
-                                    ConfirmationActivityHandler.failAnimation(this@ActionService, getString(R.string.battery_sync_disabled))
-                                }
-                            }
-                        }
-                    } else {
-                        when (action) {
-                            LOCK_PHONE_PATH -> ConfirmationActivityHandler.failAnimation(this@ActionService, getString(R.string.lock_phone_failed_message))
-                            REQUEST_BATTERY_UPDATE_PATH -> ConfirmationActivityHandler.failAnimation(this@ActionService, getString(R.string.phone_battery_update_failed))
-                        }
-                    }
-                }
+    override fun onCreate() {
+        super.onCreate()
+        messageClient = Wearable.getMessageClient(this)
     }
 
-    private fun sendMessage(node: Node?) {
-        Wearable.getMessageClient(this)
-                .sendMessage(node!!.id, action, null)
+    override fun onHandleIntent(intent: Intent?) {
+        val phoneId = PreferenceManager.getDefaultSharedPreferences(this).getString(PHONE_ID_KEY, "")
+        val action = intent?.getStringExtra(EXTRA_ACTION)!!
+
+        if (!phoneId.isNullOrEmpty()) {
+            val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+            when (action) {
+                LOCK_PHONE_PATH -> {
+                    if (prefs.getBoolean(PreferenceKey.PHONE_LOCKING_ENABLED_KEY, false)) {
+                        sendMessage(phoneId, action, getString(R.string.lock_phone_success_message), getString(R.string.lock_phone_failed_message))
+                    } else {
+                        ConfirmationActivityHandler.failAnimation(this, getString(R.string.lock_phone_disabled_message))
+                    }
+                    val providerUpdateRequester = ProviderUpdateRequester(this, ComponentName(packageName, PhoneBatteryComplicationProvider::class.java.name))
+                    providerUpdateRequester.requestUpdateAll()
+                }
+                REQUEST_BATTERY_UPDATE_PATH -> {
+                    if (prefs.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)) {
+                        sendMessage(phoneId, action, getString(R.string.battery_sync_force_sync_success), getString(R.string.battery_sync_force_sync_fail))
+                    } else {
+                        ConfirmationActivityHandler.failAnimation(this, getString(R.string.battery_sync_disabled))
+                    }
+                }
+            }
+        } else {
+            when (action) {
+                LOCK_PHONE_PATH -> ConfirmationActivityHandler.failAnimation(this, getString(R.string.lock_phone_failed_message))
+                REQUEST_BATTERY_UPDATE_PATH -> ConfirmationActivityHandler.failAnimation(this, getString(R.string.phone_battery_update_failed))
+            }
+        }
+    }
+
+    private fun sendMessage(nodeId: String, action: String, successMessage: String, failMessage: String) {
+        messageClient
+                .sendMessage(nodeId, action, null)
                 .addOnSuccessListener {
-                    ConfirmationActivityHandler.successAnimation(this, getString(R.string.request_success_message))
+                    ConfirmationActivityHandler.successAnimation(this, successMessage)
                 }
                 .addOnFailureListener {
-                    ConfirmationActivityHandler.failAnimation(this, getString(R.string.request_failed_message))
+                    ConfirmationActivityHandler.failAnimation(this, failMessage)
                 }
     }
 
