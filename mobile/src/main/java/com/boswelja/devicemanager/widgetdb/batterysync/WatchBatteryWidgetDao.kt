33f2a8f1a84a45849955a318fab1ebf323882508
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-01-24 22:53:57 +1300

    Allow WatchBatteryWidgetDao.kt findWidgetById to return a nullable result

--- mobile/src/main/java/com/boswelja/devicemanager/widgetdb/batterysync/WatchBatteryWidgetDao.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/widgetdb/batterysync/WatchBatteryWidgetDao.kt
@@ -21,7 +21,7 @@ interface WatchBatteryWidgetDao {
     fun getAllForWatch(watchId: String): List<WatchBatteryWidgetId>
 
     @Query("SELECT * FROM watch_battery_widget_ids WHERE widgetId = :widgetId LIMIT 1")
-    fun findByWidgetId(widgetId: Int): WatchBatteryWidgetId
+    fun findByWidgetId(widgetId: Int): WatchBatteryWidgetId?
 
     @Insert
     fun addWidget(watchBatteryWidgetId: WatchBatteryWidgetId)
