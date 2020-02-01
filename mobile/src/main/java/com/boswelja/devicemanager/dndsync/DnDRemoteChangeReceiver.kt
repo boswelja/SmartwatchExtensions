cf6fca4529c16a82271effca2a1c7bd3d44325d8
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-02-01 23:13:46 +1300

    Significantly simplify DnDRemoteChangeReceiver.kt Should prevent a crash caused by a DataEventBuffer being closed by the time we wanted to access it

--- mobile/src/main/java/com/boswelja/devicemanager/dndsync/DnDRemoteChangeReceiver.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/dndsync/DnDRemoteChangeReceiver.kt
@@ -7,75 +7,23 @@
  */
 package com.boswelja.devicemanager.dndsync
 
-import android.util.Log
-import com.boswelja.devicemanager.Utils
-import com.boswelja.devicemanager.common.PreferenceKey
+import com.boswelja.devicemanager.Utils.setInterruptionFilter
 import com.boswelja.devicemanager.common.References
-import com.boswelja.devicemanager.watchconnectionmanager.WatchConnectionService
 import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.DataMapItem
 import com.google.android.gms.wearable.WearableListenerService
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 
 class DnDRemoteChangeReceiver : WearableListenerService() {
 
-    private val watchConnectionManagerConnection = object : WatchConnectionService.Connection() {
-        override fun onWatchManagerBound(service: WatchConnectionService) {
-            watchConnectionManager = service
-        }
-
-        override fun onWatchManagerUnbound() {
-            watchConnectionManager = null
-        }
-    }
-
-    private var watchConnectionManager: WatchConnectionService? = null
-
-    override fun onCreate() {
-        super.onCreate()
-        WatchConnectionService.bind(this, watchConnectionManagerConnection)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unbindService(watchConnectionManagerConnection)
-    }
-
     override fun onDataChanged(dataEventBuffer: DataEventBuffer) {
-        super.onDataChanged(dataEventBuffer)
-        if (watchConnectionManager != null) {
-            MainScope().launch {
-                val dndSyncReceiveEnabledPrefs = watchConnectionManager!!.getBoolPrefsForRegisteredWatches(PreferenceKey.DND_SYNC_TO_PHONE_KEY)
-
-                if (!dndSyncReceiveEnabledPrefs.isNullOrEmpty()) {
-                    val dndStatePerWatch = HashMap<String, Boolean>()
-                    for (dataEvent in dataEventBuffer) {
-                        val senderId = dataEvent.dataItem.uri.host!!
-                        val isDnDSyncReceiveEnabledForWatch = dndSyncReceiveEnabledPrefs.firstOrNull { it.watchId == senderId }?.value
-                        if (isDnDSyncReceiveEnabledForWatch == true) {
-                            val dataMap = DataMapItem.fromDataItem(dataEvent.dataItem).dataMap
-                            val dndEnabled = dataMap.getBoolean(NEW_DND_STATE_KEY)
-                            dndStatePerWatch[senderId] = dndEnabled
-                        }
-                    }
-                    if (dndStatePerWatch.isNotEmpty()) {
-                        var shouldEnableDnD = false
-                        for (watchState in dndStatePerWatch) {
-                            if (watchState.value) {
-                                shouldEnableDnD = true
-                                break
-                            }
-                        }
-                        Utils.setInterruptionFilter(this@DnDRemoteChangeReceiver, shouldEnableDnD)
-                    }
-                } else {
-                    Log.e("DnDRemoteChangeReceiver", "dndSyncReceiveEnabledPrefs.isNullOrEmpty()")
-                }
-            }
-        } else {
-            Log.e("DnDRemoteChangeReceiver", "watchConnectionManager == null")
+        val watchDndStates = ArrayList<Boolean>()
+        for (dataEvent in dataEventBuffer) {
+            val dataMap = DataMapItem.fromDataItem(dataEvent.dataItem).dataMap
+            val dndEnabled = dataMap.getBoolean(NEW_DND_STATE_KEY)
+            watchDndStates.add(dndEnabled)
         }
+        val shouldEnableDnD = watchDndStates.any { dndEnabled -> dndEnabled }
+        setInterruptionFilter(this, shouldEnableDnD)
     }
 
     companion object {
