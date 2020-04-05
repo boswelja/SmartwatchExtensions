32a6f24a8e87630b46d3fcf3448036d20619b8a2
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-04-06 01:59:24 +1200

    Cut back line length a bit

--- mobile/src/main/java/com/boswelja/devicemanager/ui/main/messages/MessagesAdapter.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/main/messages/MessagesAdapter.kt
@@ -113,7 +113,8 @@ internal class MessagesAdapter(private val fragment: MessageFragment) :
         }
     }
 
-    class SwipeDismissCallback(private val adapter: MessagesAdapter, context: Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+    class SwipeDismissCallback(private val adapter: MessagesAdapter, context: Context) :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
 
         private val alphaMultiplier: Int = 3
 
@@ -122,25 +123,36 @@ internal class MessagesAdapter(private val fragment: MessageFragment) :
         private val iconMargin = Utils.complexTypeDp(context.resources, 16f).toInt()
         private val iconMaxAlpha = icon.alpha
 
-        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean = false
+        override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder): Boolean = false
 
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
             val position = viewHolder.adapterPosition
             adapter.dismissMessage(position)
         }
 
-        override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+        override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float, dY: Float,
+                actionState: Int, isCurrentlyActive: Boolean) {
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
             when {
                 dX > 0 -> {
                     val itemView = viewHolder.itemView
                     if (dX > icon.intrinsicWidth + iconMargin) {
-                        val iconTop = itemView.top + (itemView.height - icon.intrinsicHeight) / 2
+                        val iconTop =
+                                itemView.top + (itemView.height - icon.intrinsicHeight) / 2
                         val iconBottom = iconTop + icon.intrinsicHeight
                         val iconLeft = itemView.left + iconMargin
                         val iconRight = itemView.left + iconMargin + icon.intrinsicWidth
                         icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
-                        icon.alpha = min(((dX - icon.intrinsicWidth - iconMargin) * alphaMultiplier).toInt(), iconMaxAlpha)
+                        icon.alpha = min(
+                                ((dX - icon.intrinsicWidth - iconMargin) * alphaMultiplier).toInt(),
+                                iconMaxAlpha)
                         icon.draw(c)
                     }
 
@@ -157,7 +169,9 @@ internal class MessagesAdapter(private val fragment: MessageFragment) :
                         val iconLeft = itemView.right - iconMargin - icon.intrinsicWidth
                         val iconRight = itemView.right - iconMargin
                         icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
-                        icon.alpha = min(((dX + icon.intrinsicWidth + iconMargin) * alphaMultiplier * -1).toInt(), iconMaxAlpha)
+                        icon.alpha = min(
+                                ((dX + icon.intrinsicWidth + iconMargin) * alphaMultiplier * -1).toInt(),
+                                iconMaxAlpha)
                         icon.draw(c)
                     }
 
