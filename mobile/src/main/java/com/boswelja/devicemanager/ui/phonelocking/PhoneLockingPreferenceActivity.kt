5073200650daae8eb676171e04376f961da08416
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-04-10 15:30:33 +1200

    SpotlessApply

--- mobile/src/main/java/com/boswelja/devicemanager/ui/phonelocking/PhoneLockingPreferenceActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/phonelocking/PhoneLockingPreferenceActivity.kt
@@ -1,3 +1,10 @@
+/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
 package com.boswelja.devicemanager.ui.phonelocking
 
 import androidx.fragment.app.Fragment
@@ -9,5 +16,4 @@ class PhoneLockingPreferenceActivity : BasePreferenceActivity() {
     override fun createWidgetFragment(): Fragment? = null
     override fun createPreferenceFragment(): BasePreferenceFragment =
             PhoneLockingPreferenceFragment()
-
 }
