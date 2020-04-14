9ad7b7246417cc9b112b05f157409545b1a48f2b
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-04-14 19:18:19 +1200

    Implement a custom crash handler Show an activity with 3 actions; restart, report, and close

--- mobile/src/main/java/com/boswelja/devicemanager/MainApplication.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/MainApplication.kt
@@ -7,16 +7,102 @@
  */
 package com.boswelja.devicemanager
 
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
+import android.os.Bundle
+import android.os.Process
+import com.boswelja.devicemanager.crashhandler.CrashHandlerActivity
+import com.boswelja.devicemanager.crashhandler.CrashHandlerActivity.Companion.EXTRA_STACKTRACE
 import timber.log.Timber
+import java.io.PrintWriter
+import java.io.StringWriter
+import kotlin.system.exitProcess
 
 class MainApplication : Application() {
+
+    private var defaultUncaughtExceptionHandler: Thread.UncaughtExceptionHandler? = null
+
+    private var isInForeground = true
 
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            private var startedActivities = 0
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                // Do nothing
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+                startedActivities++
+                updateIsInBackground()
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                // Do nothing
+            }
+            override fun onActivityPaused(activity: Activity) {
+                // Do nothing
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                startedActivities--
+                updateIsInBackground()
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+                // Do nothing
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                // Do nothing
+            }
+
+            private fun updateIsInBackground() {
+                isInForeground = startedActivities > 0
+            }
+        })
+
+        defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
+            Timber.i("Uncaught exception occurred")
+            if (isInForeground) {
+                var stacktrace = ""
+                StringWriter().use { sw ->
+                    PrintWriter(sw).use { pw ->
+                        exception.printStackTrace(pw)
+                    }
+                    stacktrace = sw.toString()
+                }
+                if (stacktrace.length > 131071) {
+                    val truncatedText = "..."
+                    stacktrace = stacktrace.substring(0, 131071 - truncatedText.length) + truncatedText
+                }
+
+                Intent(this, CrashHandlerActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    putExtra(EXTRA_STACKTRACE, stacktrace)
+                }.also {
+                    Timber.i("Starting CrashHandlerActivity")
+                    startActivity(it)
+                }
+            }
+
+            Process.killProcess(Process.myPid())
+            exitProcess(10)
+
+//            if (defaultUncaughtExceptionHandler != null) {
+//                defaultUncaughtExceptionHandler!!.uncaughtException(thread, exception)
+//            } else {
+//                exitProcess(2)
+//            }
         }
     }
 }
---
+++ mobile/src/main/java/com/boswelja/devicemanager/crashhandler/CrashHandlerActivity.kt
@@ -0,0 +1,69 @@
+package com.boswelja.devicemanager.crashhandler
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import androidx.databinding.DataBindingUtil
+import com.boswelja.devicemanager.BuildConfig
+import com.boswelja.devicemanager.R
+import com.boswelja.devicemanager.databinding.ActivityCrashHandlerBinding
+import com.boswelja.devicemanager.ui.main.MainActivity
+import timber.log.Timber
+
+class CrashHandlerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCrashHandlerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i("onCreate")
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_crash_handler)
+
+        val stacktrace = intent.getStringExtra(EXTRA_STACKTRACE)
+
+        binding.apply {
+            restartButton.setOnClickListener {
+                Timber.i("Restarting app")
+                Intent(this@CrashHandlerActivity, MainActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }.also {
+                    startActivity(it)
+                }
+            }
+
+            reportButton.setOnClickListener {
+                val emailBody = "App Version: ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})\n" +
+                        "System API Level: ${Build.VERSION.SDK_INT}\n" +
+                        "Device: ${Build.MANUFACTURER} ${Build.MODEL} (${Build.HARDWARE})\n\n" +
+                        "Stacktrace:\n${stacktrace}\n\n" +
+                        "Steps to reproduce (if applicable):\n\n"
+                Intent(Intent.ACTION_SENDTO).apply {
+                    type ="text/plain"
+                    data = "mailto:".toUri()
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("boswelja78@gmail.com"))
+                    putExtra(Intent.EXTRA_SUBJECT, "Wearable Extensions crash")
+                    putExtra(Intent.EXTRA_TEXT, emailBody)
+                }.also {
+                    if (it.resolveActivity(packageManager) != null) {
+                        Timber.i("Launching email program to report error")
+                        startActivity(it)
+                    } else {
+                        Timber.w("No email client found")
+                    }
+                }
+            }
+
+            closeButton.setOnClickListener {
+                Timber.i("Closing")
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        const val EXTRA_STACKTRACE = "extra_stacktrace"
+    }
+}
