ebfddff6c75c02ec587cad193bf8771c06f4f97f
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-04-28 22:23:50 +1200

    Create a test for view visibility for BatterySyncPreferenceWidgetFragment.kt Also created some custom ViewActions to help with testing views that are not at their max size / visible by default

---
+++ mobile/src/androidTest/java/com/boswelja/devicemanager/TestExtensions.kt
@@ -0,0 +1,45 @@
+package com.boswelja.devicemanager
+
+import android.view.View
+import android.widget.TextView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+
+
+object TestExtensions {
+    fun setText(value: String?): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return allOf(isDisplayed(), isAssignableFrom(TextView::class.java))
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                (view as TextView).text = value
+            }
+
+            override fun getDescription(): String {
+                return "Replace text in TextView"
+            }
+        }
+    }
+
+    fun setVisibility(visibility: Int): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(View::class.java)
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                view.visibility = visibility
+            }
+
+            override fun getDescription(): String {
+                return "Set view visibility"
+            }
+        }
+    }
+}
---
+++ mobile/src/androidTest/java/com/boswelja/devicemanager/ui/batterysync/BatterySyncPreferenceWidgetFragmentTest.kt
@@ -0,0 +1,37 @@
+package com.boswelja.devicemanager.ui.batterysync
+
+import android.view.View
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.boswelja.devicemanager.R
+import com.boswelja.devicemanager.TestExtensions.setText
+import com.boswelja.devicemanager.TestExtensions.setVisibility
+import org.junit.Test
+
+class BatterySyncPreferenceWidgetFragmentTest {
+
+    private fun createScenario(): FragmentScenario<BatterySyncPreferenceWidgetFragment> {
+        return launchFragmentInContainer(themeResId = R.style.AppTheme)
+    }
+
+    @Test
+    fun testViewVisibility() {
+        createScenario()
+        // Set all views to their largest size and make them visible.
+        onView(withId(R.id.watch_battery_percent))
+                .perform(setVisibility(View.VISIBLE))
+                .perform(setText("Battery Sync Disabled"))
+                .check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.watch_battery_indicator))
+                .perform(setVisibility(View.VISIBLE))
+                .check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.last_updated_time))
+                .perform(setVisibility(View.VISIBLE))
+                .perform(setText("Last updated less than a minute ago"))
+                .check(matches(isCompletelyDisplayed()))
+    }
+}
