0d23b862c0a7679b6a51810f0ee8547398344475
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-04-29 21:49:58 +1200

    Remove CrashHandlerActivity.kt Crash logs weren't being redirected to the Google Play Console automatically.

--- mobile/src/main/java/com/boswelja/devicemanager/MainApplication.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/MainApplication.kt
@@ -7,27 +7,14 @@
  */
 package com.boswelja.devicemanager
 
-import android.app.Activity
 import android.app.Application
 import android.content.Intent
-import android.os.Bundle
-import android.os.Process
 import com.boswelja.devicemanager.bootorupdate.BootOrUpdateHandlerService
 import com.boswelja.devicemanager.bootorupdate.updater.Updater
 import com.boswelja.devicemanager.common.Compat
-import com.boswelja.devicemanager.crashhandler.CrashHandlerActivity
-import com.boswelja.devicemanager.crashhandler.CrashHandlerActivity.Companion.EXTRA_STACKTRACE
-import java.io.PrintWriter
-import java.io.StringWriter
-import java.lang.ref.WeakReference
-import kotlin.system.exitProcess
 import timber.log.Timber
 
 class MainApplication : Application() {
-
-    private var defaultUncaughtExceptionHandler: Thread.UncaughtExceptionHandler? = null
-    private var isInForeground = true
-    private var lastActivityCreated: WeakReference<Activity>? = null
 
     override fun onCreate() {
         super.onCreate()
@@ -36,7 +23,6 @@ class MainApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
 
-        setupCrashhandler()
         ensureUpdated()
     }
 
@@ -55,88 +41,4 @@ class MainApplication : Application() {
         }
     }
 
-    /**
-     * Set up our [CrashHandlerActivity].
-     */
-    private fun setupCrashhandler() {
-        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
-            private var startedActivities = 0
-
-            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                if (activity !is CrashHandlerActivity) {
-                    lastActivityCreated = WeakReference(activity)
-                }
-            }
-
-            override fun onActivityStarted(activity: Activity) {
-                startedActivities++
-                updateIsInBackground()
-            }
-
-            override fun onActivityResumed(activity: Activity) {
-                // Do nothing
-            }
-            override fun onActivityPaused(activity: Activity) {
-                // Do nothing
-            }
-
-            override fun onActivityStopped(activity: Activity) {
-                startedActivities--
-                updateIsInBackground()
-            }
-
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-                // Do nothing
-            }
-
-            override fun onActivityDestroyed(activity: Activity) {
-                // Do nothing
-            }
-
-            private fun updateIsInBackground() {
-                isInForeground = startedActivities > 0
-            }
-        })
-
-        defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
-
-        Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
-            Timber.i("Uncaught exception occurred")
-            if (isInForeground) {
-                var stacktrace = ""
-                StringWriter().use { sw ->
-                    PrintWriter(sw).use { pw ->
-                        exception.printStackTrace(pw)
-                    }
-                    stacktrace = sw.toString()
-                }
-                if (stacktrace.length > 131071) {
-                    val truncatedText = "..."
-                    stacktrace = stacktrace.substring(0, 131071 - truncatedText.length) + truncatedText
-                }
-
-                Intent(this, CrashHandlerActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    putExtra(EXTRA_STACKTRACE, stacktrace)
-                }.also {
-                    Timber.i("Starting CrashHandlerActivity")
-                    startActivity(it)
-                }
-            } else {
-                defaultUncaughtExceptionHandler!!.uncaughtException(thread, exception)
-            }
-
-            lastActivityCreated?.get()?.finish()
-            lastActivityCreated?.clear()
-
-            killCurrentProcess()
-        }
-    }
-
-    companion object {
-        fun killCurrentProcess() {
-            Process.killProcess(Process.myPid())
-            exitProcess(10)
-        }
-    }
 }
