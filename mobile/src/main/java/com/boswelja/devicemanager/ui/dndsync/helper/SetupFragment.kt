3c14a37e94438d1d65dcded4cc5dc60bffb2b2af
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-03 16:43:35 +1200

    SpotlessApply

--- mobile/src/main/java/com/boswelja/devicemanager/ui/dndsync/helper/SetupFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/dndsync/helper/SetupFragment.kt
@@ -13,7 +13,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
