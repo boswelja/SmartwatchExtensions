9d2311ae3eede0e49e98e6f1128c3ab75f5f5b4a
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-03 16:40:47 +1200

    Simplify PreferenceChangeReceiver.kt a bit. Also catch an IllegalArgumentException that seems to be happening on DataMapItem.fromDataItem

--- mobile/src/main/java/com/boswelja/devicemanager/receiver/PreferenceChangeReceiver.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/receiver/PreferenceChangeReceiver.kt
@@ -8,9 +8,6 @@
 package com.boswelja.devicemanager.receiver
 
 import android.content.Intent
-import android.content.SharedPreferences
-import androidx.core.content.edit
-import androidx.preference.PreferenceManager
 import com.boswelja.devicemanager.batterysync.BatterySyncWorker
 import com.boswelja.devicemanager.common.Compat
 import com.boswelja.devicemanager.common.PreferenceKey
@@ -23,6 +20,7 @@ import com.google.android.gms.wearable.WearableListenerService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 class PreferenceChangeReceiver : WearableListenerService() {
@@ -30,7 +28,6 @@ class PreferenceChangeReceiver : WearableListenerService() {
     private val coroutineScope = MainScope()
 
     private var watchConnectionManager: WatchManager? = null
-    private var sharedPreferences: SharedPreferences? = null
 
     private val watchConnManConnection = object : WatchManager.Connection() {
         override fun onWatchManagerBound(watchManager: WatchManager) {
@@ -59,82 +56,38 @@ class PreferenceChangeReceiver : WearableListenerService() {
         Timber.i("onDataChanged() called")
         if (dataEvents != null) {
             Timber.i("Handling preference change")
-            val selectedWatchId = watchConnectionManager?.connectedWatch?.id
             for (event in dataEvents) {
                 val senderId = event.dataItem.uri.host!!
-                if (!selectedWatchId.isNullOrEmpty() && senderId == selectedWatchId) {
-                    handleSelectedWatchPreferenceChange(event)
-                } else {
-                    handleOtherWatchPreferenceChange(senderId, event)
-                }
+                handleWatchPreferenceChange(senderId, event)
             }
         }
     }
 
     /**
-     * Handle a preference change for the watch that's currently selected in [WatchManager].
-     * @param key The key of the changed preference.
-     * @param newValue The new value of the preference.
+     * Starts or stops the [BatterySyncWorker] as needed.
+     * @param batterySyncEnabled Whether battery sync is enabled.
+     * @param watchId The watch ID to start/stop the worker for.
      */
-    private fun onPreferenceChanged(key: String, newValue: Any) {
-        Timber.i("onPreferenceChanged($key, $newValue) called")
-        when (key) {
-            PreferenceKey.BATTERY_SYNC_ENABLED_KEY -> {
-                coroutineScope.launch(Dispatchers.IO) {
-                    if (newValue == true) {
-                        Timber.i("Starting BatterySyncWorker")
-                        BatterySyncWorker.startWorker(watchConnectionManager)
-                    } else {
-                        Timber.i("Stopping BatterySyncWorker")
-                        BatterySyncWorker.stopWorker(watchConnectionManager)
-                    }
-                }
+    private suspend fun updateBatterySyncWorker(batterySyncEnabled: Boolean, watchId: String) {
+        withContext(Dispatchers.IO) {
+            if (batterySyncEnabled) {
+                Timber.i("Starting BatterySyncWorker")
+                BatterySyncWorker.startWorker(watchConnectionManager, watchId)
+            } else {
+                Timber.i("Stopping BatterySyncWorker")
+                BatterySyncWorker.stopWorker(watchConnectionManager, watchId)
             }
-            PreferenceKey.DND_SYNC_TO_WATCH_KEY -> {
-                if (newValue == true) {
-                    Intent(this, DnDLocalChangeService::class.java).also {
-                        Timber.i("Starting DnDLocalChangeService")
-                        Compat.startForegroundService(this, it)
-                    }
-                }
-            }
-        }
-        coroutineScope.launch(Dispatchers.IO) {
-            Timber.i("Updating preference in database")
-            watchConnectionManager?.updatePreferenceInDatabase(key, newValue)
         }
     }
 
     /**
-     * Determine which preferences have changed from a given [DataEvent] and handle them for the
-     * currently selected watch.
-     * @param event The [DataEvent] that holds a change in preferences.
+     * Starts the [DnDLocalChangeService] if needed.
      */
-    private fun handleSelectedWatchPreferenceChange(event: DataEvent) {
-        val dataMap = DataMapItem.fromDataItem(event.dataItem).dataMap
-        if (!dataMap.isEmpty) {
-            if (sharedPreferences == null) sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-            sharedPreferences!!.edit {
-                for (key in dataMap.keySet()) {
-                    when (key) {
-                        PreferenceKey.PHONE_LOCKING_ENABLED_KEY,
-                        PreferenceKey.BATTERY_SYNC_ENABLED_KEY,
-                        PreferenceKey.BATTERY_PHONE_CHARGE_NOTI_KEY,
-                        PreferenceKey.BATTERY_WATCH_CHARGE_NOTI_KEY,
-                        PreferenceKey.DND_SYNC_TO_WATCH_KEY,
-                        PreferenceKey.DND_SYNC_TO_PHONE_KEY,
-                        PreferenceKey.DND_SYNC_WITH_THEATER_KEY -> {
-                            val newValue = dataMap.getBoolean(key)
-                            putBoolean(key, newValue)
-                            onPreferenceChanged(key, newValue)
-                        }
-                        PreferenceKey.BATTERY_CHARGE_THRESHOLD_KEY -> {
-                            val newValue = dataMap.getInt(key)
-                            putInt(key, newValue)
-                            onPreferenceChanged(key, newValue)
-                        }
-                    }
-                }
+    private fun updateDnDSyncToWatch(dndSyncEnabled: Boolean) {
+        if (dndSyncEnabled) {
+            Intent(this, DnDLocalChangeService::class.java).also {
+                Timber.i("Starting DnDLocalChangeService")
+                Compat.startForegroundService(this, it)
             }
         }
     }
@@ -143,28 +96,41 @@ class PreferenceChangeReceiver : WearableListenerService() {
      * Determine which preferences have changed from a given [DataEvent] and handle them.
      * @param event The [DataEvent] that holds a change in preferences.
      */
-    private fun handleOtherWatchPreferenceChange(senderId: String, event: DataEvent) {
+    private fun handleWatchPreferenceChange(senderId: String, event: DataEvent) {
         coroutineScope.launch(Dispatchers.IO) {
-            val dataMap = DataMapItem.fromDataItem(event.dataItem).dataMap
-            if (!dataMap.isEmpty) {
-                for (key in dataMap.keySet()) {
-                    when (key) {
-                        PreferenceKey.PHONE_LOCKING_ENABLED_KEY,
-                        PreferenceKey.BATTERY_SYNC_ENABLED_KEY,
-                        PreferenceKey.BATTERY_PHONE_CHARGE_NOTI_KEY,
-                        PreferenceKey.BATTERY_WATCH_CHARGE_NOTI_KEY,
-                        PreferenceKey.DND_SYNC_TO_WATCH_KEY,
-                        PreferenceKey.DND_SYNC_TO_PHONE_KEY,
-                        PreferenceKey.DND_SYNC_WITH_THEATER_KEY -> {
-                            val newValue = dataMap.getBoolean(key)
-                            watchConnectionManager?.updatePreferenceInDatabase(senderId, key, newValue)
-                        }
-                        PreferenceKey.BATTERY_CHARGE_THRESHOLD_KEY -> {
-                            val newValue = dataMap.getInt(key)
-                            watchConnectionManager?.updatePreferenceInDatabase(senderId, key, newValue)
+            try {
+                val dataMap = DataMapItem.fromDataItem(event.dataItem).dataMap
+                if (!dataMap.isEmpty) {
+                    for (key in dataMap.keySet()) {
+                        when (key) {
+                            PreferenceKey.PHONE_LOCKING_ENABLED_KEY,
+                            PreferenceKey.BATTERY_PHONE_CHARGE_NOTI_KEY,
+                            PreferenceKey.BATTERY_WATCH_CHARGE_NOTI_KEY,
+                            PreferenceKey.DND_SYNC_TO_PHONE_KEY,
+                            PreferenceKey.DND_SYNC_WITH_THEATER_KEY
+                            -> {
+                                val newValue = dataMap.getBoolean(key)
+                                watchConnectionManager?.updatePreferenceInDatabase(senderId, key, newValue)
+                            }
+                            PreferenceKey.DND_SYNC_TO_WATCH_KEY -> {
+                                val newValue = dataMap.getBoolean(key)
+                                watchConnectionManager?.updatePreferenceInDatabase(senderId, key, newValue)
+                                updateDnDSyncToWatch(newValue)
+                            }
+                            PreferenceKey.BATTERY_SYNC_ENABLED_KEY -> {
+                                val newValue = dataMap.getBoolean(key)
+                                watchConnectionManager?.updatePreferenceInDatabase(senderId, key, newValue)
+                                updateBatterySyncWorker(newValue, senderId)
+                            }
+                            PreferenceKey.BATTERY_CHARGE_THRESHOLD_KEY -> {
+                                val newValue = dataMap.getInt(key)
+                                watchConnectionManager?.updatePreferenceInDatabase(senderId, key, newValue)
+                            }
                         }
                     }
                 }
+            } catch (e: IllegalArgumentException) {
+                Timber.e(e)
             }
         }
     }
