df84c14b1c1d13389bd60831b025a03ad07bb011
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-07 16:17:20 +1200

    SpotlessApply

--- mobile/src/main/java/com/boswelja/devicemanager/ui/widget/WidgetSettingsWidget.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/widget/WidgetSettingsWidget.kt
@@ -1,3 +1,10 @@
+/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
 package com.boswelja.devicemanager.ui.widget
 
 import android.app.WallpaperManager
