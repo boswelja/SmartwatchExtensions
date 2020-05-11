80feb7724c4d228a823958cf3086ee6c25208a1c
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-11 18:57:58 +1200

    Move ConfirmationActivityHandler.kt to ui package in wear module

--- wear/src/main/java/com/boswelja/devicemanager/ui/ConfirmationActivityHandler.kt
+++ wear/src/main/java/com/boswelja/devicemanager/ui/ConfirmationActivityHandler.kt
@@ -5,7 +5,7 @@
  * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
  * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
  */
-package com.boswelja.devicemanager
+package com.boswelja.devicemanager.ui
 
 import android.content.Context
 import android.content.Intent
