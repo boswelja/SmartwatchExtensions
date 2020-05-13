c12bd3082d0b3d125b281356b4e3c7f2e7363af8
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-13 17:11:45 +1200

    Clean up ActionService.kt a bit Remove some calls to ConfirmationActivityHandler.kt Removed an unnecessary update to phone battery complications

--- wear/src/main/java/com/boswelja/devicemanager/service/ActionService.kt
+++ wear/src/main/java/com/boswelja/devicemanager/service/ActionService.kt
@@ -8,16 +8,13 @@
 package com.boswelja.devicemanager.service
 
 import android.app.IntentService
-import android.content.ComponentName
 import android.content.Intent
-import android.support.wearable.complications.ProviderUpdateRequester
 import androidx.preference.PreferenceManager
 import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.common.PreferenceKey
 import com.boswelja.devicemanager.common.PreferenceKey.BATTERY_SYNC_ENABLED_KEY
 import com.boswelja.devicemanager.common.References.LOCK_PHONE_PATH
 import com.boswelja.devicemanager.common.batterysync.References.REQUEST_BATTERY_UPDATE_PATH
-import com.boswelja.devicemanager.complication.PhoneBatteryComplicationProvider
 import com.boswelja.devicemanager.phoneconnectionmanager.References.PHONE_ID_KEY
 import com.boswelja.devicemanager.ui.ConfirmationActivityHandler
 import com.google.android.gms.wearable.MessageClient
@@ -33,7 +30,8 @@ class ActionService : IntentService("ActionService") {
     }
 
     override fun onHandleIntent(intent: Intent?) {
-        val phoneId = PreferenceManager.getDefaultSharedPreferences(this).getString(PHONE_ID_KEY, "")
+        val phoneId = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(PHONE_ID_KEY, "")
         val action = intent?.getStringExtra(EXTRA_ACTION)!!
 
         if (!phoneId.isNullOrEmpty()) {
@@ -42,25 +40,22 @@ class ActionService : IntentService("ActionService") {
             when (action) {
                 LOCK_PHONE_PATH -> {
                     if (prefs.getBoolean(PreferenceKey.PHONE_LOCKING_ENABLED_KEY, false)) {
-                        sendMessage(phoneId, action, getString(R.string.lock_phone_success_message), getString(R.string.lock_phone_failed_message))
-                    } else {
-                        ConfirmationActivityHandler.failAnimation(this, getString(R.string.lock_phone_disabled_message))
+                        sendMessage(
+                                phoneId,
+                                action,
+                                getString(R.string.lock_phone_success_message),
+                                getString(R.string.lock_phone_failed_message))
                     }
-                    val providerUpdateRequester = ProviderUpdateRequester(this, ComponentName(packageName, PhoneBatteryComplicationProvider::class.java.name))
-                    providerUpdateRequester.requestUpdateAll()
                 }
                 REQUEST_BATTERY_UPDATE_PATH -> {
                     if (prefs.getBoolean(BATTERY_SYNC_ENABLED_KEY, false)) {
-                        sendMessage(phoneId, action, getString(R.string.battery_sync_force_sync_success), getString(R.string.battery_sync_force_sync_fail))
-                    } else {
-                        ConfirmationActivityHandler.failAnimation(this, getString(R.string.battery_sync_disabled))
+                        sendMessage(
+                                phoneId,
+                                action,
+                                getString(R.string.battery_sync_force_sync_success),
+                                getString(R.string.battery_sync_force_sync_fail))
                     }
                 }
-            }
-        } else {
-            when (action) {
-                LOCK_PHONE_PATH -> ConfirmationActivityHandler.failAnimation(this, getString(R.string.lock_phone_failed_message))
-                REQUEST_BATTERY_UPDATE_PATH -> ConfirmationActivityHandler.failAnimation(this, getString(R.string.phone_battery_update_failed))
             }
         }
     }
