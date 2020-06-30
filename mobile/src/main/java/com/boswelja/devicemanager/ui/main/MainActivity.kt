fb647a5f9e80dd36f835270f645ded1b36430782
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-30 13:33:26 +1200

    Switch MainActivity.kt to the AndroidX Navigation components

--- mobile/src/main/java/com/boswelja/devicemanager/ui/main/MainActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/main/MainActivity.kt
@@ -8,27 +8,19 @@
 package com.boswelja.devicemanager.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.databinding.ActivityMainBinding
 import com.boswelja.devicemanager.messages.database.MessageDatabase.Companion.MESSAGE_COUNT_KEY
 import com.boswelja.devicemanager.ui.base.BaseWatchPickerActivity
 import com.boswelja.devicemanager.ui.changelog.ChangelogDialogFragment
-import com.boswelja.devicemanager.ui.main.appinfo.AppInfoFragment
-import com.boswelja.devicemanager.ui.main.appsettings.AppSettingsFragment
-import com.boswelja.devicemanager.ui.main.extensions.ExtensionsFragment
-import com.boswelja.devicemanager.ui.main.messages.MessageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import timber.log.Timber
 
 class MainActivity : BaseWatchPickerActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private val extensionsFragment = ExtensionsFragment()
-    private var messagesFragment: MessageFragment? = null
-    private var appSettingsFragment: AppSettingsFragment? = null
-    private var appInfoFragment: AppInfoFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,9 +29,7 @@ class MainActivity : BaseWatchPickerActivity() {
         setContentView(binding.root)
         binding.apply {
             setupWatchPickerSpinner(toolbarLayout.toolbar)
-            bottomNavigation.setOnNavigationItemSelectedListener {
-                handleNavigation(it.itemId)
-            }
+            bottomNavigation.setupWithNavController(findNavController(R.id.nav_host_fragment))
         }
     }
 
@@ -48,11 +38,6 @@ class MainActivity : BaseWatchPickerActivity() {
         Timber.d("onStart() called")
         updateMessagesBadge()
         showChangelogIfNeeded()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        handleNavigation(binding.bottomNavigation.selectedItemId)
     }
 
     /**
@@ -66,83 +51,6 @@ class MainActivity : BaseWatchPickerActivity() {
     }
 
     /**
-     * Handle navigation when a new item is selected in the [BottomNavigationView].
-     * @param selectedItemId The ID of the selected item.
-     */
-    private fun handleNavigation(selectedItemId: Int): Boolean {
-        Timber.d("handleNavigation() called")
-        return when (selectedItemId) {
-            R.id.extensions_navigation -> {
-                showExtensionsFragment()
-                true
-            }
-            R.id.messages_navigation -> {
-                showMessagesFragment()
-                true
-            }
-            R.id.settings_navigation -> {
-                showAppSettingsFragment()
-                true
-            }
-            R.id.app_info_navigation -> {
-                showAppInfoFragment()
-                true
-            }
-            else -> false
-        }
-    }
-
-    /**
-     * Shows the [ExtensionsFragment].
-     */
-    private fun showExtensionsFragment() {
-        Timber.i("Showing ExtensionsFragment")
-        navigate(extensionsFragment)
-    }
-
-    /**
-     * Shows the [MessageFragment].
-     */
-    private fun showMessagesFragment() {
-        if (messagesFragment == null) messagesFragment = MessageFragment()
-        Timber.i("Showing MessageFragment")
-        navigate(messagesFragment!!)
-    }
-
-    /**
-     * Shows the [AppSettingsFragment].
-     */
-    private fun showAppSettingsFragment() {
-        if (appSettingsFragment == null) appSettingsFragment = AppSettingsFragment()
-        Timber.i("Showing AppSettingsFragment")
-        navigate(appSettingsFragment!!)
-    }
-
-    /**
-     * Shows the [AppInfoFragment].
-     */
-    private fun showAppInfoFragment() {
-        if (appInfoFragment == null) appInfoFragment = AppInfoFragment()
-        Timber.i("Showing AppInfoFragment")
-        navigate(appInfoFragment!!)
-    }
-
-    /**
-     * Navigates to a given [Fragment].
-     * @param fragment The [Fragment] instance to navigate to.
-     */
-    private fun navigate(fragment: Fragment) {
-        try {
-            supportFragmentManager.beginTransaction()
-                    .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
-                    .replace(R.id.fragment_holder, fragment)
-                    .commit()
-        } catch (e: IllegalStateException) {
-            Timber.w(e)
-        }
-    }
-
-    /**
      * Updates the message count badge on the [BottomNavigationView].
      */
     fun updateMessagesBadge() {
@@ -151,11 +59,11 @@ class MainActivity : BaseWatchPickerActivity() {
         val shouldShowMessageBadge = messageCount > 0
         binding.apply {
             if (shouldShowMessageBadge) {
-                bottomNavigation.getOrCreateBadge(R.id.messages_navigation).apply {
+                bottomNavigation.getOrCreateBadge(R.id.messageFragment).apply {
                     number = messageCount
                 }
             } else {
-                bottomNavigation.removeBadge(R.id.messages_navigation)
+                bottomNavigation.removeBadge(R.id.messageFragment)
             }
         }
     }
