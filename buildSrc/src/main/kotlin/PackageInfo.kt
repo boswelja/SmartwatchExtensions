16d85b35ced383209ded572b2555dc78493dd8cd
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-07-22 22:28:54 +1200

    Migrate to Gradle Kotlin DSL for build scripts

---
+++ buildSrc/src/main/kotlin/PackageInfo.kt
@@ -0,0 +1,82 @@
+@file:Suppress("SpellCheckingInspection")
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileWriter
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.Properties
+import java.util.concurrent.TimeUnit
+
+object DebugInfo {
+    const val idSuffix = ".debug"
+}
+
+object PackageInfo {
+    const val packageName = "com.boswelja.devicemanager"
+    const val versionName = "2.4.5"
+
+    fun getVersionCode(): Int {
+        val versionBuildKey = "version.build_number"
+        val versionPropFile = File("version.properties")
+        val lastModified = versionPropFile.lastModified()
+        val versionProps = Properties()
+        if (!versionPropFile.canRead()) {
+            if (versionPropFile.createNewFile()) {
+                versionProps.load(FileInputStream(versionPropFile))
+            } else throw IOException("Unable to open ${versionPropFile.absolutePath}")
+        } else {
+            versionProps.load(FileInputStream(versionPropFile))
+        }
+
+        val versionBuild: Int = if (versionProps.containsKey(versionBuildKey) && !isOldData(lastModified)) {
+            versionProps[versionBuildKey].toString().toInt()
+        } else {
+            0
+        }
+        if (versionBuild < 99) {
+            if (shouldIncrementVersion(lastModified)) {
+                versionProps[versionBuildKey] = (versionBuild + 1).toString()
+                versionProps.store(FileWriter(versionPropFile), null)
+            }
+        } else {
+            throw Exception("Build limit reached")
+        }
+        val buildNumber = String.format("%02d", versionBuild)
+
+        val dateSection = SimpleDateFormat("yyyyMMdd", Locale.ROOT).format(Date())
+        return dateSection.plus(buildNumber).toInt()
+    }
+
+    private fun shouldIncrementVersion(lastEditedTimestamp: Long): Boolean {
+        val lastBuildDate = Calendar.getInstance()
+        lastBuildDate.timeInMillis = lastEditedTimestamp
+
+        val currentDate = Calendar.getInstance()
+        currentDate.timeInMillis = System.currentTimeMillis()
+
+        val minimumDiff = TimeUnit.SECONDS.toMillis(45)
+        return (currentDate.timeInMillis - lastBuildDate.timeInMillis) >= minimumDiff
+    }
+
+    private fun isOldData(timestamp: Long): Boolean {
+        val lastBuildDate = Calendar.getInstance()
+        lastBuildDate.timeInMillis = timestamp
+        lastBuildDate.set(Calendar.HOUR_OF_DAY, 0)
+        lastBuildDate.set(Calendar.MINUTE, 0)
+        lastBuildDate.set(Calendar.SECOND, 0)
+        lastBuildDate.set(Calendar.MILLISECOND, 0)
+
+        val currentDate = Calendar.getInstance()
+        currentDate.timeInMillis = System.currentTimeMillis()
+        currentDate.set(Calendar.HOUR_OF_DAY, 0)
+        currentDate.set(Calendar.MINUTE, 0)
+        currentDate.set(Calendar.SECOND, 0)
+        currentDate.set(Calendar.MILLISECOND, 0)
+        return lastBuildDate < currentDate
+    }
+
+}
---
+++ mobile/build.gradle.kts
@@ -0,0 +1,109 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+    id(BuildPlugins.kotlinKapt)
+    id("com.diffplug.spotless") version "5.1.0"
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compile)
+
+    defaultConfig {
+        minSdkVersion(AndroidSdk.mobileMin)
+        targetSdkVersion(AndroidSdk.target)
+
+        applicationId = PackageInfo.packageName
+        versionCode = PackageInfo.getVersionCode()
+        versionName = PackageInfo.versionName
+
+        testApplicationId = PackageInfo.packageName + ".test"
+        testInstrumentationRunner ="androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments(mapOf("room.schemaLocation" to "$rootDir/schemas"))
+            }
+        }
+    }
+
+    buildFeatures.viewBinding = true
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            applicationIdSuffix(DebugInfo.idSuffix)
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree("libs") { include("*.aar") })
+    implementation(project(":common"))
+
+    implementation(Libraries.kotlinStdlib)
+
+    implementation(Libraries.androidxAppCompat)
+    implementation(Libraries.androidxBrowser)
+    implementation(Libraries.androidxConstraintLayout)
+    implementation(Libraries.androidxCoreKtx)
+    implementation(Libraries.androidxFragment)
+    implementation(Libraries.androidxLifecycleViewModel)
+    implementation(Libraries.androidxLifecycleLiveData)
+    implementation(Libraries.androidxLifecycleCommon)
+    implementation(Libraries.androidxPreference)
+    implementation(Libraries.androidxRoomRuntime)
+    implementation(Libraries.androidxRoomKtx)
+    implementation(Libraries.androidxWork)
+
+    implementation(Libraries.billingClient)
+    implementation(Libraries.materialComponents)
+    implementation(Libraries.playServicesWearable)
+    implementation(Libraries.timber)
+
+    kapt(Libraries.androidxRoomKapt)
+
+    debugImplementation(DebugLibraries.androidxFragment)
+
+    testImplementation(TestLibraries.androidxArch)
+    testImplementation(TestLibraries.junit)
+    testImplementation(TestLibraries.googleTruth)
+
+    androidTestImplementation(AndroidTestLibraries.androidxEspresso)
+    androidTestImplementation(AndroidTestLibraries.androidxTestRunner)
+    androidTestImplementation(AndroidTestLibraries.androidxTestRules)
+    androidTestImplementation(AndroidTestLibraries.googleTruth)
+}
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+
+        ktlint("0.37.2")
+        endWithNewline()
+
+        licenseHeaderFile("../License")
+    }
+    kotlinGradle {
+        ktlint("0.37.2")
+        endWithNewline()
+    }
+    format("xml") {
+        target("**/*.xml")
+
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+}
