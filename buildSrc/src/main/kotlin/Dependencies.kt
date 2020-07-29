f0814b88b26879bc0ec99c26e4e0269df9564b65
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-07-29 18:24:44 +1200

    Update Gradle

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ const val kotlinVersion = "1.4.0-rc"
 
 object BuildPlugins {
     object Versions {
-        const val buildToolsVersion = "4.2.0-alpha05"
+        const val buildToolsVersion = "4.2.0-alpha06"
         const val googleServicesVersion = "4.3.3"
     }
 
