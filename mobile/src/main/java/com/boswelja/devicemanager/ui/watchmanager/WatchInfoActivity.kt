494fe7feae813bf0e315cc0b5bfcb562f687856d
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-08-01 15:46:09 +1200

    Tidy up WatchInfoActivity.kt Remove WatchDatabase variable in favor of WatchManager

--- mobile/src/main/java/com/boswelja/devicemanager/ui/watchmanager/WatchInfoActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/ui/watchmanager/WatchInfoActivity.kt
@@ -7,16 +7,13 @@
  */
 package com.boswelja.devicemanager.ui.watchmanager
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.doAfterTextChanged
 import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.databinding.ActivityWatchInfoBinding
 import com.boswelja.devicemanager.ui.base.BaseToolbarActivity
 import com.boswelja.devicemanager.watchmanager.WatchManager
-import com.boswelja.devicemanager.watchmanager.database.WatchDatabase
 import com.boswelja.devicemanager.watchmanager.item.Watch
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -26,42 +23,15 @@ import timber.log.Timber
 
 class WatchInfoActivity : BaseToolbarActivity() {
 
-    private val nicknameChangeListener = object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            binding.apply {
-                if (!editable.isNullOrBlank()) {
-                    Timber.i("Updating watch nickname")
-                    watchNameLayout.isErrorEnabled = false
-                    coroutineScope.launch(Dispatchers.IO) {
-                        watchManager.database.watchDao().setName(watchId!!, editable.toString())
-                    }
-                    resultCode = RESULT_WATCH_NAME_CHANGED
-                } else {
-                    Timber.w("Invalid watch nickname")
-                    watchNameLayout.isErrorEnabled = true
-                    watchNameField.error = "Nickname cannot be blank"
-                }
-            }
-        }
-
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {} // Do nothing
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {} // Do nothing
-    }
-
     private val coroutineScope = MainScope()
-    private val watchDatabase: WatchDatabase by lazy { WatchDatabase.get(this) }
+    private val watchManager by lazy { WatchManager.get(this) }
 
     private lateinit var binding: ActivityWatchInfoBinding
 
-    private lateinit var watchManager: WatchManager
-    private var watchId: String? = null
-
-    private var resultCode = RESULT_WATCH_UNCHANGED
+    private val watchId by lazy { intent?.getStringExtra(EXTRA_WATCH_ID)!! }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        watchId = intent?.getStringExtra(EXTRA_WATCH_ID)
 
         binding = ActivityWatchInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -76,18 +46,7 @@ class WatchInfoActivity : BaseToolbarActivity() {
             }
         }
 
-        watchManager = WatchManager.get(this)
         resetNicknameTextField()
-    }
-
-    override fun finish() {
-        setResultCode()
-        super.finish()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        binding.watchNameField.removeTextChangedListener(nicknameChangeListener)
     }
 
     /**
@@ -95,26 +54,25 @@ class WatchInfoActivity : BaseToolbarActivity() {
      */
     private fun resetNicknameTextField() {
         coroutineScope.launch(Dispatchers.IO) {
-            val watchName = watchDatabase.watchDao().get(watchId!!)?.name
+            val watchName = watchManager.database.watchDao().get(watchId)?.name
             withContext(Dispatchers.Main) {
                 binding.apply {
                     watchNameField.setText(watchName)
-                    watchNameField.addTextChangedListener(nicknameChangeListener)
+                    watchNameField.doAfterTextChanged {
+                        if (!it.isNullOrBlank()) {
+                            Timber.i("Updating watch nickname")
+                            watchNameLayout.isErrorEnabled = false
+                            coroutineScope.launch(Dispatchers.IO) {
+                                watchManager.database.watchDao().setName(watchId, it.toString())
+                            }
+                        } else {
+                            Timber.w("Invalid watch nickname")
+                            watchNameLayout.isErrorEnabled = true
+                            watchNameField.error = "Nickname cannot be blank"
+                        }
+                    }
                 }
             }
-        }
-    }
-
-    /**
-     * Sets the activity result code, as well as any extras that may need to be included.
-     */
-    private fun setResultCode() {
-        if (resultCode != RESULT_WATCH_UNCHANGED) {
-            Intent().putExtra(EXTRA_WATCH_ID, watchId).also {
-                setResult(resultCode, it)
-            }
-        } else {
-            setResult(resultCode)
         }
     }
 
@@ -165,7 +123,7 @@ class WatchInfoActivity : BaseToolbarActivity() {
     private fun confirmForgetWatch() {
         Timber.d("confirmForgetWatch() called")
         coroutineScope.launch(Dispatchers.IO) {
-            val watch = watchDatabase.watchDao().get(watchId!!)
+            val watch = watchManager.database.watchDao().get(watchId)
             withContext(Dispatchers.Main) {
                 AlertDialog.Builder(this@WatchInfoActivity).apply {
                     setTitle(R.string.forget_watch_dialog_title)
@@ -193,7 +151,6 @@ class WatchInfoActivity : BaseToolbarActivity() {
             withContext(Dispatchers.Main) {
                 if (success) {
                     Timber.i("Successfully forgot watch")
-                    resultCode = RESULT_WATCH_REMOVED
                     finish()
                 } else {
                     Timber.w("Failed to forget watch")
@@ -205,9 +162,5 @@ class WatchInfoActivity : BaseToolbarActivity() {
 
     companion object {
         const val EXTRA_WATCH_ID = "extra_watch_id"
-
-        const val RESULT_WATCH_UNCHANGED = 0
-        const val RESULT_WATCH_REMOVED = 1
-        const val RESULT_WATCH_NAME_CHANGED = 2
     }
 }
