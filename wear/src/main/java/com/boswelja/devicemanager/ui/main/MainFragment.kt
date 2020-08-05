ef2d6ad53f846b073bb01aaf6a339779a13079d7
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-08-05 21:39:30 +1200

    Don't use LiveData to check whether the watch is registered in MainViewModel.kt LiveData objects require a lifecycle owner (viewLifecycleOwner for fragments) and only start receiving updates when the lifecycle owner is started. We can avoid both of these to reduce startup overhead

--- wear/src/main/java/com/boswelja/devicemanager/ui/main/MainFragment.kt
+++ wear/src/main/java/com/boswelja/devicemanager/ui/main/MainFragment.kt
@@ -8,9 +8,6 @@
 package com.boswelja.devicemanager.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -19,19 +16,12 @@ class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return View(requireContext()) // Empty view so onViewCreated gets called
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.isRegistered.observe(viewLifecycleOwner) {
-            it?.let { isRegistered ->
-                if (isRegistered) {
-                    findNavController().navigate(MainFragmentDirections.toExtensionsFragment())
-                } else {
-                    findNavController().navigate(MainFragmentDirections.toSetupFragment())
-                }
-            }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (viewModel.isRegistered) {
+            findNavController().navigate(MainFragmentDirections.toExtensionsFragment())
+        } else {
+            findNavController().navigate(MainFragmentDirections.toSetupFragment())
         }
     }
 }
--- wear/src/main/java/com/boswelja/devicemanager/ui/main/MainViewModel.kt
+++ wear/src/main/java/com/boswelja/devicemanager/ui/main/MainViewModel.kt
@@ -9,19 +9,13 @@ package com.boswelja.devicemanager.ui.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.preference.PreferenceManager
 import com.boswelja.devicemanager.phoneconnectionmanager.References.PHONE_ID_KEY
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
     private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(application)
 
-    private val _isRegistered = MutableLiveData<Boolean?>(null)
-    val isRegistered: LiveData<Boolean?>
-        get() = _isRegistered
-
-    init {
-        _isRegistered.postValue(!sharedPreferences.getString(PHONE_ID_KEY, "").isNullOrBlank())
-    }
+    val isRegistered: Boolean
+        get() = !sharedPreferences.getString(PHONE_ID_KEY, "").isNullOrBlank()
 }
