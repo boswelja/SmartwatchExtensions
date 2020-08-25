1d6bbac9f0dce9284e74924aff9e27f706dbf4bd
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-08-25 15:31:31 +1200

    Add LiveDataTestUtils.kt to Wear module

---
+++ wear/src/test/java/com/boswelja/devicemanager/LiveDataTestUtils.kt
@@ -0,0 +1,41 @@
+/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
+package com.boswelja.devicemanager
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2, timeUnit: TimeUnit = TimeUnit.SECONDS, afterObserve: (value: T?) -> Unit = {}
+): T {
+  var data: T? = null
+  val latch = CountDownLatch(1)
+  val observer =
+      object : Observer<T?> {
+        override fun onChanged(t: T?) {
+          data = t
+          latch.countDown()
+          this@getOrAwaitValue.removeObserver(this)
+        }
+      }
+  this.observeForever(observer)
+
+  afterObserve.invoke(data)
+
+  // Don't wait indefinitely if the LiveData is not set.
+  if (!latch.await(time, timeUnit)) {
+    this.removeObserver(observer)
+    throw TimeoutException("LiveData value was never set.")
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  return data as T
+}
