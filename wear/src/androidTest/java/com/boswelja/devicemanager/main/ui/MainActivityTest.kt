532ad8a9f458c347a335cf47fa2174b77c099752
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-08-29 13:25:21 +1200

    Drop TestNavHostController from MainActivityTest.kt Tests take longer now, but this is needed until there's a way to change the nav controller before onStart at least

--- wear/src/androidTest/java/com/boswelja/devicemanager/main/ui/MainActivityTest.kt
+++ wear/src/androidTest/java/com/boswelja/devicemanager/main/ui/MainActivityTest.kt
@@ -2,9 +2,7 @@ package com.boswelja.devicemanager.main.ui
 
 import android.content.SharedPreferences
 import androidx.core.content.edit
-import androidx.lifecycle.Lifecycle
-import androidx.navigation.Navigation
-import androidx.navigation.testing.TestNavHostController
+import androidx.navigation.findNavController
 import androidx.preference.PreferenceManager
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
@@ -12,25 +10,24 @@ import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.phoneconnectionmanager.References
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
-
 import org.junit.Test
 
 class MainActivityTest {
 
   private lateinit var sharedPreferences: SharedPreferences
-  private lateinit var navController: TestNavHostController
 
   @Before
   fun setUp() {
     sharedPreferences = PreferenceManager.getDefaultSharedPreferences(ApplicationProvider.getApplicationContext())
-    navController = TestNavHostController(ApplicationProvider.getApplicationContext())
   }
 
   @Test
   fun testSetupOpensIfNoPhoneID() {
     sharedPreferences.edit(commit = true) { remove(References.PHONE_ID_KEY) }
     val scenario = getScenario()
-    assertThat(navController.currentDestination?.id).isEqualTo(R.id.setupFragment)
+    scenario.onActivity {
+      assertThat(it.findNavController(R.id.nav_host_fragment).currentDestination?.id).isEqualTo(R.id.setupFragment)
+    }
     scenario.close()
   }
 
@@ -38,15 +35,11 @@ class MainActivityTest {
   fun testExtensionsOpenIfHasPhoneID() {
     sharedPreferences.edit(commit = true) { putString(References.PHONE_ID_KEY, "id123") }
     val scenario = getScenario()
-    assertThat(navController.currentDestination?.id).isEqualTo(R.id.extensionsFragment)
+    scenario.onActivity {
+      assertThat(it.findNavController(R.id.nav_host_fragment).currentDestination?.id).isEqualTo(R.id.extensionsFragment)
+    }
     scenario.close()
   }
 
-  private fun getScenario() = launchActivity<MainActivity>().apply {
-    onActivity {
-      navController.setGraph(R.navigation.main_graph)
-      Navigation.setViewNavController(it.requireViewById(R.id.nav_host_fragment), navController)
-    }
-    moveToState(Lifecycle.State.STARTED)
-  }
+  private fun getScenario() = launchActivity<MainActivity>()
 }
