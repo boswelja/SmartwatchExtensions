fca8f468d866a91d2a87427b76b4391fe359235e
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-10-15 00:05:47 +1300

    Add a test for SelectedWatchHandler.kt

--- mobile/src/test/java/com/boswelja/devicemanager/watchmanager/SelectedWatchHandlerTest.kt
+++ mobile/src/test/java/com/boswelja/devicemanager/watchmanager/SelectedWatchHandlerTest.kt
@@ -1,26 +1,53 @@
+/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
 package com.boswelja.devicemanager.watchmanager
 
-import org.junit.After
+import android.os.Build
+import android.os.Looper
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.boswelja.devicemanager.getOrAwaitValue
+import com.boswelja.devicemanager.watchmanager.item.Watch
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.spyk
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.Config
 
-import org.junit.Assert.*
-
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.Q])
 class SelectedWatchHandlerTest {
+  @get:Rule val instantExecutorRule = InstantTaskExecutorRule()
 
-    @Before
-    fun setUp() {
-    }
+  val selectedWatchHandler =
+      spyk(SelectedWatchHandler.get(ApplicationProvider.getApplicationContext()))
 
-    @After
-    fun tearDown() {
-    }
+  val dummyWatch = Watch("an-id-1234", "Watch 1", null)
 
-    @Test
-    fun getSelectedWatch() {
-    }
+  @Before
+  fun setUp() {
+    every { selectedWatchHandler.database.watchDao().get(dummyWatch.id) } returns dummyWatch
+    every { selectedWatchHandler.database.watchDao().get("") } returns null
+  }
 
-    @Test
-    fun selectWatchById() {
-    }
+  @Test
+  fun `Selecting a watch correctly updates corresponding LiveData`() {
+    selectedWatchHandler.selectWatchById(dummyWatch.id)
+    shadowOf(Looper.getMainLooper()).idle()
+    selectedWatchHandler.selectedWatch.getOrAwaitValue { assertThat(it).isEqualTo(dummyWatch) }
+
+    selectedWatchHandler.selectWatchById("")
+    shadowOf(Looper.getMainLooper()).idle()
+    selectedWatchHandler.selectedWatch.getOrAwaitValue { assertThat(it).isNull() }
+  }
 }
