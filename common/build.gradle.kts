1c4112b76c5dd31eafdaa721655308257f681bcc
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-10-25 18:36:05 +1300

    More formatting

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,44 +1,44 @@
 plugins {
-    id(BuildPlugins.androidLibrary)
-    id(BuildPlugins.kotlinAndroid)
-    id(BuildPlugins.kotlinAndroidExtensions)
-    id(BuildPlugins.kotlinKapt)
+  id(BuildPlugins.androidLibrary)
+  id(BuildPlugins.kotlinAndroid)
+  id(BuildPlugins.kotlinAndroidExtensions)
+  id(BuildPlugins.kotlinKapt)
 }
 
 android {
-    compileSdkVersion(AndroidSdk.compile)
+  compileSdkVersion(AndroidSdk.compile)
 
-    defaultConfig {
-        minSdkVersion(AndroidSdk.mobileMin)
-        targetSdkVersion(AndroidSdk.target)
-    }
+  defaultConfig {
+    minSdkVersion(AndroidSdk.mobileMin)
+    targetSdkVersion(AndroidSdk.target)
+  }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 
-    buildFeatures.viewBinding = true
-    buildFeatures.dataBinding = true
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
+  buildFeatures.viewBinding = true
+  buildFeatures.dataBinding = true
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = true
+      isShrinkResources = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
+  }
 }
 
 dependencies {
-    implementation(Libraries.androidxAppCompat)
-    implementation(Libraries.androidxConstraintLayout)
-    implementation(Libraries.androidxCoreKtx)
-    implementation(Libraries.androidxPreference)
+  implementation(Libraries.androidxAppCompat)
+  implementation(Libraries.androidxConstraintLayout)
+  implementation(Libraries.androidxCoreKtx)
+  implementation(Libraries.androidxPreference)
 
-    implementation(Libraries.materialComponents)
-    implementation(Libraries.playServicesWearable)
-    implementation(Libraries.timber)
+  implementation(Libraries.materialComponents)
+  implementation(Libraries.playServicesWearable)
+  implementation(Libraries.timber)
 }
