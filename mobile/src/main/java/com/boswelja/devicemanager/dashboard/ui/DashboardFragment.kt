ed517df43da4b21d701ec4fa4e5ec988427bd001
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-11-08 19:06:35 +1300

    Add basic watch status reporting to DashboardFragment.kt

--- mobile/src/main/java/com/boswelja/devicemanager/dashboard/ui/DashboardFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/dashboard/ui/DashboardFragment.kt
@@ -6,14 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.boswelja.devicemanager.R
+import com.boswelja.devicemanager.databinding.FragmentDashboardBinding
+import com.boswelja.devicemanager.watchmanager.SelectedWatchHandler
+import com.boswelja.devicemanager.watchmanager.WatchStatus
 
 class DashboardFragment : Fragment() {
+
+    private val selectedWatchHandler by lazy { SelectedWatchHandler.get(requireContext()) }
+
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        selectedWatchHandler.status.observe(this) { status ->
+            val statusStringRes = when (status) {
+                WatchStatus.UNKNOWN -> R.string.watch_status_unknown
+                WatchStatus.MISSING_APP -> R.string.watch_status_missing_app
+                WatchStatus.NOT_REGISTERED -> R.string.watch_status_not_registered
+                WatchStatus.DISCONNECTED -> R.string.watch_status_disconnected
+                WatchStatus.CONNECTED -> R.string.watch_status_connected
+                else -> R.string.watch_status_error
+            }
+            binding.watchStatusText.setText(statusStringRes)
+        }
     }
 }
