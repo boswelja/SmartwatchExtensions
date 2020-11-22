2566ca568058d340e713d1c588f382f1b931f74b
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-11-22 17:38:54 +1300

    Create MessagesAdapter.kt

---
+++ mobile/src/main/java/com/boswelja/devicemanager/messages/ui/MessagesAdapter.kt
@@ -0,0 +1,45 @@
+package com.boswelja.devicemanager.messages.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.boswelja.devicemanager.databinding.MessageItemBinding
+import com.boswelja.devicemanager.messages.Message
+import com.boswelja.devicemanager.messages.MessageDiffCallback
+
+class MessagesAdapter(
+    private val actionButtonCallback: (action: Message.Action) -> Unit
+) : PagingDataAdapter<Message, MessagesAdapter.ViewHolder>(
+    MessageDiffCallback()
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder.from(parent)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { message ->
+            holder.binding.messageIcon.setImageResource(message.iconRes)
+            holder.binding.messageTitle.text = message.label
+            holder.binding.messageText.text = message.desc
+            if (message.hasAction) {
+                holder.binding.messageActionButton.apply {
+                    text = message.buttonLabel
+                    setOnClickListener { actionButtonCallback(message.action!!) }
+                    visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    class ViewHolder(val binding: MessageItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = MessageItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
