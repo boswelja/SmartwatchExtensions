f8bc38286023fd21b36d597d73ad9cbb8686041e
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-11-23 19:24:40 +1300

    Replace iconRes with an enum class Icon Defines available icons, should fix issues where drawable IDs might change between updates. Dropped buttonLabel from Message.kt

--- mobile/src/main/java/com/boswelja/devicemanager/messages/Message.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/messages/Message.kt
@@ -1,16 +1,17 @@
 package com.boswelja.devicemanager.messages
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import com.boswelja.devicemanager.R
 
 @Entity(tableName = "messages")
 data class Message(
-    @DrawableRes val iconRes: Int,
+    val icon: Icon,
     val title: String,
     val text: String,
-    val buttonLabel: String = "",
     val action: Action? = null,
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val deleted: Boolean = false,
@@ -21,9 +22,17 @@ data class Message(
     val hasAction: Boolean = action != null
 
     /** Possible actions for a given [Message] action button. */
-    enum class Action {
-        LAUNCH_NOTIFICATION_SETTINGS,
-        LAUNCH_PLAY_STORE,
-        LAUNCH_CHANGELOG
+    enum class Action(@StringRes val labelRes: Int) {
+        LAUNCH_NOTIFICATION_SETTINGS(R.string.message_action_noti_settings),
+        LAUNCH_PLAY_STORE(R.string.message_action_play_store),
+        LAUNCH_CHANGELOG(R.string.message_action_changelog),
+        INSTALL_UPDATE(R.string.message_action_install_update)
+    }
+
+    /** Possible icons for a given [Message] **/
+    enum class Icon(@DrawableRes val iconRes: Int) {
+        UPDATE(R.drawable.noti_ic_update),
+        HELP(R.drawable.noti_ic_help),
+        ERROR(R.drawable.noti_ic_error)
     }
 }
