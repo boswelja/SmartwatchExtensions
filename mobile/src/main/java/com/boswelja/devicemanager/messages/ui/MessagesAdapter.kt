2c945c9f191d659c4e8dcf8fb6d9b168234f9196
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-11-23 20:04:53 +1300

    Show message receive time/date in the UI

--- mobile/src/main/java/com/boswelja/devicemanager/messages/ui/MessagesAdapter.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/messages/ui/MessagesAdapter.kt
@@ -1,5 +1,6 @@
 package com.boswelja.devicemanager.messages.ui
 
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.boswelja.devicemanager.databinding.MessageItemBinding
 import com.boswelja.devicemanager.messages.Message
 import com.boswelja.devicemanager.messages.MessageDiffCallback
+import java.util.Date
+import java.util.concurrent.TimeUnit
 
 class MessagesAdapter(
     private val showAction: Boolean = true,
@@ -21,9 +24,7 @@ class MessagesAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         getItem(position)?.let { message ->
-            holder.binding.messageIcon.setImageResource(message.icon.iconRes)
-            holder.binding.messageTitle.text = message.title
-            holder.binding.messageText.text = message.text
+            holder.bind(message)
             if (showAction && message.action != null) {
                 holder.binding.messageActionButton.apply {
                     setText(message.action.labelRes)
@@ -35,6 +36,22 @@ class MessagesAdapter(
     }
 
     class ViewHolder(val binding: MessageItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(message: Message) {
+            binding.messageIcon.setImageResource(message.icon.iconRes)
+            binding.messageTitle.text = message.title
+            binding.messageText.text = message.text
+            val todayMillis = System.currentTimeMillis()
+            val received = Date(message.timestamp)
+            val isToday = (todayMillis - message.timestamp) < TimeUnit.DAYS.toMillis(1)
+            val timeString = if (isToday) {
+                DateFormat.getTimeFormat(itemView.context).format(received)
+            } else {
+                DateFormat.getDateFormat(itemView.context).format(received)
+            }
+            binding.receivedText.text = timeString
+        }
+
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
