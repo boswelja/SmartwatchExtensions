24c780ac3b0476a07b58de10995d11ae290439ef
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-11-29 17:42:34 +1300

    Rewrite MessagesViewModelTest.kt to work with the latest changes

--- mobile/src/test/java/com/boswelja/devicemanager/messages/ui/MessagesViewModelTest.kt
+++ mobile/src/test/java/com/boswelja/devicemanager/messages/ui/MessagesViewModelTest.kt
@@ -8,11 +8,18 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.boswelja.devicemanager.getOrAwaitValue
 import com.boswelja.devicemanager.messages.Message
+import com.boswelja.devicemanager.messages.MessageHandler
 import com.boswelja.devicemanager.messages.database.MessageDatabase
+import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.common.truth.Truth.assertThat
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
 import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -37,18 +44,32 @@ class MessagesViewModelTest {
 
     private lateinit var messagesViewModel: MessagesViewModel
     private lateinit var messageDatabase: MessageDatabase
+    private lateinit var messageHandler: MessageHandler
+
+    @RelaxedMockK private lateinit var appUpdateManager: AppUpdateManager
 
     @Before
     fun setUp() {
+        MockKAnnotations.init(this)
         messageDatabase = spyk(
             Room.inMemoryDatabaseBuilder(context, MessageDatabase::class.java)
                 .allowMainThreadQueries()
                 .build()
         )
+        messageHandler = spyk(
+            MessageHandler(
+                context,
+                messageDatabase,
+                mockk(relaxed = true),
+                TestCoroutineScope()
+            )
+        )
         messagesViewModel = MessagesViewModel(
             ApplicationProvider.getApplicationContext(),
             messageDatabase,
-            TestCoroutineDispatcher()
+            TestCoroutineDispatcher(),
+            appUpdateManager,
+            messageHandler
         )
     }
 
@@ -67,5 +88,19 @@ class MessagesViewModelTest {
 
         messagesViewModel.dismissMessage(messageId)
         count.getOrAwaitValue { assertThat(it).isEqualTo(0) }
+        verify(exactly = 1) { messageHandler.dismissMessage(messageId) }
+    }
+
+    @Test
+    fun `Restoring a message marks the message as not deleted in database`() {
+        val messageId = messageDatabase.messageDao().createMessage(message)
+        val count = messageDatabase.messageDao().getActiveMessagesCount()
+
+        messagesViewModel.dismissMessage(messageId)
+        count.getOrAwaitValue { assertThat(it).isEqualTo(0) }
+
+        messagesViewModel.restoreMessage(messageId)
+        count.getOrAwaitValue { assertThat(it).isEqualTo(1) }
+        verify(exactly = 1) { messageHandler.restoreMessage(messageId) }
     }
 }
