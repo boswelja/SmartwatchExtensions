80fdc8aa89fe760c454c3df8a0e2a200dace7a66
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-12-08 17:20:09 +1300

    Bump app version This will need to be done on each PR merge to ensure releases are published with unique names

--- buildSrc/src/main/kotlin/PackageInfo.kt
+++ buildSrc/src/main/kotlin/PackageInfo.kt
@@ -17,7 +17,7 @@ object DebugInfo {
 
 object PackageInfo {
     const val packageName = "com.boswelja.devicemanager"
-    const val versionName = "2.5.2"
+    const val versionName = "2.5.3"
 
     fun getVersionCode(): Int {
         val versionBuildKey = "version.build_number"
