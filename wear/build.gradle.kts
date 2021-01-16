cba01338c4558bd3f70959c3a81cbfc0dbd381dc
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-01-16 22:01:10 +1300

    Start moving wear module away from buildSrc Seems to still cause a fair bit of compile overhead, as well as break incremental builds, hence the move.

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -52,41 +52,69 @@ android {
 dependencies {
     implementation(project(":common"))
 
-    implementation(Libraries.androidxAppCompat)
-    implementation(Libraries.androidxConstraintLayout)
-    implementation(Libraries.androidxCoreKtx)
-    implementation(Libraries.androidxFragment)
-    implementation(Libraries.androidxLifecycleCommon)
-    implementation(Libraries.androidxLifecycleLiveData)
-    implementation(Libraries.androidxLifecycleViewModel)
-    implementation(Libraries.androidxNavigationUI)
-    implementation(Libraries.androidxNavigationFragment)
-    implementation(Libraries.androidxPreference)
-    implementation(Libraries.androidxWear)
+    val androidxArch = "2.1.0"
+    val androidxAppCompat = "1.3.0-alpha02"
+    val androidxBrowser = "1.3.0"
+    val androidxConstraintLayout = "2.1.0-alpha1"
+    val androidxCoreKtx = "1.5.0-alpha05"
+    val androidxEspresso = "3.1.0"
+    val androidxFragment = "1.3.0-beta02"
+    val androidxLifecycle = "2.3.0-beta01"
+    val androidxNavigation = "2.3.2"
+    val androidxPaging = "3.0.0-alpha10"
+    val androidxPreference = "1.1.1"
+    val androidxRoom = "2.3.0-alpha03"
+    val androidxTest = "1.3.1-alpha02"
+    val androidxTestExt = "1.1.2-rc03"
+    val androidxWork = "2.5.0-beta02"
+    val androidxWear = "1.2.0-alpha05"
 
-    implementation(Libraries.wearableSupport)
-    implementation(Libraries.playServicesWearable)
-    implementation(Libraries.materialComponents)
+    val billingClient = "3.0.2"
+    val googleMaterial = "1.3.0-alpha04"
+    val googlePlayCore = "1.9.0"
+    val playServicesWearable = "17.0.0"
+    val timber = "4.7.1"
+    val junit = "4.13"
+    val truth = "1.1"
+    val mockk = "1.10.3"
+    val robolectric = "4.4"
+    val coroutines = "1.4.2"
+    val wearableSupport = "2.8.1"
 
-    implementation(Libraries.timber)
+    implementation("androidx.appcompat:appcompat:$androidxAppCompat")
+    implementation("androidx.constraintlayout:constraintlayout:$androidxConstraintLayout")
+    implementation("androidx.core:core-ktx:$androidxCoreKtx")
+    implementation("androidx.lifecycle:lifecycle-common-java8:$androidxLifecycle")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidxLifecycle")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$androidxLifecycle")
+    implementation("androidx.navigation:navigation-fragment-ktx:$androidxNavigation")
+    implementation("androidx.navigation:navigation-ui-ktx:$androidxNavigation")
+    implementation("androidx.preference:preference:$androidxPreference")
+    implementation("androidx.wear:wear:$androidxWear")
 
-    compileOnly(Libraries.wearableCompile)
+    implementation("com.google.android.support:wearable:$wearableSupport")
+    implementation("com.google.android.gms:play-services-wearable:$playServicesWearable")
+    implementation("com.google.android.material:material:$googleMaterial")
+    implementation("com.jakewharton.timber:timber:$timber")
 
-    debugImplementation(DebugLibraries.androidxFragment)
+    compileOnly("com.google.android.wearable:wearable:$wearableSupport")
 
-    testImplementation(TestLibraries.androidxArch)
-    testImplementation(TestLibraries.androidxTestCore)
-    testImplementation(TestLibraries.androidxTestExt)
-    testImplementation(TestLibraries.junit)
-    testImplementation(TestLibraries.googleTruth)
-    testImplementation(TestLibraries.mockk)
-    testImplementation(TestLibraries.robolectric)
+    debugImplementation("androidx.fragment:fragment-testing:$androidxFragment")
 
-    androidTestImplementation(AndroidTestLibraries.googleTruth)
-    androidTestImplementation(AndroidTestLibraries.androidxEspresso)
-    androidTestImplementation(AndroidTestLibraries.androidxTestRunner)
-    androidTestImplementation(AndroidTestLibraries.androidxTestRules)
-    androidTestImplementation(AndroidTestLibraries.androidxTestExt)
-    androidTestImplementation(AndroidTestLibraries.androidxTestCore)
-    androidTestImplementation(AndroidTestLibraries.androidxNavigation)
+    testImplementation("androidx.arch.core:core-testing:$androidxArch")
+    testImplementation("androidx.test:core-ktx:$androidxTest")
+    testImplementation("androidx.test.ext:junit-ktx:$androidxTestExt")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutines")
+    testImplementation("junit:junit:$junit")
+    testImplementation("com.google.truth:truth:$truth")
+    testImplementation("io.mockk:mockk:$mockk")
+    testImplementation("org.robolectric:robolectric:$robolectric")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:$androidxEspresso")
+    androidTestImplementation("androidx.navigation:navigation-testing:$androidxNavigation")
+    androidTestImplementation("androidx.test:runner:$androidxTest")
+    androidTestImplementation("androidx.test:rules:$androidxTest")
+    androidTestImplementation("androidx.test:core-ktx:$androidxTest")
+    androidTestImplementation("com.google.truth:truth:$truth")
+    androidTestImplementation("io.mockk:mockk-android:$mockk")
 }
