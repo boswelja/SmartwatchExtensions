df1192f05d6405c5d7acb322c1b94083c47292a7
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-01-20 21:06:09 +1300

    Abstract BaseWidgetConfigActivity.kt Handles code that can be common between widget config activities

--- mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationActivity.kt
@@ -7,30 +7,25 @@
  */
 package com.boswelja.devicemanager.batterysync.widget.configuration
 
-import android.app.Activity
-import android.appwidget.AppWidgetManager
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.boswelja.devicemanager.R
-import com.boswelja.devicemanager.common.ui.BaseToolbarActivity
+import com.boswelja.devicemanager.common.ui.BaseWidgetConfigActivity
 import com.boswelja.devicemanager.common.ui.adapter.WatchAdapter
 import com.boswelja.devicemanager.databinding.ActivityWatchBatteryWidgetConfigurationBinding
 import com.boswelja.devicemanager.widget.database.WatchBatteryWidgetId
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
-class WatchBatteryWidgetConfigurationActivity : BaseToolbarActivity() {
+class WatchBatteryWidgetConfigurationActivity : BaseWidgetConfigActivity() {
 
     private val viewModel: WatchBatteryWidgetConfigurationViewModel by viewModels()
-    private val resultIntent = Intent()
-    private val adapter by lazy { WatchAdapter { finishAndCreateWidget(it.id) } }
+    private val adapter by lazy {
+        WatchAdapter {
+            finishWidgetConfig(WatchBatteryWidgetId(it.id, widgetId))
+        }
+    }
 
     private lateinit var binding: ActivityWatchBatteryWidgetConfigurationBinding
-
-    private var widgetId: Int = AppWidgetManager.INVALID_APPWIDGET_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,36 +34,12 @@ class WatchBatteryWidgetConfigurationActivity : BaseToolbarActivity() {
         setContentView(binding.root)
 
         setupToolbar(binding.toolbarLayout.toolbar, showTitle = true, showUpButton = true)
-        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_close)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
 
         binding.watchPickerRecyclerView.adapter = adapter
         viewModel.allRegisteredWatches.observe(this) {
             Timber.d("Got ${it.count()} watches")
             adapter.submitList(it)
-        }
-
-        widgetId = getWidgetId()
-        resultIntent.apply { putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId) }
-        setResult(Activity.RESULT_CANCELED, resultIntent)
-    }
-
-    /**
-     * Gets the widget ID from the activity [getIntent].
-     * @return The widget ID.
-     */
-    private fun getWidgetId(): Int {
-        return intent?.extras?.getInt(
-            AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID
-        ) ?: AppWidgetManager.INVALID_APPWIDGET_ID
-    }
-
-    /** Stores the new widget's information, creates it then finishes this activity. */
-    private fun finishAndCreateWidget(watchId: String) {
-        Timber.d("finishAndCreateWidget($watchId) called")
-        setResult(Activity.RESULT_OK, resultIntent)
-
-        viewModel.addWidgetToDatabase(WatchBatteryWidgetId(watchId, widgetId)).invokeOnCompletion {
-            finish()
         }
     }
 }
---
+++ mobile/src/main/java/com/boswelja/devicemanager/common/ui/BaseWidgetConfigActivity.kt
@@ -0,0 +1,59 @@
+package com.boswelja.devicemanager.common.ui
+
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.boswelja.devicemanager.batterysync.widget.WatchBatteryWidget
+import com.boswelja.devicemanager.widget.database.WatchBatteryWidgetId
+import com.boswelja.devicemanager.widget.database.WidgetDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+/**
+ * An activity containing common code for widget configuration activities
+ */
+abstract class BaseWidgetConfigActivity : BaseToolbarActivity() {
+
+    private val resultIntent = Intent()
+
+    /**
+     * The ID of the widget we're configuring
+     */
+    protected var widgetId: Int = 0
+    private lateinit var widgetDatabase: WidgetDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Get the ID of the widget we're configuring
+        widgetId = intent?.extras?.getInt(
+            AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID
+        ) ?: AppWidgetManager.INVALID_APPWIDGET_ID
+
+        resultIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+        setResult(RESULT_CANCELED, resultIntent)
+        lifecycleScope.launch(Dispatchers.IO) {
+            widgetDatabase = WidgetDatabase.open(this@BaseWidgetConfigActivity)
+        }
+    }
+
+    /**
+     * Perform finishing touches for the widget config
+     * @param widgetInfo An object containing the widget ID, and the associated watch ID.
+     */
+    protected fun finishWidgetConfig(widgetInfo: WatchBatteryWidgetId) {
+        Timber.d("finishWidgetConfig(${widgetInfo.widgetId}) called")
+        setResult(RESULT_OK, resultIntent)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            widgetDatabase.watchBatteryWidgetDao().addWidget(widgetInfo)
+            WatchBatteryWidget.updateWidgets(
+                this@BaseWidgetConfigActivity,
+                intArrayOf(widgetInfo.widgetId)
+            )
+            finish()
+        }
+    }
+}
