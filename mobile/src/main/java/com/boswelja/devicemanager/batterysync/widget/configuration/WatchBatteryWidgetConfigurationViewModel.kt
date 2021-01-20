547e2e6102165438779c23f5b4f42ee6bb428d7d
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-01-20 21:06:55 +1300

    Remove unused function

--- mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationViewModel.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationViewModel.kt
@@ -2,13 +2,7 @@ package com.boswelja.devicemanager.batterysync.widget.configuration
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
-import com.boswelja.devicemanager.batterysync.widget.WatchBatteryWidget
 import com.boswelja.devicemanager.watchmanager.database.WatchDatabase
-import com.boswelja.devicemanager.widget.database.WatchBatteryWidgetId
-import com.boswelja.devicemanager.widget.database.WidgetDatabase
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 
 class WatchBatteryWidgetConfigurationViewModel internal constructor(
     application: Application,
@@ -19,17 +13,4 @@ class WatchBatteryWidgetConfigurationViewModel internal constructor(
     constructor(application: Application) : this(application, WatchDatabase.get(application))
 
     val allRegisteredWatches = watchDatabase.watchDao().getAllObservable()
-
-    fun addWidgetToDatabase(watchBatteryWidgetId: WatchBatteryWidgetId): Job {
-        return viewModelScope.launch {
-            WidgetDatabase.open(getApplication()).also {
-                it.watchBatteryWidgetDao().addWidget(watchBatteryWidgetId)
-                it.close()
-            }
-            WatchBatteryWidget.updateWidgets(
-                getApplication(),
-                intArrayOf(watchBatteryWidgetId.widgetId)
-            )
-        }
-    }
 }
