46b0f0a4e98b387d277a70beb9725b4ef0dc3189
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-01-21 21:28:56 +1300

    Override equals and hashCode for WatchWidgetAssociation.kt

--- mobile/src/main/java/com/boswelja/devicemanager/widget/database/WatchWidgetAssociation.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/widget/database/WatchWidgetAssociation.kt
@@ -7,4 +7,15 @@ abstract class WatchWidgetAssociation(
     @ColumnInfo(name = "watch_id")
     val watchId: String,
     @PrimaryKey val widgetId: Int
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other !is WatchWidgetAssociation) return false
+        return other.watchId == watchId && other.widgetId == widgetId
+    }
+
+    override fun hashCode(): Int {
+        var result = watchId.hashCode()
+        result = 31 * result + widgetId
+        return result
+    }
+}
