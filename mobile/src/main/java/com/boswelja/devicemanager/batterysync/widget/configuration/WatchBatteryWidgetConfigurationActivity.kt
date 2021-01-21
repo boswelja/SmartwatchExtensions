150a496d3f46dbe7665b33b4da89120ca3c4b234
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-01-21 13:56:49 +1300

    Make use of RadioButtons for WatchBatteryWidgetConfigurationActivity.kt instead of a RecyclerView I'm not expecting this to cause any performance issues

--- mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationActivity.kt
@@ -8,38 +8,73 @@
 package com.boswelja.devicemanager.batterysync.widget.configuration
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.ViewGroup
 import androidx.activity.viewModels
 import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.common.ui.BaseWidgetConfigActivity
-import com.boswelja.devicemanager.common.ui.adapter.WatchAdapter
 import com.boswelja.devicemanager.databinding.ActivityWatchBatteryWidgetConfigurationBinding
+import com.boswelja.devicemanager.watchmanager.item.Watch
 import com.boswelja.devicemanager.widget.database.WatchBatteryWidgetId
+import com.google.android.material.radiobutton.MaterialRadioButton
 import timber.log.Timber
 
 class WatchBatteryWidgetConfigurationActivity : BaseWidgetConfigActivity() {
 
     private val viewModel: WatchBatteryWidgetConfigurationViewModel by viewModels()
-    private val adapter by lazy {
-        WatchAdapter {
-            finishWidgetConfig(WatchBatteryWidgetId(it.id, widgetId))
-        }
-    }
 
     private lateinit var binding: ActivityWatchBatteryWidgetConfigurationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Create the layout binding
         binding = ActivityWatchBatteryWidgetConfigurationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set up toolbar
         setupToolbar(binding.toolbarLayout.toolbar, showTitle = true, showUpButton = true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
 
-        binding.watchPickerRecyclerView.adapter = adapter
+        // Disable the done button when nothing is selected
+        binding.availableWatchesGroup.setOnCheckedChangeListener { _, checkedId ->
+            binding.finishButton.isEnabled = checkedId > -1
+        }
+
+        // Observe registered watches and keep UI populated
         viewModel.allRegisteredWatches.observe(this) {
-            Timber.d("Got ${it.count()} watches")
-            adapter.submitList(it)
+            populateAvailableWatches(it)
+        }
+
+        // Set up finish button
+        binding.finishButton.setOnClickListener {
+            // Get selected watch
+            val watch =
+                viewModel.getWatchByIndex(binding.availableWatchesGroup.checkedRadioButtonId)
+            watch?.let {
+                finishWidgetConfig(WatchBatteryWidgetId(watch.id, widgetId))
+            }
+        }
+    }
+
+    /**
+     * Populates the view with available watches to pick from
+     */
+    private fun populateAvailableWatches(watches: List<Watch>) {
+        Timber.d("populateAvailableWatches() called")
+        binding.availableWatchesGroup.apply {
+            removeAllViews()
+            val layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            watches.forEachIndexed { index, watch ->
+                val radioButton = MaterialRadioButton(context)
+                radioButton.id = index
+                radioButton.text = watch.name
+                radioButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                addView(radioButton, layoutParams)
+            }
         }
     }
 }
--- mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationViewModel.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/batterysync/widget/configuration/WatchBatteryWidgetConfigurationViewModel.kt
@@ -3,6 +3,7 @@ package com.boswelja.devicemanager.batterysync.widget.configuration
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.boswelja.devicemanager.watchmanager.database.WatchDatabase
+import com.boswelja.devicemanager.watchmanager.item.Watch
 
 class WatchBatteryWidgetConfigurationViewModel internal constructor(
     application: Application,
@@ -13,4 +14,8 @@ class WatchBatteryWidgetConfigurationViewModel internal constructor(
     constructor(application: Application) : this(application, WatchDatabase.get(application))
 
     val allRegisteredWatches = watchDatabase.watchDao().getAllObservable()
+
+    fun getWatchByIndex(index: Int): Watch? {
+        return allRegisteredWatches.value?.get(index)
+    }
 }
