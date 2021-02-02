05a1ba70214a0cb9966bcedbe9f2565f1dba18f7
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-02 21:45:47 +1300

    Add another test to WatchRepositoryTest.kt

--- mobile/src/test/java/com/boswelja/devicemanager/watchmanager/WatchRepositoryTest.kt
+++ mobile/src/test/java/com/boswelja/devicemanager/watchmanager/WatchRepositoryTest.kt
@@ -5,11 +5,14 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.boswelja.devicemanager.watchmanager.connection.DummyConnectionInterface
-import com.boswelja.devicemanager.watchmanager.connection.WearOSConnectionInterface
 import com.boswelja.devicemanager.watchmanager.database.WatchDatabase
 import com.boswelja.devicemanager.watchmanager.item.Watch
 import com.google.common.truth.Truth.assertThat
+import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.spyk
+import io.mockk.verify
+import io.mockk.verifyAll
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -20,9 +23,9 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.R])
 class WatchRepositoryTest {
 
-    private val dummyWatch1 = Watch("an-id-1234", "Watch 1", WearOSConnectionInterface.PLATFORM)
-    private val dummyWatch2 = Watch("an-id-2345", "Watch 2", WearOSConnectionInterface.PLATFORM)
-    private val dummyWatch3 = Watch("an-id-3456", "Watch 3", WearOSConnectionInterface.PLATFORM)
+    private val dummyWatch1 = Watch("an-id-1234", "Watch 1", DummyConnectionInterface.PLATFORM)
+    private val dummyWatch2 = Watch("an-id-2345", "Watch 2", DummyConnectionInterface.PLATFORM)
+    private val dummyWatch3 = Watch("an-id-3456", "Watch 3", DummyConnectionInterface.PLATFORM)
     private val dummyWatches = listOf(dummyWatch1, dummyWatch2, dummyWatch3)
 
     private lateinit var repository: WatchRepository
@@ -64,5 +67,27 @@ class WatchRepositoryTest {
         val newWatches = dummyWatches.drop(1)
         val result = repository.replaceForPlatform(dummyWatches, newWatches)
         assertThat(result).containsExactlyElementsIn(newWatches)
+    }
+
+    @Test
+    fun `updateStatusForPlatform calls getStatus on the correct watches`() {
+        // Change the returning status
+        every { connectionInterface.getWatchStatus(any(), any()) } returns Watch.Status.CONNECTED
+        // Ensure we've verfied any calls to connectionInterface before testing
+        verifyAll {
+            connectionInterface.availableWatches
+            connectionInterface.dataChanged
+            connectionInterface.getPlatformIdentifier()
+        }
+        val result =
+            repository.updateStatusForPlatform(dummyWatches, DummyConnectionInterface.PLATFORM)
+        dummyWatches.forEach {
+            verify(exactly = 1) { connectionInterface.getWatchStatus(it, any()) }
+        }
+        // Ensure no extra calls to getWatchStatus were made
+        confirmVerified(connectionInterface)
+        result.filter { it.platform == DummyConnectionInterface.PLATFORM }.forEach {
+            assertThat(it.status).isEquivalentAccordingToCompareTo(Watch.Status.CONNECTED)
+        }
     }
 }
