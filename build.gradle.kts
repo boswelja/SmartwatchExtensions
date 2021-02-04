ec62c424ac7f386338aa3f41b691070b8ab5d05e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-04 19:00:32 +1300

    Update top-level Gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha05")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.google.gms:google-services:4.3.4")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2")
+        classpath("com.google.gms:google-services:4.3.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
     }
 }
 
