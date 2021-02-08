6efce2ea78838bbf04c3a700888752c9c760361e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-08 21:01:11 +1300

    Create and implement ClearCacheBottomSheet.kt

---
+++ mobile/src/main/java/com/boswelja/devicemanager/managespace/ui/ClearCacheBottomSheet.kt
@@ -0,0 +1,51 @@
+package com.boswelja.devicemanager.managespace.ui
+
+import androidx.core.content.ContextCompat
+import com.boswelja.devicemanager.R
+
+class ClearCacheBottomSheet : BaseResetBottomSheet() {
+
+    private var successful = false
+
+    override fun onCreateConfirmation(showProgress: () -> Unit): ConfirmationFragment {
+        return ConfirmationFragment(
+            getString(R.string.manage_space_clear_cache_title),
+            getString(R.string.manage_space_clear_cache_desc),
+            getString(R.string.dialog_button_reset),
+            ContextCompat.getDrawable(requireContext(), R.drawable.ic_chevron_right)!!,
+            showProgress
+        )
+    }
+
+    override fun onCreateProgress(): ProgressFragment {
+        return ProgressFragment(
+            getString(R.string.clear_cache_clearing),
+            getString(R.string.please_wait)
+        )
+    }
+
+    override fun onCreateResult(dismissSheet: () -> Unit): ConfirmationFragment {
+        val title = if (successful)
+            getString(R.string.clear_cache_success)
+        else
+            getString(R.string.clear_cache_failed)
+
+        return ConfirmationFragment(
+            title,
+            "",
+            getString(R.string.button_done),
+            ContextCompat.getDrawable(requireContext(), R.drawable.ic_check)!!,
+            dismissSheet
+        )
+    }
+
+    override fun onStartWork() {
+        viewModel.clearCache(
+            { setProgress(it) },
+            {
+                successful = it
+                setProgress(100)
+            }
+        )
+    }
+}
