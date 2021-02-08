2d1e24ed51349015e1edbc7061c49a4bcee16153
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-08 22:19:23 +1300

    Tidy up ManageSpaceActivity.kt

--- mobile/src/main/java/com/boswelja/devicemanager/managespace/ui/ManageSpaceActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/managespace/ui/ManageSpaceActivity.kt
@@ -1,22 +1,20 @@
-/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
- *
- * This file is part of Wearable Extensions
- *
- * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
- * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
- */
 package com.boswelja.devicemanager.managespace.ui
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.boswelja.devicemanager.common.ui.BaseToolbarActivity
 import com.boswelja.devicemanager.databinding.ActivityManageSpaceBinding
 
+/**
+ * An activity to present the user with options for resetting various data used by Wearable
+ * Extensions.
+ */
 class ManageSpaceActivity : BaseToolbarActivity() {
 
     private lateinit var binding: ActivityManageSpaceBinding
 
-    private val viewModel: ManageSpaceViewModel by viewModels()
+    private val clearCacheSheet by lazy { ClearCacheBottomSheet() }
+    private val resetSettingsSheet by lazy { ResetSettingsBottomSheet() }
+    private val resetAppSheet by lazy { ResetAppBottomSheet() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,37 +23,26 @@ class ManageSpaceActivity : BaseToolbarActivity() {
         setContentView(binding.root)
 
         setupToolbar(binding.toolbarLayout.toolbar, showTitle = true, showUpButton = true)
-        setupButtons()
-        viewModel
-    }
 
-    /** Initialise all the buttons we need. */
-    private fun setupButtons() {
         binding.apply {
-            clearCacheButton.setOnClickListener { clearCache() }
-            resetSettingsButton.setOnClickListener { doSettingsReset() }
-            resetAppButton.setOnClickListener { doFullReset() }
+            clearCacheButton.setOnClickListener { showClearCacheSheet() }
+            resetSettingsButton.setOnClickListener { showResetSettingsSheet() }
+            resetAppButton.setOnClickListener { showResetAppSheet() }
         }
     }
 
-    /** Attempts to clear all of Wearable Extension's cache. */
-    private fun clearCache() {
-        ClearCacheBottomSheet()
-            .show(supportFragmentManager, ClearCacheBottomSheet::class.simpleName)
+    /** Shows a [ClearCacheBottomSheet]. */
+    private fun showClearCacheSheet() {
+        clearCacheSheet.show(supportFragmentManager, clearCacheSheet::class.simpleName)
     }
 
-    /** Attempts to reset settings for each watch registered with Wearable Extensions. */
-    private fun doSettingsReset() {
-        ResetSettingsBottomSheet().show(supportFragmentManager, "ResetSettingsBottomSheet")
+    /** Shows a [ResetSettingsBottomSheet]. */
+    private fun showResetSettingsSheet() {
+        resetSettingsSheet.show(supportFragmentManager, resetSettingsSheet::class.simpleName)
     }
 
-    /** Attempts a full reset of Wearable Extensions. */
-    private fun doFullReset() {
-        ResetAppBottomSheet().show(supportFragmentManager, "ResetAppBottomSheet")
-    }
-
-    companion object {
-        private const val DATABASE_COUNT = 3
-        private const val PREFERENCE_STORE_COUNT = 1
+    /** Shows a [ResetAppBottomSheet]. */
+    private fun showResetAppSheet() {
+        resetAppSheet.show(supportFragmentManager, resetAppSheet::class.simpleName)
     }
 }
