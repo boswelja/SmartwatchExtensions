3d956db8dd8fe52ce561d16e6ce573913137df05
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-08 22:07:08 +1300

    Create and implement ResetAppBottomSheet.kt As a result, a lot of code in ManageSpaceActivity.kt was made redundant. It has been removed.

--- mobile/src/main/java/com/boswelja/devicemanager/managespace/ui/ManageSpaceActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/managespace/ui/ManageSpaceActivity.kt
@@ -7,44 +7,20 @@
  */
 package com.boswelja.devicemanager.managespace.ui
 
-import android.app.ActivityManager
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.edit
-import androidx.preference.PreferenceManager
-import com.boswelja.devicemanager.R
-import com.boswelja.devicemanager.analytics.Analytics
-import com.boswelja.devicemanager.batterysync.BatterySyncWorker
-import com.boswelja.devicemanager.batterysync.database.WatchBatteryStatsDatabase
 import com.boswelja.devicemanager.common.ui.BaseToolbarActivity
 import com.boswelja.devicemanager.databinding.ActivityManageSpaceBinding
-import com.boswelja.devicemanager.watchmanager.WatchManager
-import com.boswelja.devicemanager.watchmanager.database.WatchDatabase
-import com.boswelja.devicemanager.watchmanager.item.Watch
-import com.boswelja.devicemanager.widget.database.WidgetDatabase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class ManageSpaceActivity : BaseToolbarActivity() {
 
-    private val coroutineScope = MainScope()
-
     private lateinit var binding: ActivityManageSpaceBinding
-    private lateinit var activityManager: ActivityManager
 
     private val viewModel: ManageSpaceViewModel by viewModels()
-    private val analytics: Analytics by lazy { Analytics() }
-    private val watchManager: WatchManager by lazy { WatchManager.getInstance(this) }
-    private var hasResetApp = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         binding = ActivityManageSpaceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -53,81 +29,13 @@ class ManageSpaceActivity : BaseToolbarActivity() {
         viewModel
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (hasResetApp) {
-            clearData()
-        }
-    }
-
     /** Initialise all the buttons we need. */
     private fun setupButtons() {
         binding.apply {
             clearCacheButton.setOnClickListener { clearCache() }
             resetSettingsButton.setOnClickListener { doSettingsReset() }
-            resetAppButton.setOnClickListener { showFullResetConfirmation() }
+            resetAppButton.setOnClickListener { doFullReset() }
         }
-    }
-
-    /**
-     * Sets whether the user can interact with any of the buttons on screen.
-     * @param enabled Whether the user can interact with buttons.
-     */
-    private fun setButtonsEnabled(enabled: Boolean) {
-        binding.apply {
-            clearCacheButton.isEnabled = enabled
-            resetSettingsButton.isEnabled = enabled
-            resetAppButton.isEnabled = enabled
-        }
-    }
-
-    /**
-     * Prepares the progress bar for use again. This resets progress to 0 and sets a new step count.
-     */
-    private fun initProgressBar(stepCount: Int) {
-        binding.apply {
-            progressBar.max = stepCount
-            progressBar.progress = 0
-        }
-    }
-
-    /** Increments the progress bar by 1. */
-    private fun incrementProgressBar() {
-        binding.progressBar.progress += 1
-    }
-
-    /**
-     * Updates the progress status text.
-     * @param statusText The new [String] to use for progress status.
-     */
-    private fun setProgressStatus(statusText: String) {
-        binding.progressStatus.text = statusText
-    }
-
-    /**
-     * Shows a custom [AlertDialog] to confirm the user would like to fully reset Wearable
-     * Extensions. If the user chooses to reset Wearable Extensions, the reset will be started from
-     * here.
-     */
-    private fun showFullResetConfirmation() {
-        AlertDialog.Builder(this)
-            .apply {
-                setTitle(R.string.dialog_reset_app_title)
-                setMessage(R.string.dialog_reset_app_message)
-                setPositiveButton(R.string.dialog_button_reset) { _, _ -> doFullReset() }
-                setNegativeButton(R.string.dialog_button_cancel) { dialog, _ -> dialog.cancel() }
-            }
-            .show()
-    }
-
-    /**
-     * Calls the system's [ActivityManager] clearApplicationUserData() to make sure we haven't
-     * missed anything.
-     * @return true if successful, false otherwise.
-     */
-    private fun clearData(): Boolean {
-        analytics.logStorageManagerAction("clearData")
-        return activityManager.clearApplicationUserData()
     }
 
     /** Attempts to clear all of Wearable Extension's cache. */
@@ -143,94 +51,7 @@ class ManageSpaceActivity : BaseToolbarActivity() {
 
     /** Attempts a full reset of Wearable Extensions. */
     private fun doFullReset() {
-        setButtonsEnabled(false)
-        coroutineScope.launch(Dispatchers.IO) {
-            analytics.logStorageManagerAction("fullReset")
-            var totalSteps = DATABASE_COUNT + PREFERENCE_STORE_COUNT
-            initProgressBar(totalSteps)
-            val registeredWatches = watchManager.registeredWatches.value!!
-            if (registeredWatches.isNotEmpty()) {
-                totalSteps += (registeredWatches.count() * 3)
-                withContext(Dispatchers.Main) { initProgressBar(totalSteps) }
-                for (watch in registeredWatches) {
-                    withContext(Dispatchers.Main) {
-                        setProgressStatus(getString(R.string.reset_app_resetting_for, watch.name))
-                    }
-                    resetWatchPreferences(watch)
-                    withContext(Dispatchers.Main) { incrementProgressBar() }
-                    try {
-                        watchManager.requestResetWatch(watch)
-                        withContext(Dispatchers.Main) { incrementProgressBar() }
-                    } catch (e: Exception) {
-                        withContext(Dispatchers.Main) {
-                            initProgressBar(0)
-                            setProgressStatus(getString(R.string.reset_app_failed_for, watch.name))
-                            setButtonsEnabled(true)
-                        }
-                        return@launch
-                    }
-                    watchManager.forgetWatch(watch)
-                    withContext(Dispatchers.Main) { incrementProgressBar() }
-                }
-            }
-            withContext(Dispatchers.Main) {
-                setProgressStatus(
-                    getString(
-                        R.string.reset_app_resetting_database_for,
-                        getString(R.string.database_name_battery_stats)
-                    )
-                )
-            }
-            WatchBatteryStatsDatabase.get(this@ManageSpaceActivity).clearAllTables()
-            withContext(Dispatchers.Main) {
-                incrementProgressBar()
-                setProgressStatus(
-                    getString(
-                        R.string.reset_app_resetting_database_for,
-                        getString(R.string.database_name_widget)
-                    )
-                )
-            }
-            WidgetDatabase.open(this@ManageSpaceActivity).apply { clearAllTables() }.also {
-                it.close()
-            }
-            withContext(Dispatchers.Main) {
-                incrementProgressBar()
-                setProgressStatus(
-                    getString(
-                        R.string.reset_app_resetting_database_for,
-                        getString(R.string.database_name_main)
-                    )
-                )
-            }
-            WatchDatabase.getInstance(this@ManageSpaceActivity).apply { clearAllTables() }.also {
-                it.close()
-            }
-            withContext(Dispatchers.Main) {
-                incrementProgressBar()
-                setProgressStatus(getString(R.string.reset_app_resetting_preferences))
-            }
-            PreferenceManager.getDefaultSharedPreferences(this@ManageSpaceActivity).edit(
-                commit = true
-            ) { clear() }
-            withContext(Dispatchers.Main) {
-                incrementProgressBar()
-                setProgressStatus(getString(R.string.reset_app_success))
-                setButtonsEnabled(true)
-            }
-            hasResetApp = true
-        }
-    }
-
-    /**
-     * Resets preferences for a given [Watch], and stops any running services or workers.
-     * @param watch The [Watch] to clear preferences for.
-     */
-    private fun resetWatchPreferences(watch: Watch) {
-        coroutineScope.launch {
-            watchManager.resetWatchPreferences(watch)
-            BatterySyncWorker.stopWorker(this@ManageSpaceActivity, watch.id)
-        }
+        ResetAppBottomSheet().show(supportFragmentManager, "ResetAppBottomSheet")
     }
 
     companion object {
