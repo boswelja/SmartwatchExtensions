957d4ce00391e5a3956fb351568c5ddb3296905c
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-09 19:59:20 +1300

    Create basic UI tests for ActionFragments

---
+++ mobile/src/androidTest/java/com/boswelja/devicemanager/managespace/ui/actions/ResetExtensionsActionFragmentTest.kt
@@ -0,0 +1,28 @@
+package com.boswelja.devicemanager.managespace.ui.actions
+
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.boswelja.devicemanager.R
+import org.junit.Before
+import org.junit.Test
+
+class ResetExtensionsActionFragmentTest {
+
+    private lateinit var scenario: FragmentScenario<ResetExtensionsActionFragment>
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_App)
+    }
+
+    @Test
+    fun uiIsCompletelyDisplayed() {
+        onView(withId(R.id.title)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.desc)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.button)).check(matches(isCompletelyDisplayed()))
+    }
+}
