a37821e515123414d8330e94556276d4bb8055c7
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-11 15:31:51 +1300

    Create bottom sheet UI tests

---
+++ mobile/src/androidTest/java/com/boswelja/devicemanager/managespace/ui/sheets/ClearCacheBottomSheetTest.kt
@@ -0,0 +1,53 @@
+package com.boswelja.devicemanager.managespace.ui.sheets
+
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.boswelja.devicemanager.R
+import org.junit.Before
+import org.junit.Test
+
+class ClearCacheBottomSheetTest {
+
+    private lateinit var scenario: FragmentScenario<ClearCacheBottomSheet>
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_App)
+    }
+
+    @Test
+    fun confirmationUiIsCompletelyDisplayed() {
+        // Sheet should start on confirmation fragment, no need to do anything
+        onView(withId(R.id.title)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.desc)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.button)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun progressUiIsCompletelyDisplayed() {
+        // Sheet starts on confirmation fragment, need to navigate to progress fragment
+        scenario.onFragment {
+            it.showProgress()
+        }
+
+        onView(withId(R.id.title)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.desc)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.progress)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun resultsUiIsCompletelyDisplayed() {
+        // Sheet starts on confirmation fragment, need to navigate to result fragment
+        scenario.onFragment {
+            it.showResults()
+        }
+
+        onView(withId(R.id.title)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.desc)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.button)).check(matches(isCompletelyDisplayed()))
+    }
+}
