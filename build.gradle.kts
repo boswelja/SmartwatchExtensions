e5462a456d2c46f221781ecb27b9fb177d2b261a
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-12 15:05:16 +1300

    Update Gradle and Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-    val kotlinVersion = "1.4.21"
+    val kotlinVersion = "1.4.30"
 
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha05")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha06")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.5")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
