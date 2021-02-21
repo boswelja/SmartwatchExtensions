2b659d4c7b657ff24abffb56ba9aa4305135c118
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-21 17:23:33 +1300

    Rewrite RegisterWatchFragmentTest.kt to mock viewModel instead of watchManager Removes another variable that we already have tests for

--- mobile/src/androidTest/java/com/boswelja/devicemanager/watchmanager/ui/register/RegisterWatchFragmentTest.kt
+++ mobile/src/androidTest/java/com/boswelja/devicemanager/watchmanager/ui/register/RegisterWatchFragmentTest.kt
@@ -1,5 +1,6 @@
 package com.boswelja.devicemanager.watchmanager.ui.register
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
@@ -14,38 +15,51 @@ import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.TestExtensions.getOrAwaitValue
-import com.boswelja.devicemanager.watchmanager.WatchManager
 import com.boswelja.devicemanager.watchmanager.connection.wearos.WearOSConnectionInterface
 import com.boswelja.devicemanager.watchmanager.item.Watch
 import com.google.common.truth.Truth.assertThat
 import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.mockkObject
+import io.mockk.mockkConstructor
+import io.mockk.verify
 import org.hamcrest.Matchers.not
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class RegisterWatchFragmentTest {
+
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
 
     private val dummyWatch1 = Watch("an-id-1234", "Watch 1", WearOSConnectionInterface.PLATFORM)
     private val dummyWatch2 = Watch("an-id-2345", "Watch 2", WearOSConnectionInterface.PLATFORM)
     private val dummyWatch3 = Watch("an-id-3456", "Watch 3", WearOSConnectionInterface.PLATFORM)
     private val dummyWatches = listOf(dummyWatch1, dummyWatch2, dummyWatch3)
 
-    @RelaxedMockK
-    private lateinit var watchManager: WatchManager
-
     private lateinit var fragmentScenario: FragmentScenario<RegisterWatchFragment>
     private lateinit var availableWatches: MutableLiveData<List<Watch>>
+    private lateinit var registeredWatches: MutableLiveData<List<Watch>>
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
         availableWatches = MutableLiveData()
-        mockkObject(WatchManager.Companion)
-        every { WatchManager.Companion.getInstance(any()) } returns watchManager
-        every { watchManager.availableWatches } returns availableWatches
+        registeredWatches = MutableLiveData()
+
+        mockkConstructor(RegisterWatchViewModel::class)
+        every {
+            anyConstructed<RegisterWatchViewModel>().availableWatches
+        } returns availableWatches
+        every {
+            anyConstructed<RegisterWatchViewModel>().registeredWatches
+        } returns registeredWatches
+        every {
+            anyConstructed<RegisterWatchViewModel>().registerWatch(any())
+        } answers { }
+        every {
+            anyConstructed<RegisterWatchViewModel>().refreshData()
+        } answers { }
 
         fragmentScenario = launchFragmentInContainer(themeResId = R.style.Theme_App)
     }
@@ -60,6 +74,11 @@ class RegisterWatchFragmentTest {
 
     @Test
     fun finishButtonFiresViewModelEvent() {
+        // Need to register watches to enable the button
+        fragmentScenario.onFragment {
+            registeredWatches.postValue(dummyWatches)
+        }
+
         onView(withId(R.id.finish_button)).perform(click())
         fragmentScenario.onFragment {
             assertThat(
@@ -69,70 +88,57 @@ class RegisterWatchFragmentTest {
     }
 
     @Test
-    fun noWatchesViewShownWhenNoWatches() {
-        // Test with availableWatches empty
+    fun noWatchesViewShownWhenNoWatchesRegistered() {
+        // Test with registeredWatches empty
         fragmentScenario.onFragment {
-            availableWatches.postValue(emptyList())
+            registeredWatches.postValue(emptyList())
         }
         onView(withId(R.id.no_watches_text)).check(matches(isCompletelyDisplayed()))
 
-        // Populate availableWatches and let fragment populate registeredWatches
+        // Populate registeredWatches
         fragmentScenario.onFragment {
-            availableWatches.postValue(dummyWatches)
-            it.activityViewModels<RegisterWatchViewModel>().value
-                .registeredWatches.getOrAwaitValue()
+            registeredWatches.postValue(dummyWatches)
         }
+
         onView(withId(R.id.no_watches_text))
-            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
     }
 
     @Test
-    fun finishButtonDisabledWithNoRegisteredWatches() {
-        // Test with availableWatches empty
+    fun finishButtonEnabledWithRegisteredWatches() {
+        // Test with registeredWatches empty
         fragmentScenario.onFragment {
-            availableWatches.postValue(emptyList())
+            registeredWatches.postValue(emptyList())
         }
         onView(withId(R.id.finish_button)).check(matches(not(isEnabled())))
 
-        // Populate availableWatches and let fragment populate registeredWatches
+        // Populate registeredWatches and let fragment populate registeredWatches
         fragmentScenario.onFragment {
-            availableWatches.postValue(dummyWatches)
-            it.activityViewModels<RegisterWatchViewModel>().value
-                .registeredWatches.getOrAwaitValue()
+            registeredWatches.postValue(dummyWatches)
         }
         onView(withId(R.id.finish_button)).check(matches(isEnabled()))
     }
 
     @Test
-    fun availableWatchesGetRegistered() {
+    fun availableWatchesAreRegistered() {
         // Test with availableWatches empty
         fragmentScenario.onFragment {
             availableWatches.postValue(emptyList())
         }
-        fragmentScenario.onFragment {
-            availableWatches.postValue(dummyWatches)
-            assertThat(
-                it.activityViewModels<RegisterWatchViewModel>().value
-                    .registeredWatches.getOrAwaitValue()
-            ).isEmpty()
-        }
+        verify(inverse = true) { anyConstructed<RegisterWatchViewModel>().registerWatch(any()) }
 
         // Populate availableWatches with one watch
         fragmentScenario.onFragment {
             availableWatches.postValue(listOf(dummyWatch1))
-            assertThat(
-                it.activityViewModels<RegisterWatchViewModel>().value
-                    .registeredWatches.getOrAwaitValue()
-            ).containsExactly(dummyWatch1)
         }
+        verify { anyConstructed<RegisterWatchViewModel>().registerWatch(dummyWatch1) }
 
         // Populate availableWatches with more watches
         fragmentScenario.onFragment {
             availableWatches.postValue(dummyWatches)
-            assertThat(
-                it.activityViewModels<RegisterWatchViewModel>().value
-                    .registeredWatches.getOrAwaitValue()
-            ).containsExactlyElementsIn(dummyWatches)
+        }
+        dummyWatches.forEach { watch ->
+            verify { anyConstructed<RegisterWatchViewModel>().registerWatch(watch) }
         }
     }
 }
