f32f07e1b02cba56d6547d933907e1d560e8d008
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-25 18:10:57 +1300

    Implement automatic watch switching First steps to being able to select a watch from within App Manager

--- mobile/src/main/java/com/boswelja/devicemanager/appmanager/ui/AppManagerViewModel.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/appmanager/ui/AppManagerViewModel.kt
@@ -1,33 +1,31 @@
-/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
- *
- * This file is part of Wearable Extensions
- *
- * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
- * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
- */
 package com.boswelja.devicemanager.appmanager.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.boswelja.devicemanager.appmanager.State
 import com.boswelja.devicemanager.common.DelayedFunction
 import com.boswelja.devicemanager.common.appmanager.App
 import com.boswelja.devicemanager.common.appmanager.Messages
+import com.boswelja.devicemanager.watchmanager.WatchManager
+import com.boswelja.devicemanager.watchmanager.item.Watch
 import com.google.android.gms.wearable.MessageClient
 import com.google.android.gms.wearable.Wearable
 import timber.log.Timber
 
 class AppManagerViewModel internal constructor(
     application: Application,
-    private val messageClient: MessageClient
+    private val messageClient: MessageClient,
+    private val watchManager: WatchManager
 ) : AndroidViewModel(application) {
 
     @Suppress("unused")
     constructor(application: Application) : this(
         application,
-        Wearable.getMessageClient(application)
+        Wearable.getMessageClient(application),
+        WatchManager.getInstance(application)
     )
 
     private val _state = MutableLiveData(State.CONNECTING)
@@ -36,22 +34,35 @@ class AppManagerViewModel internal constructor(
     private val _apps = ArrayList<App>()
 
     private val messageListener = MessageClient.OnMessageReceivedListener {
-        when (it.path) {
-            // Package change messages
-            Messages.PACKAGE_ADDED -> addPackage(App.fromByteArray(it.data))
-            Messages.PACKAGE_UPDATED -> updatePackage(App.fromByteArray(it.data))
-            Messages.PACKAGE_REMOVED -> removePackage(App.fromByteArray(it.data))
+        if (it.sourceNodeId == watchId) {
+            when (it.path) {
+                // Package change messages
+                Messages.PACKAGE_ADDED -> addPackage(App.fromByteArray(it.data))
+                Messages.PACKAGE_UPDATED -> updatePackage(App.fromByteArray(it.data))
+                Messages.PACKAGE_REMOVED -> removePackage(App.fromByteArray(it.data))
 
-            // Service state messages
-            Messages.SERVICE_RUNNING -> serviceRunning()
+                // Service state messages
+                Messages.SERVICE_RUNNING -> serviceRunning()
 
-            else -> Timber.w("Unknown path received, ignoring")
+                else -> Timber.w("Unknown path received, ignoring")
+            }
         }
+    }
+
+    private val selectedWatchObserver = Observer<Watch?> {
+        _state.postValue(State.CONNECTING)
+        messageClient.sendMessage(watchId!!, Messages.STOP_SERVICE, null)
+        _apps.clear()
+        _appsObservable.postValue(_apps)
+        watchId = it.id
+        startAppManagerService()
     }
 
     private val stateDisconnectedDelay = DelayedFunction(15) {
         _state.postValue(State.DISCONNECTED)
     }
+
+    private var watchId: String? = null
 
     /**
      * The current [State] of the App Manager.
@@ -63,15 +74,16 @@ class AppManagerViewModel internal constructor(
         get() = _appsObservable
 
     var canStopAppManagerService: Boolean = true
-    var watchId: String? = null
 
     init {
         messageClient.addListener(messageListener)
+        watchManager.selectedWatch.observeForever(selectedWatchObserver)
     }
 
     override fun onCleared() {
         super.onCleared()
         messageClient.removeListener(messageListener)
+        watchManager.selectedWatch.removeObserver(selectedWatchObserver)
     }
 
     /** Start the App Manager service on the connected watch. */
@@ -80,6 +92,7 @@ class AppManagerViewModel internal constructor(
         if (_state.value != State.READY) {
             Timber.i("Trying to start App Manager service")
             messageClient.sendMessage(watchId!!, Messages.START_SERVICE, null)
+            stateDisconnectedDelay.reset()
         }
     }
 
