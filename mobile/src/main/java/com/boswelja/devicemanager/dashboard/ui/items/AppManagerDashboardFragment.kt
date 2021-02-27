09f2e34b34c35b6de9aec84c210c01a2c6138eac
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-02-27 15:18:42 +1300

    Fix AppManagerDashboardFragment.kt

--- mobile/src/main/java/com/boswelja/devicemanager/dashboard/ui/items/AppManagerDashboardFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/dashboard/ui/items/AppManagerDashboardFragment.kt
@@ -5,24 +5,13 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.dashboard.ui.DashboardFragmentDirections
-import com.boswelja.devicemanager.watchmanager.WatchManager
 
 class AppManagerDashboardFragment : BaseDashboardItemFragment() {
 
-    private val watchManager by lazy { WatchManager.getInstance(requireContext()) }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupWidget(
-            getString(R.string.main_app_manager_title)
-        ) {
-            watchManager.selectedWatch.value?.let {
-                findNavController().navigate(
-                    DashboardFragmentDirections.toAppManagerActivity(
-                        it.id, it.name
-                    )
-                )
-            }
+        setupWidget(getString(R.string.main_app_manager_title)) {
+            findNavController().navigate(DashboardFragmentDirections.toAppManagerActivity())
         }
     }
 }
