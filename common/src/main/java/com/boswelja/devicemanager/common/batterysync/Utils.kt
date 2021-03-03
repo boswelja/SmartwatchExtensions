38731b948276a8c03bcbc07462ad5e342962f31c
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-03 17:53:02 +1300

    Remove file license headers They're outdated, and completely unnecessary since we have a repo-wide up-to-date license

--- common/src/main/java/com/boswelja/devicemanager/common/batterysync/Utils.kt
+++ common/src/main/java/com/boswelja/devicemanager/common/batterysync/Utils.kt
@@ -1,10 +1,3 @@
-/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
- *
- * This file is part of Wearable Extensions
- *
- * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
- * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
- */
 package com.boswelja.devicemanager.common.batterysync
 
 import android.content.Context
