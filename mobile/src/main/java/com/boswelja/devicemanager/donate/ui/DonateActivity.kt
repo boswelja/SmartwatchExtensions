049bc880d1b8ead5809987d3687dae2f3cdc18f0
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-05 12:58:14 +1300

    Show a message when the user successfully donates Also put donate status in SharedPreferences for possible later use

--- mobile/src/main/java/com/boswelja/devicemanager/donate/ui/DonateActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/donate/ui/DonateActivity.kt
@@ -1,13 +1,18 @@
 package com.boswelja.devicemanager.donate.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.common.ui.activity.BaseToolbarActivity
 import com.boswelja.devicemanager.databinding.ActivityDonateBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class DonateActivity : BaseToolbarActivity() {
+
+    private val viewModel: DonateViewModel by viewModels()
 
     private lateinit var binding: ActivityDonateBinding
 
@@ -35,5 +40,14 @@ class DonateActivity : BaseToolbarActivity() {
                 else -> tab.text = "One-Time"
             }
         }.attach()
+
+        viewModel.onDonated.observe(this) {
+            createSnackBar(getString(R.string.donate_complete))
+            sharedPreferences.edit { putBoolean(HAS_DONATED_KEY, true) }
+        }
+    }
+
+    companion object {
+        private const val HAS_DONATED_KEY = "has_donated"
     }
 }
