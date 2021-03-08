f848e757308d3402056288a4d2360d68f30b6bec
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-08 20:43:45 +1300

    Convert OneTimeDonateFragment.kt and RecurringDonateFragment.kt to composable layouts

--- mobile/src/main/java/com/boswelja/devicemanager/donate/ui/RecurringDonateFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/donate/ui/RecurringDonateFragment.kt
@@ -1,25 +1,47 @@
 package com.boswelja.devicemanager.donate.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.activityViewModels
-import com.boswelja.devicemanager.common.ui.fragment.ListFragment
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.viewmodel.compose.viewModel
 
-class RecurringDonateFragment : ListFragment() {
+class RecurringDonateFragment : Fragment() {
 
-    private val viewModel: DonateViewModel by activityViewModels()
-    private val adapter by lazy {
-        DonateAdapter {
-            viewModel.launchBillingFlow(requireActivity(), it)
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.recyclerView.adapter = adapter
-        viewModel.recurringDonations.observe(viewLifecycleOwner) {
-            isLoading = it.isEmpty()
-            adapter.submitList(it)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                val viewModel: DonateViewModel = viewModel()
+                val isLoading = viewModel.clientConnected.observeAsState()
+                val donateOptions = viewModel.recurringDonations.observeAsState()
+                Column(
+                    Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth()
+                ) {
+                    isLoading.value?.let {
+                        LinearProgressIndicator(Modifier.fillMaxWidth())
+                    }
+                    donateOptions.value?.let { donateOptions ->
+                        DonateList(
+                            donateOptions = donateOptions,
+                            onClick = { viewModel.launchBillingFlow(requireActivity(), it) }
+                        )
+                    }
+                }
+            }
         }
     }
 }
