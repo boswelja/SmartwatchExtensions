ad2cd1aa339583179b5101aa1bee1c44d966f231
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-16 15:36:04 +1300

    Convert WidgetSettingsActivity.kt to Composable UI WidgetSettingsFragment.kt is still using PreferenceFragment, but this is a step in the right direction

--- mobile/src/main/java/com/boswelja/devicemanager/widget/ui/WidgetSettingsFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/widget/ui/WidgetSettingsFragment.kt
@@ -1,17 +1,10 @@
-/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
- *
- * This file is part of Wearable Extensions
- *
- * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
- * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
- */
 package com.boswelja.devicemanager.widget.ui
 
 import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
 import com.boswelja.devicemanager.R
-import com.boswelja.devicemanager.common.ui.activity.BasePreferenceFragment
 
-class WidgetSettingsFragment : BasePreferenceFragment() {
+class WidgetSettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.prefs_widget_settings)
