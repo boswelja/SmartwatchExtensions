e72c3f9ea481f479537a3d2146a95a140d4f8358
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-16 18:25:25 +1300

    Convert DnDSyncPreferenceWidgetFragment.kt to composable UI

--- mobile/src/main/java/com/boswelja/devicemanager/dndsync/ui/DnDSyncPreferenceWidgetFragment.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/dndsync/ui/DnDSyncPreferenceWidgetFragment.kt
@@ -1,32 +1,134 @@
 package com.boswelja.devicemanager.dndsync.ui
 
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.boswelja.devicemanager.databinding.SettingsWidgetDndSyncBinding
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.boswelja.devicemanager.R
+import com.boswelja.devicemanager.common.ui.AppTheme
 
 class DnDSyncPreferenceWidgetFragment : Fragment() {
-
-    private val viewModel: DnDSyncPreferenceWidgetViewModel by viewModels()
-
-    private lateinit var binding: SettingsWidgetDndSyncBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = SettingsWidgetDndSyncBinding.inflate(inflater, container, false)
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            setContent {
+                AppTheme {
+                    DnDSyncSettingsHeader()
+                }
+            }
+        }
     }
+}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.drawableRes.observe(viewLifecycleOwner) {
-            binding.dndSyncStatusIndicator.setImageResource(it)
+@Composable
+fun DnDSyncSettingsHeader() {
+    val viewModel: DnDSyncPreferenceWidgetViewModel = viewModel()
+    val iconRes by viewModel.drawableRes.observeAsState()
+    if (LocalConfiguration.current.orientation == ORIENTATION_PORTRAIT) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(3f)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                ContextCompat
+                    .getDrawable(LocalContext.current, R.drawable.ic_dnd_phone)!!
+                    .toBitmap().asImageBitmap(),
+                null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f),
+                tint = MaterialTheme.colors.onBackground
+            )
+            iconRes?.let {
+                Icon(
+                    painterResource(it),
+                    null,
+                    modifier = Modifier.size(64.dp),
+                    tint = MaterialTheme.colors.onBackground
+                )
+            }
+            Icon(
+                ContextCompat
+                    .getDrawable(LocalContext.current, R.drawable.ic_dnd_watch)!!
+                    .toBitmap().asImageBitmap(),
+                null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f),
+                tint = MaterialTheme.colors.onBackground
+            )
+        }
+    } else {
+        Column(
+            Modifier
+                .fillMaxHeight()
+                .aspectRatio(0.5f)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                ContextCompat
+                    .getDrawable(LocalContext.current, R.drawable.ic_dnd_phone)!!
+                    .toBitmap().asImageBitmap(),
+                null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                tint = MaterialTheme.colors.onBackground
+            )
+            iconRes?.let {
+                Icon(
+                    painterResource(it),
+                    null,
+                    modifier = Modifier.size(40.dp),
+                    tint = MaterialTheme.colors.onBackground
+                )
+            }
+            Icon(
+                ContextCompat
+                    .getDrawable(LocalContext.current, R.drawable.ic_dnd_watch)!!
+                    .toBitmap().asImageBitmap(),
+                null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                tint = MaterialTheme.colors.onBackground
+            )
         }
     }
 }
