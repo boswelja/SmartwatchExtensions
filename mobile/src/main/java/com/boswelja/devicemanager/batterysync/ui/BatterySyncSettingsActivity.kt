148c242d61787eba557b5ceb518ebeeb0f055dd7
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-22 22:47:24 +1300

    Convert BatterySyncPreferenceWidgetFragment.kt to Composable UI

--- mobile/src/main/java/com/boswelja/devicemanager/batterysync/ui/BatterySyncSettingsActivity.kt
+++ mobile/src/main/java/com/boswelja/devicemanager/batterysync/ui/BatterySyncSettingsActivity.kt
@@ -1,23 +1,15 @@
 package com.boswelja.devicemanager.batterysync.ui
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.fragment.app.commit
-import com.boswelja.devicemanager.R
 import com.boswelja.devicemanager.common.ui.AppTheme
 import com.boswelja.devicemanager.common.ui.UpNavigationWatchPickerAppBar
 import com.boswelja.devicemanager.watchmanager.WatchManager
@@ -47,25 +39,7 @@ class BatterySyncSettingsActivity : AppCompatActivity() {
                     }
                 ) {
                     Column {
-                        AndroidView(
-                            factory = {
-                                FrameLayout(it).apply {
-                                    id = R.id.fragment_holder
-                                    layoutParams = ViewGroup.LayoutParams(
-                                        ViewGroup.LayoutParams.MATCH_PARENT,
-                                        ViewGroup.LayoutParams.WRAP_CONTENT
-                                    )
-                                }
-                            },
-                            update = {
-                                supportFragmentManager.commit {
-                                    replace(it.id, BatterySyncPreferenceWidgetFragment())
-                                }
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .wrapContentHeight()
-                        )
+                        BatterySyncSettingsWidget()
                         Divider()
                         BatterySyncSettingsScreen()
                     }
