00aef2078c42f7c6e7a43340ae661ab6a394eb67
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-23 18:24:04 +1300

    Convert LockPhoneFragment.kt to Composable UI

--- wearos/src/main/java/com/boswelja/devicemanager/phonelocking/ui/LockPhoneFragment.kt
+++ wearos/src/main/java/com/boswelja/devicemanager/phonelocking/ui/LockPhoneFragment.kt
@@ -1,43 +1,96 @@
-/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
- *
- * This file is part of Wearable Extensions
- *
- * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
- * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
- */
 package com.boswelja.devicemanager.phonelocking.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PhonelinkLock
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.boswelja.devicemanager.databinding.FragmentLockPhoneBinding
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.boswelja.devicemanager.R
 
 @Suppress("unused")
 class LockPhoneFragment : Fragment() {
 
-    private val viewModel: LockPhoneViewModel by viewModels()
-
-    private lateinit var binding: FragmentLockPhoneBinding
-
+    @ExperimentalMaterialApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentLockPhoneBinding.inflate(inflater, container, false)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.phoneLockingEnabled.observe(viewLifecycleOwner) {
-            if (it) binding.motionLayout.transitionToStart()
-            else binding.motionLayout.transitionToEnd()
+        return ComposeView(requireContext()).apply {
+            setContent {
+                PhoneLockingScreen()
+            }
         }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun PhoneLockingScreen() {
+    val viewModel: LockPhoneViewModel = viewModel()
+    val phoneLockingEnabled by viewModel.phoneLockingEnabled.observeAsState()
+    if (phoneLockingEnabled == true) {
+        val phoneName by viewModel.phoneName.observeAsState()
+        PhoneLockingEnabled(
+            phoneName = phoneName ?: stringResource(R.string.default_phone_name)
+        ) {
+            viewModel.requestLockPhone()
+        }
+    } else {
+        PhoneLockingDisabled()
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun PhoneLockingDisabled() {
+    ListItem(
+        text = { Text(stringResource(R.string.lock_phone_disabled)) },
+        icon = { Icon(Icons.Outlined.PhonelinkLock, null) }
+    )
+}
+
+@Composable
+fun PhoneLockingEnabled(
+    phoneName: String,
+    onClick: () -> Unit
+) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            Icons.Outlined.PhonelinkLock,
+            null,
+            Modifier.size(56.dp)
+        )
+        Text(
+            stringResource(R.string.lock_phone, phoneName),
+            style = MaterialTheme.typography.h6
+        )
     }
 }
