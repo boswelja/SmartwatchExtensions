709a4dfd3f686e43a26af529681f191cfbfd08b1
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-23 14:02:53 +1300

    Remove navigation safe args

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.0-alpha10")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.5")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.4")
     }
 }
 
