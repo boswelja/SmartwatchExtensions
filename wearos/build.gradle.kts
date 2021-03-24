f897bcf7dd6995315b3d8bf2b17cc412d9128cda
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-24 16:53:52 +1300

    Remove unneeded deps from wearos

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -55,12 +55,9 @@ dependencies {
 
     val androidxArch = "2.1.0"
     val androidxAppCompat = "1.3.0-beta01"
-    val androidxConstraintLayout = "2.1.0-alpha2"
     val androidxCoreKtx = "1.5.0-beta02"
     val androidxEspresso = "3.1.0"
-    val androidxFragment = "1.3.0"
     val androidxLifecycle = "2.3.0"
-    val androidxNavigation = "2.3.3"
     val androidxPreference = "1.1.1"
     val androidxTest = "1.4.0-alpha04"
     val androidxTestExt = "1.1.3-alpha04"
@@ -70,9 +67,7 @@ dependencies {
     val compose = "1.0.0-beta02"
     val lifecycleCompose = "1.0.0-alpha02"
     val activityCompose = "1.3.0-alpha03"
-    val navigationCompose = "1.0.0-alpha09"
 
-    val googleMaterial = "1.4.0-alpha01"
     val playServicesWearable = "17.0.0"
     val timber = "4.7.1"
     val junit = "4.13.2"
@@ -82,25 +77,20 @@ dependencies {
     val coroutines = "1.4.3"
 
     implementation("androidx.appcompat:appcompat:$androidxAppCompat")
-    implementation("androidx.constraintlayout:constraintlayout:$androidxConstraintLayout")
     implementation("androidx.core:core-ktx:$androidxCoreKtx")
     implementation("androidx.lifecycle:lifecycle-common-java8:$androidxLifecycle")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidxLifecycle")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$androidxLifecycle")
     implementation("androidx.lifecycle:lifecycle-service:$androidxLifecycle")
-    implementation("androidx.navigation:navigation-fragment-ktx:$androidxNavigation")
-    implementation("androidx.navigation:navigation-ui-ktx:$androidxNavigation")
     implementation("androidx.preference:preference:$androidxPreference")
     implementation("androidx.wear:wear:$androidxWear")
     implementation("androidx.wear:wear-complications-provider:$androidxWearComplications")
 
     implementation("com.google.android.gms:play-services-wearable:$playServicesWearable")
-    implementation("com.google.android.material:material:$googleMaterial")
     implementation("com.jakewharton.timber:timber:$timber")
 
     implementation("androidx.compose.ui:ui:$compose")
     implementation("androidx.compose.ui:ui-tooling:$compose")
-    implementation("androidx.compose.ui:ui-viewbinding:$compose")
     implementation("androidx.compose.foundation:foundation:$compose")
     implementation("androidx.compose.material:material:$compose")
     implementation("androidx.compose.material:material-icons-core:$compose")
@@ -108,9 +98,6 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:$compose")
     implementation("androidx.activity:activity-compose:$activityCompose")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleCompose")
-    implementation("androidx.navigation:navigation-compose:$navigationCompose")
-
-    debugImplementation("androidx.fragment:fragment-testing:$androidxFragment")
 
     testImplementation("androidx.arch.core:core-testing:$androidxArch")
     testImplementation("androidx.test:core-ktx:$androidxTest")
@@ -122,7 +109,6 @@ dependencies {
     testImplementation("org.robolectric:robolectric:$robolectric")
 
     androidTestImplementation("androidx.test.espresso:espresso-core:$androidxEspresso")
-    androidTestImplementation("androidx.navigation:navigation-testing:$androidxNavigation")
     androidTestImplementation("androidx.test:runner:$androidxTest")
     androidTestImplementation("androidx.test:rules:$androidxTest")
     androidTestImplementation("androidx.test:core-ktx:$androidxTest")
