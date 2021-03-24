356defa679ccae57e7d4c948ae60ee1e9f649735
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-03-24 15:40:58 +1300

    Remove unused resources

--- wearos/src/main/java/com/boswelja/devicemanager/phoneconnectionmanager/References.kt
+++ wearos/src/main/java/com/boswelja/devicemanager/phoneconnectionmanager/References.kt
@@ -1,15 +1,7 @@
-/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
- *
- * This file is part of Wearable Extensions
- *
- * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
- * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
- */
 package com.boswelja.devicemanager.phoneconnectionmanager
 
 object References {
 
     const val PHONE_NAME_KEY = "registered_phone_name"
     const val PHONE_ID_KEY = "registered_phone_id"
-    const val PHONE_CONNECTED_KEY = "registered_phone_connected"
 }
