6a6732652ac5c469c22032361063560cf741296e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-06 14:15:37 +1200

    Adapt DnDRemoteChangeReceiver.kt to use our phone state store

--- wearos/src/main/kotlin/com/boswelja/devicemanager/dndsync/DnDRemoteChangeReceiver.kt
+++ wearos/src/main/kotlin/com/boswelja/devicemanager/dndsync/DnDRemoteChangeReceiver.kt
@@ -1,37 +1,35 @@
 package com.boswelja.devicemanager.dndsync
 
-import androidx.preference.PreferenceManager
 import com.boswelja.devicemanager.common.Compat
 import com.boswelja.devicemanager.common.dndsync.References
 import com.boswelja.devicemanager.common.preference.PreferenceKey.DND_SYNC_TO_WATCH_KEY
+import com.boswelja.devicemanager.phoneStateStore
 import com.boswelja.devicemanager.phoneconnectionmanager.PreferenceSyncHelper
-import com.boswelja.devicemanager.phoneconnectionmanager.References.PHONE_ID_KEY
 import com.google.android.gms.wearable.DataEvent
 import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.DataMapItem
 import com.google.android.gms.wearable.WearableListenerService
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
 class DnDRemoteChangeReceiver : WearableListenerService() {
 
     override fun onDataChanged(dataEventBuffer: DataEventBuffer) {
         super.onDataChanged(dataEventBuffer)
-
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-
-        if (sharedPreferences.getBoolean(DND_SYNC_TO_WATCH_KEY, false)) {
-            val dataEvent = dataEventBuffer.last()
-            if (dataEvent.type == DataEvent.TYPE_CHANGED) {
-                val dataMap = DataMapItem.fromDataItem(dataEvent.dataItem).dataMap
-                val interruptFilterEnabled = dataMap.getBoolean(References.NEW_DND_STATE_KEY)
-                val success = Compat.setInterruptionFilter(this, interruptFilterEnabled)
-                if (!success) {
-                    PreferenceSyncHelper(
-                        this,
-                        sharedPreferences.getString(PHONE_ID_KEY, "") ?: ""
-                    ).also { it.pushData(DND_SYNC_TO_WATCH_KEY, false) }
-                }
+        val dataEvent = dataEventBuffer.last()
+        if (dataEvent.type == DataEvent.TYPE_CHANGED) {
+            val dataMap = DataMapItem.fromDataItem(dataEvent.dataItem).dataMap
+            val interruptFilterEnabled = dataMap.getBoolean(References.NEW_DND_STATE_KEY)
+            val success = Compat.setInterruptionFilter(this, interruptFilterEnabled)
+            if (!success) {
+                val phoneId = runBlocking { phoneStateStore.data.map { it.id }.first() }
+                PreferenceSyncHelper(
+                    this,
+                    phoneId
+                ).also { it.pushData(DND_SYNC_TO_WATCH_KEY, false) }
             }
-            dataEventBuffer.release()
         }
+        dataEventBuffer.release()
     }
 }
