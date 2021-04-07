ffe8f5d513418e22110aebffc5c82b8d0be70bb6
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-07 22:32:04 +1200

    Bump version

--- buildSrc/src/main/kotlin/PackageInfo.kt
+++ buildSrc/src/main/kotlin/PackageInfo.kt
@@ -7,7 +7,7 @@ object DebugInfo {
 object PackageInfo {
     const val targetSdk = 30
     const val packageName = "com.boswelja.devicemanager"
-    const val versionName = "3.2.1"
+    const val versionName = "3.2.2"
 
     fun getVersionCode(): Int {
         // We need an offset to ensure it's newer than the old versions
