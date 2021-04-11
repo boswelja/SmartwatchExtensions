85112c435ec1f9f496e7025d2394f9eaab441166
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-11 15:07:34 +1200

    Revert "Only override Application in debug builds We don't need to override on release builds, since all we were doing was planting a Timber tree"
    This reverts commit 67a94a94

---
+++ wearos/src/main/kotlin/com/boswelja/devicemanager/MainApplication.kt
@@ -0,0 +1,20 @@
+/* Copyright (C) 2020 Jack Boswell <boswelja@outlook.com>
+ *
+ * This file is part of Wearable Extensions
+ *
+ * This file, and any part of the Wearable Extensions app/s cannot be copied and/or distributed
+ * without permission from Jack Boswell (boswelja) <boswela@outlook.com>
+ */
+package com.boswelja.devicemanager
+
+import android.app.Application
+import timber.log.Timber
+
+@Suppress("unused")
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
