dff2e4bfbd41f43373012e97901d6402b6a61bf4
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-14 21:29:43 +1200

    Remove any trailing info from versionName

--- buildSrc/src/main/kotlin/PackageInfo.kt
+++ buildSrc/src/main/kotlin/PackageInfo.kt
@@ -12,18 +12,18 @@ object PackageInfo {
         return try {
             System.getenv("VERSION_NAME")
         } catch (e: Exception) {
-            "0.1.0"
+            "0.1.0-DEV"
         }
     }
 
     fun getVersionCode(platformIdentifier: Char): Int {
         val versionParts = getVersionName().split('.')
-        // Calculate major part at the start of version code, 300xxxxxx
+        // Calculate major part at the start of version code, 30xxxxx
         val majorPart = versionParts[0].padEnd(2, '0')
-        // Calculate minor part in the middle of the version code, xxx200xxx
+        // Calculate minor part in the middle of the version code, xx20xxx
         val minorPart = versionParts[1].padEnd(2, '0')
         // Calculate patch part at the end of the version code, xxxxxx500
-        val patchPart = versionParts[2].padEnd(3, '0')
+        val patchPart = versionParts[2].split('-')[0].padEnd(3, '0')
         return (majorPart + minorPart + patchPart + platformIdentifier).toInt()
     }
 }
