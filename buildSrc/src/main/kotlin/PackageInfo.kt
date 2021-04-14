cf649d2e8b54d54c4fc9433190d1068618c324dc
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-14 21:43:06 +1200

    Fix possible issues with double digit minor/patch versions

--- buildSrc/src/main/kotlin/PackageInfo.kt
+++ buildSrc/src/main/kotlin/PackageInfo.kt
@@ -18,12 +18,12 @@ object PackageInfo {
 
     fun getVersionCode(platformIdentifier: Char): Int {
         val versionParts = getVersionName().split('.')
-        // Calculate major part at the start of version code, 30xxxxx
-        val majorPart = versionParts[0].padEnd(2, '0')
-        // Calculate minor part in the middle of the version code, xx20xxx
-        val minorPart = versionParts[1].padEnd(2, '0')
-        // Calculate patch part at the end of the version code, xxxxxx500
-        val patchPart = versionParts[2].split('-')[0].padEnd(3, '0')
+        // Calculate major part at the start of version code, 1xxxx
+        val majorPart = versionParts[0]
+        // Calculate minor part in the middle of the version code, x02xx
+        val minorPart = versionParts[1].padStart(2, '0')
+        // Calculate patch part at the end of the version code, xxx05
+        val patchPart = versionParts[2].split('-')[0].padStart(2, '0')
         return (majorPart + minorPart + patchPart + platformIdentifier).toInt()
     }
 }
