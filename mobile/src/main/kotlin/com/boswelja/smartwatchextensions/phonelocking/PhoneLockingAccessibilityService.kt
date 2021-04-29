6da160a30fcc5346161e724332235b4d771197c6
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-30 09:00:22 +1200

    Don't automatically stop PhoneLockingAccessibilityService.kt This should simplify enabling and disabling phone locking, since it's now independent from the service

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/phonelocking/PhoneLockingAccessibilityService.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/phonelocking/PhoneLockingAccessibilityService.kt
@@ -2,11 +2,9 @@ package com.boswelja.smartwatchextensions.phonelocking
 
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
-import androidx.lifecycle.Observer
 import com.boswelja.smartwatchextensions.common.connection.Messages.LOCK_PHONE
 import com.boswelja.smartwatchextensions.common.preference.PreferenceKey.PHONE_LOCKING_ENABLED_KEY
 import com.boswelja.smartwatchextensions.watchmanager.WatchManager
-import com.boswelja.smartwatchextensions.watchmanager.item.BoolPreference
 import com.boswelja.watchconnection.core.MessageListener
 import com.boswelja.watchconnection.core.Watch
 import com.google.android.gms.wearable.MessageEvent
@@ -21,22 +19,9 @@ class PhoneLockingAccessibilityService :
     AccessibilityService(),
     MessageListener {
 
-    private val settingsObserver = Observer<Array<BoolPreference>> { prefs ->
-        if (prefs.none { it.value }) {
-            Timber.d("Stopping due to no watches with Phone Locking enabled")
-            stop()
-        }
-    }
-
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
     private val watchManager: WatchManager by lazy {
         WatchManager.getInstance(this)
-    }
-
-    private val settingsLiveData by lazy {
-        watchManager.settingsDatabase.boolPrefDao().getAllObservableForKey(
-            PHONE_LOCKING_ENABLED_KEY
-        )
     }
 
     private var isStopping = false
@@ -52,7 +37,6 @@ class PhoneLockingAccessibilityService :
     override fun onServiceConnected() {
         Timber.i("onServiceConnected() called")
         watchManager.registerMessageListener(this)
-        settingsLiveData.observeForever(settingsObserver)
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
@@ -106,7 +90,6 @@ class PhoneLockingAccessibilityService :
                 )
             }
             watchManager.unregisterMessageListener(this)
-            settingsLiveData.removeObserver(settingsObserver)
         }
     }
 }
