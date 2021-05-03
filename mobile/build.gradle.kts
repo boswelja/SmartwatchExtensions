560b49c5542abac86b9cb8835e53c3af746a34cf
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-05-03 12:49:02 +1200

    Use libs store for mobile dependencies

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -54,7 +54,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-beta05"
+        kotlinCompilerExtensionVersion = libs.versions.compose.get()
     }
 
     packagingOptions {
@@ -72,88 +72,49 @@ configurations.all {
 }
 
 dependencies {
-    val androidxArch = "2.1.0"
-    val androidxAppCompat = "1.3.0-rc01"
-    val androidxBrowser = "1.3.0"
-    val androidxCoreKtx = "1.6.0-alpha02"
-    val androidxDataStore = "1.0.0-beta01"
-    val androidxEspresso = "3.4.0-alpha05"
-    val androidxLifecycle = "2.4.0-alpha01"
-    val androidxRoom = "2.4.0-alpha01"
-    val androidxTest = "1.4.0-alpha05"
-    val androidxTestExt = "1.1.3-alpha05"
-    val androidxWork = "2.7.0-alpha03"
-
-    val compose = "1.0.0-beta05"
-    val lifecycleCompose = "1.0.0-alpha04"
-    val activityCompose = "1.3.0-alpha06"
-
-    val watchconnection = "2.0.1"
-    val billingClient = "3.0.3"
-    val googlePlayCore = "1.10.0"
-    val timber = "4.7.1"
-    val junit = "4.13.2"
-    val strikt = "0.31.0"
-    val mockk = "1.11.0"
-    val robolectric = "4.5.1"
-    val coroutines = "1.4.3"
-
     implementation(project(":common"))
 
-    implementation(platform("com.google.firebase:firebase-bom:26.2.0"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation(libs.firebase.analytics)
 
-    implementation("androidx.appcompat:appcompat:$androidxAppCompat")
-    implementation("androidx.browser:browser:$androidxBrowser")
-    implementation("androidx.core:core-ktx:$androidxCoreKtx")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidxLifecycle")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$androidxLifecycle")
-    implementation("androidx.lifecycle:lifecycle-common-java8:$androidxLifecycle")
-    implementation("androidx.lifecycle:lifecycle-service:$androidxLifecycle")
-    implementation("androidx.room:room-runtime:$androidxRoom")
-    implementation("androidx.room:room-ktx:$androidxRoom")
-    implementation("androidx.work:work-runtime-ktx:$androidxWork")
-    implementation("androidx.datastore:datastore-preferences:$androidxDataStore")
-    implementation("androidx.datastore:datastore:$androidxDataStore")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.room)
+    implementation(libs.androidx.work.ktx)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.proto)
 
-    implementation("com.android.billingclient:billing-ktx:$billingClient")
-    implementation("com.google.android.play:core:$googlePlayCore")
-    implementation("com.google.android.play:core-ktx:1.8.1")
-    implementation("com.jakewharton.timber:timber:$timber")
+    implementation(libs.billingclient)
+    implementation(libs.googleplay.core)
+    implementation(libs.googleplay.corektx)
+    implementation(libs.timber)
+    implementation(libs.watchconnection.wearos)
 
-    implementation("io.github.boswelja.watchconnection:wearos:$watchconnection")
-    implementation("androidx.compose.ui:ui:$compose")
-    implementation("androidx.compose.ui:ui-tooling:$compose")
-    implementation("androidx.compose.ui:ui-viewbinding:$compose")
-    implementation("androidx.compose.foundation:foundation:$compose")
-    implementation("androidx.compose.material:material:$compose")
-    implementation("androidx.compose.material:material-icons-core:$compose")
-    implementation("androidx.compose.material:material-icons-extended:$compose")
-    implementation("androidx.compose.runtime:runtime-livedata:$compose")
-    implementation("androidx.activity:activity-compose:$activityCompose")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleCompose")
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.compose.ui.viewbinding)
 
-    ksp("androidx.room:room-compiler:$androidxRoom")
+    ksp(libs.androidx.room.compiler)
 
-    testImplementation("androidx.arch.core:core-testing:$androidxArch")
-    testImplementation("androidx.test:core-ktx:$androidxTest")
-    testImplementation("androidx.test.ext:junit-ktx:$androidxTestExt")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutines")
-    testImplementation("junit:junit:$junit")
-    testImplementation("io.strikt:strikt-core:$strikt")
-    testImplementation("io.strikt:strikt-mockk:$strikt")
-    testImplementation("io.mockk:mockk:$mockk")
-    testImplementation("org.robolectric:robolectric:$robolectric")
+    testImplementation(libs.androidx.arch.core.test)
+    testImplementation(libs.androidx.test.corektx)
+    testImplementation(libs.androidx.test.ext.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.strikt.core)
+    testImplementation(libs.strikt.mockk)
+    testImplementation(libs.mockk.core)
+    testImplementation(libs.robolectric)
 
-    androidTestImplementation("androidx.arch.core:core-testing:$androidxArch")
-    androidTestImplementation("androidx.test.espresso:espresso-core:$androidxEspresso")
-    androidTestImplementation("androidx.test:runner:$androidxTest")
-    androidTestImplementation("androidx.test:rules:$androidxTest")
-    androidTestImplementation("androidx.test:core-ktx:$androidxTest")
-    androidTestImplementation("io.strikt:strikt-core:$strikt")
-    androidTestImplementation("io.strikt:strikt-mockk:$strikt")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose")
-    androidTestImplementation("io.mockk:mockk-android:$mockk")
+    androidTestImplementation(libs.androidx.arch.core.test)
+    androidTestImplementation(libs.androidx.test.espresso)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.androidx.test.corektx)
+    androidTestImplementation(libs.strikt.core)
+    androidTestImplementation(libs.strikt.mockk)
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    androidTestImplementation(libs.mockk.android)
 }
 
 wire {
