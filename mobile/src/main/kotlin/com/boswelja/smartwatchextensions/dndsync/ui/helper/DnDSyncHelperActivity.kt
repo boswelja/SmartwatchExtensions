9dd6458ff75fc6570a936046327b2ca481c6076d
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-05-23 23:16:59 +1200

    Add missing remember to mutable state

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dndsync/ui/helper/DnDSyncHelperActivity.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dndsync/ui/helper/DnDSyncHelperActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -52,7 +53,7 @@ class DnDSyncHelperActivity : AppCompatActivity() {
         setContent {
             val viewModel: HelperViewModel = viewModel()
             val setupResult by viewModel.result.observeAsState()
-            var isLoading by mutableStateOf(false)
+            var isLoading by remember { mutableStateOf(false) }
             AppTheme {
                 Scaffold(
                     topBar = {
