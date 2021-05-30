b3aacc2e8d951d6177a0d360e9b0cbaa34cea188
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-05-30 16:36:06 +1200

    Use Utils function for sending all apps

--- wearos/src/main/kotlin/com/boswelja/smartwatchextensions/appmanager/AppManagerWorker.kt
+++ wearos/src/main/kotlin/com/boswelja/smartwatchextensions/appmanager/AppManagerWorker.kt
@@ -1,22 +1,16 @@
 package com.boswelja.smartwatchextensions.appmanager
 
 import android.content.Context
-import android.content.pm.PackageManager
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
-import com.boswelja.smartwatchextensions.common.appmanager.App
-import com.boswelja.smartwatchextensions.common.appmanager.Messages
-import com.boswelja.smartwatchextensions.common.toByteArray
 import com.boswelja.smartwatchextensions.phoneStateStore
-import com.google.android.gms.wearable.Wearable
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 
 class AppManagerWorker(
@@ -32,45 +26,15 @@ class AppManagerWorker(
             return Result.retry()
         }
 
-        // Get all apps
-        val packageManager = applicationContext.packageManager
-        val allApps = packageManager.getAllApps()
-        Timber.d("Got %s apps", allApps.count())
-
-        val messageClient = Wearable.getMessageClient(applicationContext)
-
         try {
-            // Send expected app count to the phone
-            val data = allApps.count().toByteArray()
-            messageClient.sendMessage(
-                phoneId,
-                Messages.EXPECTED_APP_COUNT,
-                data
-            ).await()
-
-            // Start sending apps
-            allApps.forEach { app ->
-                val appBytes = app.toByteArray()
-                Timber.d("Sending %s bytes", appBytes.count())
-                messageClient.sendMessage(
-                    phoneId,
-                    Messages.PACKAGE_ADDED,
-                    appBytes
-                )
-            }
+            Timber.d("Sending all apps")
+            applicationContext.sendAllApps(phoneId)
         } catch (e: Exception) {
             Timber.e(e)
             return Result.retry()
         }
 
         return Result.success()
-    }
-
-    private fun PackageManager.getAllApps(): List<App> {
-        return getInstalledPackages(PackageManager.GET_PERMISSIONS)
-            .map {
-                App(this, it)
-            }
     }
 
     companion object {
