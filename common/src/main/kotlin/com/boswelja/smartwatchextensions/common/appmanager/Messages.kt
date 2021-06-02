8cf71f5bb0500eaabe0aa826c3d2e694275a3250
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-02 20:51:10 +1200

    Add basic cache validation to AppManagerMessageReceiver.kt We'll use the hash code of the list of package names to check cache validity

--- common/src/main/kotlin/com/boswelja/smartwatchextensions/common/appmanager/Messages.kt
+++ common/src/main/kotlin/com/boswelja/smartwatchextensions/common/appmanager/Messages.kt
@@ -2,18 +2,11 @@ package com.boswelja.smartwatchextensions.common.appmanager
 
 object Messages {
 
-    const val PACKAGE_ADDED = "/app_manager_package_added"
-    const val PACKAGE_REMOVED = "/app_manager_package_removed"
-    const val PACKAGE_UPDATED = "/app_manager_package_updated"
-
     const val REQUEST_OPEN_PACKAGE = "/app_manager_request_open_package"
     const val REQUEST_UNINSTALL_PACKAGE = "/app_manager_request_uninstall_package"
-
-    const val START_SERVICE = "/start_app_manager_service"
-    const val STOP_SERVICE = "/stop_app_manager_service"
 
     const val START_SENDING_APPS = "/app_manager_sending_all"
     const val ALL_APPS = "/app_manager_all_apps"
 
-    const val CHECK_CACHE_VALID = "/app_manager_cache_check"
+    const val VALIDATE_CACHE = "/app_manager_cache_check"
 }
--- wearos/src/main/kotlin/com/boswelja/smartwatchextensions/appmanager/AppManagerMessageReceiver.kt
+++ wearos/src/main/kotlin/com/boswelja/smartwatchextensions/appmanager/AppManagerMessageReceiver.kt
@@ -1,10 +1,16 @@
 package com.boswelja.smartwatchextensions.appmanager
 
-import com.boswelja.smartwatchextensions.common.appmanager.Messages.CHECK_CACHE_VALID
 import com.boswelja.smartwatchextensions.common.appmanager.Messages.REQUEST_OPEN_PACKAGE
 import com.boswelja.smartwatchextensions.common.appmanager.Messages.REQUEST_UNINSTALL_PACKAGE
+import com.boswelja.smartwatchextensions.common.appmanager.Messages.VALIDATE_CACHE
+import com.boswelja.smartwatchextensions.common.fromByteArray
+import com.boswelja.smartwatchextensions.phoneStateStore
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import timber.log.Timber
 
 class AppManagerMessageReceiver : WearableListenerService() {
 
@@ -20,8 +26,27 @@ class AppManagerMessageReceiver : WearableListenerService() {
                     openPackage(packageName)
                 }
             }
-            CHECK_CACHE_VALID -> {
-                // TODO Validate cache and send a response
+            VALIDATE_CACHE -> {
+                val currentPackages = packageManager.getInstalledPackages(0)
+                    .map {
+                        it.packageName
+                    }
+                    .hashCode()
+                val cacheHash = Int.fromByteArray(event.data)
+                if (cacheHash == currentPackages) {
+                    Timber.d("Cache appears to be up to date")
+                } else {
+                    Timber.d(
+                        "Received cache hash %s, but %s was expected",
+                        cacheHash,
+                        currentPackages
+                    )
+                    // Since WearableListenerService runs on another thread, we can use runBlocking
+                    runBlocking {
+                        val phoneId = phoneStateStore.data.map { it.id }.first()
+                        sendAllApps(phoneId)
+                    }
+                }
             }
         }
     }
