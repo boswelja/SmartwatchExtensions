20911de96ce9740c40fbb4c4bc662d89c7d27356
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-05 21:08:05 +1200

    Auto-queue app manager cache worker on upgrading installs

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/bootorupdate/updater/Updater.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/bootorupdate/updater/Updater.kt
@@ -5,8 +5,12 @@ package com.boswelja.smartwatchextensions.bootorupdate.updater
 import android.content.Context
 import com.boswelja.smartwatchextensions.BuildConfig
 import com.boswelja.smartwatchextensions.appStateStore
+import com.boswelja.smartwatchextensions.appmanager.AppCacheUpdateWorker
+import com.boswelja.smartwatchextensions.watchmanager.database.WatchDatabase
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.take
 
 class Updater(private val context: Context) {
 
@@ -24,7 +28,17 @@ class Updater(private val context: Context) {
      * Update the app's working environment.
      * @return The [Result] of the update
      */
-    fun doUpdate(): Result {
+    suspend fun doUpdate(): Result {
+        if (lastAppVersion <= 401011) {
+            WatchDatabase.getInstance(context)
+                .watchDao().getAll().take(1)
+                .collect {
+                    it.forEach { watch ->
+                        AppCacheUpdateWorker.enqueueWorkerFor(context, watch.id)
+                    }
+                }
+            return Result.COMPLETED
+        }
         return Result.NOT_NEEDED
     }
 }
