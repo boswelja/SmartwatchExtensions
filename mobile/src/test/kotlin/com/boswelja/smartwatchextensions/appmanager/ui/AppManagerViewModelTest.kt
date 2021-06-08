fa888dea3bdf9b3f8f69be3d726ae4ebf4922c1a
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-08 21:53:40 +1200

    Create very basic AppManagerViewModelTest.kt

---
+++ mobile/src/test/kotlin/com/boswelja/smartwatchextensions/appmanager/ui/AppManagerViewModelTest.kt
@@ -0,0 +1,120 @@
+package com.boswelja.smartwatchextensions.appmanager.ui
+
+import android.app.Application
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.liveData
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.boswelja.smartwatchextensions.WatchManagerTestRule
+import com.boswelja.smartwatchextensions.appmanager.App
+import com.boswelja.smartwatchextensions.appmanager.database.WatchAppDatabase
+import com.boswelja.smartwatchextensions.common.appmanager.Messages.REQUEST_OPEN_PACKAGE
+import com.boswelja.smartwatchextensions.common.appmanager.Messages.REQUEST_UNINSTALL_PACKAGE
+import com.boswelja.smartwatchextensions.common.appmanager.Messages.VALIDATE_CACHE
+import com.boswelja.smartwatchextensions.watchmanager.database.DbWatch.Companion.toDbWatch
+import com.boswelja.watchconnection.core.Watch
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.just
+import java.util.UUID
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@ExperimentalCoroutinesApi
+@FlowPreview
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.R])
+class AppManagerViewModelTest {
+
+    private val watch = Watch(UUID.randomUUID(), "", "", "")
+    private val app = App(
+        watchId = watch.id,
+        icon = null,
+        version = "",
+        packageName = "",
+        label = "",
+        isSystemApp = false,
+        hasLaunchActivity = true,
+        isEnabled = true,
+        installTime = 0,
+        lastUpdateTime = 0,
+        requestedPermissions = emptyList()
+    )
+
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val watchManagerRule = WatchManagerTestRule()
+
+    private lateinit var context: Application
+    private lateinit var appDatabase: WatchAppDatabase
+
+    private lateinit var viewModel: AppManagerViewModel
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+
+        // Create dummy app database
+        appDatabase = Room.inMemoryDatabaseBuilder(context, WatchAppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        every {
+            watchManagerRule.watchManager.selectedWatch
+        } returns flow { emit(watch.toDbWatch()) }
+        every {
+            watchManagerRule.watchManager.registeredWatches
+        } returns liveData { emit(listOf(watch)) }
+        every { watchManagerRule.watchManager.registerMessageListener(any()) } just Runs
+        every { watchManagerRule.watchManager.unregisterMessageListener(any()) } just Runs
+        coEvery { watchManagerRule.watchManager.sendMessage(any(), any(), any()) } returns true
+
+        viewModel = AppManagerViewModel(context, appDatabase, watchManagerRule.watchManager)
+    }
+
+    @Test
+    fun `sendOpenRequest sends the correct message to the watch`(): Unit = runBlocking {
+        viewModel.sendOpenRequest(app)
+
+        coVerify {
+            watchManagerRule.watchManager.sendMessage(
+                watch, REQUEST_OPEN_PACKAGE, app.packageName.toByteArray(Charsets.UTF_8)
+            )
+        }
+    }
+
+    @Test
+    fun `sendUninstallRequest sends the correct message to the watch`(): Unit = runBlocking {
+        viewModel.sendUninstallRequest(app)
+
+        coVerify {
+            watchManagerRule.watchManager.sendMessage(
+                watch, REQUEST_UNINSTALL_PACKAGE, app.packageName.toByteArray(Charsets.UTF_8)
+            )
+        }
+    }
+
+    @Test
+    fun `validateCacheFor sends the correct message to the watch`(): Unit = runBlocking {
+        viewModel.validateCacheFor(watch)
+
+        coVerify {
+            watchManagerRule.watchManager.sendMessage(
+                watch, VALIDATE_CACHE, isNull(inverse = true)
+            )
+        }
+    }
+}
