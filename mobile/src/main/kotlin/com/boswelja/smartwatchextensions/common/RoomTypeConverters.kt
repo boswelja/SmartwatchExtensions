6f606676ff145096620932528b2d7b1e33622ab0
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-09 19:50:49 +1200

    Remove type converters for enums

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/common/RoomTypeConverters.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/common/RoomTypeConverters.kt
@@ -3,38 +3,10 @@ package com.boswelja.smartwatchextensions.common
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.room.TypeConverter
-import com.boswelja.smartwatchextensions.messages.Message
 import java.io.ByteArrayOutputStream
 import java.util.UUID
 
 class RoomTypeConverters {
-    @TypeConverter
-    fun actionFromString(str: String?): Message.Action? {
-        return if (str != null) {
-            Message.Action.valueOf(str)
-        } else {
-            null
-        }
-    }
-
-    @TypeConverter
-    fun actionToString(action: Message.Action?): String? {
-        return action?.toString()
-    }
-
-    @TypeConverter
-    fun iconFromString(str: String?): Message.Icon? {
-        return if (str != null) {
-            Message.Icon.valueOf(str)
-        } else {
-            null
-        }
-    }
-
-    @TypeConverter
-    fun iconToString(icon: Message.Icon?): String? {
-        return icon?.toString()
-    }
 
     @TypeConverter
     fun uuidFromString(value: String?): UUID? {
