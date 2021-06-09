2cacf816f829640c31be4a1ea1c9c12e112b8991
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-09 21:18:52 +1200

    Remove liveData from WidgetSettingsViewModel.kt

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/widget/ui/WidgetSettingsViewModel.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/widget/ui/WidgetSettingsViewModel.kt
@@ -3,13 +3,10 @@ package com.boswelja.smartwatchextensions.widget.ui
 import android.app.Application
 import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.boswelja.smartwatchextensions.widget.ui.WidgetSettingsActivity.Companion.SHOW_WIDGET_BACKGROUND_KEY
 import com.boswelja.smartwatchextensions.widget.ui.WidgetSettingsActivity.Companion.WIDGET_BACKGROUND_OPACITY_KEY
 import com.boswelja.smartwatchextensions.widget.widgetSettings
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
@@ -17,27 +14,10 @@ class WidgetSettingsViewModel(application: Application) : AndroidViewModel(appli
 
     private val widgetSettings = application.widgetSettings
 
-    private val _widgetBackgroundVisible = MutableLiveData(true)
-    private val _widgetBackgroundOpacity = MutableLiveData(60)
-
-    val widgetBackgroundVisible: LiveData<Boolean>
-        get() = _widgetBackgroundVisible
-    val widgetBackgroundOpacity: LiveData<Int>
-        get() = _widgetBackgroundOpacity
-
-    init {
-        viewModelScope.launch {
-            widgetSettings.data.map { it[SHOW_WIDGET_BACKGROUND_KEY] }.collect {
-                _widgetBackgroundVisible.postValue(it ?: true)
-            }
-            widgetSettings.data.map { it[WIDGET_BACKGROUND_OPACITY_KEY] }.collect {
-                _widgetBackgroundOpacity.postValue(it ?: 60)
-            }
-        }
-    }
+    val widgetBackgroundVisible = widgetSettings.data.map { it[SHOW_WIDGET_BACKGROUND_KEY] }
+    val widgetBackgroundOpacity = widgetSettings.data.map { it[WIDGET_BACKGROUND_OPACITY_KEY] }
 
     fun setShowBackground(showBackground: Boolean) {
-        _widgetBackgroundVisible.postValue(showBackground)
         viewModelScope.launch {
             widgetSettings.edit {
                 it[SHOW_WIDGET_BACKGROUND_KEY] = showBackground
@@ -46,7 +26,6 @@ class WidgetSettingsViewModel(application: Application) : AndroidViewModel(appli
     }
 
     fun setBackgroundOpacity(opacity: Int) {
-        _widgetBackgroundOpacity.postValue(opacity)
         viewModelScope.launch {
             widgetSettings.edit {
                 it[WIDGET_BACKGROUND_OPACITY_KEY] = opacity
