3d2e6a01b25099daff97ecca6e0e1486fb03c5f4
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-12 16:22:09 +1200

    Use StaggeredVerticalGrid for DashboardScreen

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/DashboadScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/DashboadScreen.kt
@@ -1,12 +1,14 @@
 package com.boswelja.smartwatchextensions.dashboard.ui
 
 import android.content.Intent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
@@ -32,6 +34,7 @@ import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.appmanager.ui.AppManagerActivity
 import com.boswelja.smartwatchextensions.batterysync.ui.BatterySyncSettingsActivity
 import com.boswelja.smartwatchextensions.batterysync.ui.BatterySyncSettingsHeader
+import com.boswelja.smartwatchextensions.common.ui.StaggeredVerticalGrid
 import com.boswelja.smartwatchextensions.dndsync.ui.DnDSyncSettingsActivity
 import com.boswelja.smartwatchextensions.phonelocking.ui.PhoneLockingSettingsActivity
 import com.boswelja.watchconnection.core.Status
@@ -39,6 +42,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import timber.log.Timber
 
+@ExperimentalFoundationApi
 @ExperimentalCoroutinesApi
 @ExperimentalMaterialApi
 @Composable
@@ -51,51 +55,57 @@ fun DashboardScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(scrollState),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+            .verticalScroll(scrollState)
     ) {
         WatchStatus(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp),
             status = watchStatus
         )
-        DashboardItem(
-            content = { BatterySyncSettingsHeader() },
-            buttonLabel = stringResource(
-                R.string.dashboard_settings_label,
-                stringResource(R.string.battery_sync_title)
-            ),
-            onClick = {
-                context.startActivity(Intent(context, BatterySyncSettingsActivity::class.java))
-            }
-        )
-        DashboardItem(
-            content = { },
-            buttonLabel = stringResource(
-                R.string.dashboard_settings_label,
-                stringResource(R.string.main_dnd_sync_title)
-            ),
-            onClick = {
-                context.startActivity(Intent(context, DnDSyncSettingsActivity::class.java))
-            }
-        )
-        DashboardItem(
-            content = { },
-            buttonLabel = stringResource(
-                R.string.dashboard_settings_label,
-                stringResource(R.string.main_phone_locking_title)
-            ),
-            onClick = {
-                context.startActivity(Intent(context, PhoneLockingSettingsActivity::class.java))
-            }
-        )
-        DashboardItem(
-            content = { },
-            buttonLabel = stringResource(R.string.main_app_manager_title),
-            onClick = {
-                context.startActivity(Intent(context, AppManagerActivity::class.java))
-            }
-        )
+        StaggeredVerticalGrid(
+            modifier = Modifier.padding(8.dp),
+            cells = GridCells.Fixed(2)
+        ) {
+            DashboardItem(
+                content = { BatterySyncSettingsHeader() },
+                buttonLabel = stringResource(
+                    R.string.dashboard_settings_label,
+                    stringResource(R.string.battery_sync_title)
+                ),
+                onClick = {
+                    context.startActivity(Intent(context, BatterySyncSettingsActivity::class.java))
+                }
+            )
+            DashboardItem(
+                content = { },
+                buttonLabel = stringResource(
+                    R.string.dashboard_settings_label,
+                    stringResource(R.string.main_dnd_sync_title)
+                ),
+                onClick = {
+                    context.startActivity(Intent(context, DnDSyncSettingsActivity::class.java))
+                }
+            )
+            DashboardItem(
+                content = { },
+                buttonLabel = stringResource(
+                    R.string.dashboard_settings_label,
+                    stringResource(R.string.main_phone_locking_title)
+                ),
+                onClick = {
+                    context.startActivity(Intent(context, PhoneLockingSettingsActivity::class.java))
+                }
+            )
+            DashboardItem(
+                content = { },
+                buttonLabel = stringResource(R.string.main_app_manager_title),
+                onClick = {
+                    context.startActivity(Intent(context, AppManagerActivity::class.java))
+                }
+            )
+        }
     }
 }
 
@@ -129,9 +139,9 @@ fun WatchStatus(
                 stringResource(R.string.watch_status_error)
             )
     }
-    Card {
+    Card(modifier) {
         Row(
-            modifier = modifier.padding(16.dp),
+            modifier = Modifier.padding(16.dp),
             horizontalArrangement = Arrangement.Center
         ) {
             Icon(icon, null)
@@ -149,7 +159,7 @@ fun DashboardItem(
     onClick: () -> Unit
 ) {
     Card(
-        modifier = modifier,
+        modifier = modifier.padding(8.dp),
         onClick = onClick
     ) {
         Column(Modifier.fillMaxSize()) {
