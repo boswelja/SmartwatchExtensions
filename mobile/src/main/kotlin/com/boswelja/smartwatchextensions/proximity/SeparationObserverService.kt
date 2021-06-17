4c3a0de94ddd369339366fdd252e7440a1b85547
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-17 21:39:37 +1200

    Initial implementation of collecting watch statuses and notifying the user

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/proximity/SeparationObserverService.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/proximity/SeparationObserverService.kt
@@ -1,6 +1,7 @@
 package com.boswelja.smartwatchextensions.proximity
 
 import android.app.Notification
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
@@ -10,47 +11,117 @@ import androidx.lifecycle.lifecycleScope
 import com.boswelja.smartwatchextensions.NotificationChannelHelper
 import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.common.preference.PreferenceKey.WATCH_SEPARATION_NOTI_KEY
+import com.boswelja.smartwatchextensions.watchmanager.WatchManager
 import com.boswelja.smartwatchextensions.watchmanager.database.WatchSettingsDatabase
+import com.boswelja.watchconnection.core.Watch
+import com.boswelja.watchconnection.core.discovery.Status
+import java.util.UUID
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class SeparationObserverService : LifecycleService() {
 
+    private val watchManager by lazy { WatchManager.getInstance(this) }
     private val settingsDatabase by lazy { WatchSettingsDatabase.getInstance(this) }
+    private var statusCollectorJob: Job? = null
 
+    @FlowPreview
     @ExperimentalCoroutinesApi
     override fun onCreate() {
         super.onCreate()
         NotificationChannelHelper.createForSeparationObserver(this, getSystemService()!!)
-        startForeground(1, createNotification())
+        startForeground(1, createForegroundNotification())
         lifecycleScope.launch(Dispatchers.IO) {
             startCollectingSettings()
         }
     }
 
+    @FlowPreview
     @ExperimentalCoroutinesApi
     private suspend fun startCollectingSettings() {
+        Timber.d("Collecting settings changes")
         settingsDatabase.boolSettings().getByKey(WATCH_SEPARATION_NOTI_KEY).mapLatest {
             it.filter { setting -> setting.value }.map { setting -> setting.watchId }
         }.collect { watchIds ->
             if (watchIds.isEmpty()) {
                 tryStop()
+            } else {
+                collectStatusesFor(watchIds)
             }
         }
     }
 
-    private fun createNotification(): Notification {
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    private suspend fun collectStatusesFor(watchIds: List<UUID>) {
+        // Map IDs to a list of Flows
+        val flows = watchIds.mapNotNull { watchId ->
+            // Get the associated watch
+            val watch = watchManager.getWatchById(watchId).first()
+
+            // Try get watch status
+            watch?.let {
+                watchManager.getStatusFor(it)?.debounce(100)?.map { status ->
+                    // Pair watch ID to status
+                    watch to status
+                }
+            }
+        }.toTypedArray()
+        Timber.d("Collecting statuses for %s watches", flows.count())
+
+        // Cancel any existing jobs
+        statusCollectorJob?.cancel()
+
+        // Start collecting new values
+        statusCollectorJob = lifecycleScope.launch(Dispatchers.Default) {
+            merge(*flows).collect { handleStatusChange(it.first, it.second) }
+        }
+    }
+
+    private fun handleStatusChange(watch: Watch, newStatus: Status) {
+        Timber.d("%s status changed to %s", watch.name, newStatus)
+        val notificationManager = getSystemService<NotificationManager>()!!
+        val notiId = watch.id.hashCode()
+        if (newStatus != Status.CONNECTED_NEARBY) {
+            val notification = createSeparationNotification(watch.name)
+            getSystemService<NotificationManager>()?.notify(notiId, notification)
+        } else {
+            notificationManager.cancel(notiId)
+        }
+    }
+
+    private fun createSeparationNotification(watchName: String): Notification {
+        return NotificationCompat.Builder(this, OBSERVER_NOTI_CHANNEL_ID)
+            .setContentTitle(getString(R.string.separation_notification_title, watchName))
+            .setContentText(getString(R.string.separation_notification_text, watchName))
+            .setSmallIcon(R.drawable.noti_ic_watch)
+            .setLocalOnly(true)
+            .build()
+    }
+
+    private fun createForegroundNotification(): Notification {
         return NotificationCompat.Builder(this, OBSERVER_NOTI_CHANNEL_ID)
             .setContentTitle(getString(R.string.proximity_observer_title))
             .setContentText(getString(R.string.proximity_observer_summary))
             .setSmallIcon(R.drawable.noti_ic_watch)
+            .setLocalOnly(true)
+            .setUsesChronometer(false)
             .build()
     }
 
     private fun tryStop() {
+        Timber.d("Stopping service")
+        statusCollectorJob?.cancel()
         stopForeground(true)
         stopSelf()
     }
