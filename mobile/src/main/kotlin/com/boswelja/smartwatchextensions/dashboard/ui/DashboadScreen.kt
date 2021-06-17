4e9041d537f538044e25119addf20d9d441b41d4
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-17 14:02:02 +1200

    Add a dashboard item for proximity settings

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/DashboadScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/DashboadScreen.kt
@@ -36,6 +36,7 @@ import com.boswelja.smartwatchextensions.common.startActivity
 import com.boswelja.smartwatchextensions.common.ui.StaggeredVerticalGrid
 import com.boswelja.smartwatchextensions.dndsync.ui.DnDSyncSettingsActivity
 import com.boswelja.smartwatchextensions.phonelocking.ui.PhoneLockingSettingsActivity
+import com.boswelja.smartwatchextensions.proximity.ui.ProximitySettingsActivity
 import com.boswelja.smartwatchextensions.watchmanager.ui.WatchStatusSummarySmall
 import com.boswelja.watchconnection.core.discovery.Status
 import kotlinx.coroutines.Dispatchers
@@ -92,6 +93,12 @@ fun DashboardScreen(
                 titleText = stringResource(R.string.main_phone_locking_title),
                 onClick = {
                     context.startActivity<PhoneLockingSettingsActivity>()
+                }
+            )
+            DashboardItem(
+                titleText = stringResource(R.string.proximity_settings_title),
+                onClick = {
+                    context.startActivity<ProximitySettingsActivity>()
                 }
             )
         }
