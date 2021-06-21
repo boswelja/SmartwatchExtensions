863e580347dec76c9fcd18243b47146075763f3f
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-21 12:51:30 +1200

    Add FindMyWatchSummarySmall to the dashboard

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/DashboadScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/DashboadScreen.kt
@@ -36,6 +36,7 @@ import com.boswelja.smartwatchextensions.common.startActivity
 import com.boswelja.smartwatchextensions.common.ui.StaggeredVerticalGrid
 import com.boswelja.smartwatchextensions.dndsync.ui.DnDSyncSettingsActivity
 import com.boswelja.smartwatchextensions.phonelocking.ui.PhoneLockingSettingsActivity
+import com.boswelja.smartwatchextensions.proximity.ui.FindMyWatchSummarySmall
 import com.boswelja.smartwatchextensions.proximity.ui.ProximitySettingsActivity
 import com.boswelja.smartwatchextensions.watchmanager.ui.WatchStatusSummarySmall
 import com.boswelja.watchconnection.core.discovery.Status
@@ -101,6 +102,13 @@ fun DashboardScreen(
                     context.startActivity<ProximitySettingsActivity>()
                 }
             )
+            DashboardItem(
+                content = {
+                    FindMyWatchSummarySmall()
+                },
+                onClick = {
+                }
+            )
         }
     }
 }
@@ -123,11 +131,13 @@ fun DashboardItem(
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                titleText?.let {
+                if (titleText != null) {
                     Text(
                         text = titleText,
                         modifier = Modifier.weight(1f)
                     )
+                } else {
+                    Spacer(modifier = Modifier.weight(1f))
                 }
                 if (onClick != null) {
                     Icon(
