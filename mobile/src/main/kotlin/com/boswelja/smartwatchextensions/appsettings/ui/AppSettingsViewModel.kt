635d12012a69b5eaf1fb1d907aef0c3791537277
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-27 22:34:12 +1200

    Add values for QS Tile watch picker

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/appsettings/ui/AppSettingsViewModel.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/appsettings/ui/AppSettingsViewModel.kt
@@ -8,23 +8,41 @@ import androidx.lifecycle.viewModelScope
 import com.boswelja.smartwatchextensions.analytics.Analytics
 import com.boswelja.smartwatchextensions.appsettings.Settings
 import com.boswelja.smartwatchextensions.appsettings.appSettingsStore
+import com.boswelja.smartwatchextensions.watchmanager.WatchManager
+import com.boswelja.watchconnection.core.Watch
+import java.util.UUID
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class AppSettingsViewModel internal constructor(
     application: Application,
     private val dataStore: DataStore<Settings>,
-    private val analytics: Analytics
+    private val analytics: Analytics,
+    private val watchManager: WatchManager
 ) : AndroidViewModel(application) {
 
     val analyticsEnabled = dataStore.data.map { it.analyticsEnabled }
     val appTheme = dataStore.data.map { it.appTheme }
+    val registeredWatches = watchManager.registeredWatches
+    @ExperimentalCoroutinesApi
+    val qsTilesWatch = dataStore.data.map {
+        it.qsTileWatchId
+    }.flatMapLatest { idString ->
+        if (idString.isNotEmpty()) {
+            watchManager.getWatchById(UUID.fromString(idString))
+        } else {
+            watchManager.registeredWatches.map { it.firstOrNull() }
+        }
+    }
 
     @Suppress("unused")
     constructor(application: Application) : this(
         application,
         application.appSettingsStore,
-        Analytics()
+        Analytics(),
+        WatchManager.getInstance(application)
     )
 
     fun setAnalyticsEnabled(analyticsEnabled: Boolean) {
@@ -48,6 +66,14 @@ class AppSettingsViewModel internal constructor(
                     Settings.Theme.DARK -> AppCompatDelegate.MODE_NIGHT_YES
                 }
             )
+        }
+    }
+
+    fun setQSTilesWatch(watch: Watch) {
+        viewModelScope.launch {
+            dataStore.updateData {
+                it.copy(qsTileWatchId = watch.id.toString())
+            }
         }
     }
 }
