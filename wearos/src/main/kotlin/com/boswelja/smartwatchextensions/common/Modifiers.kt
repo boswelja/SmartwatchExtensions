b41845a25ba487d7fc84606a556ba80e72ca101e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-02 17:20:42 +1200

    Slight optimisations for rotary input

--- wearos/src/main/kotlin/com/boswelja/smartwatchextensions/common/Modifiers.kt
+++ wearos/src/main/kotlin/com/boswelja/smartwatchextensions/common/Modifiers.kt
@@ -8,27 +8,26 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.InputDeviceCompat
 import androidx.core.view.MotionEventCompat
 import androidx.core.view.ViewConfigurationCompat
-import timber.log.Timber
 
 fun Modifier.rotaryInput(
     onRotaryInput: (delta: Float) -> Unit
 ) = composed {
     val rootView = LocalView.current
-    val context = rootView.context
+    val viewConfig = ViewConfiguration.get(rootView.context)
+    val verticalScrollScale = ViewConfigurationCompat.getScaledVerticalScrollFactor(
+        viewConfig, rootView.context
+    )
+
     rootView.setOnGenericMotionListener { _, event ->
-        Timber.d("Got pointer input")
         if (event.action == MotionEvent.ACTION_SCROLL &&
             event.isFromSource(InputDeviceCompat.SOURCE_ROTARY_ENCODER)
         ) {
-            Timber.d("Handling scroll")
-            val delta = -event.getAxisValue(MotionEventCompat.AXIS_SCROLL) *
-                ViewConfigurationCompat.getScaledVerticalScrollFactor(
-                    ViewConfiguration.get(context), context
-                )
+            val delta = -event.getAxisValue(MotionEventCompat.AXIS_SCROLL) * verticalScrollScale
             onRotaryInput(delta)
         }
         true
     }
+
     rootView.isFocusable = true
     rootView.requestFocus()
     this
