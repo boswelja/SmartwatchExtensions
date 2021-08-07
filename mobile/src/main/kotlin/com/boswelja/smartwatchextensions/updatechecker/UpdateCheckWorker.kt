29f620738e3f811fc42be359e7ad3ec24847de32
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-08 00:34:30 +1200

    Create UpdateChecker.kt interface and UpdateCheckWorker.kt The goal is to move update checks away from MainActivity, into their own worker that we can schedule.

---
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/updatechecker/UpdateCheckWorker.kt
@@ -0,0 +1,66 @@
+package com.boswelja.smartwatchextensions.updatechecker
+
+import android.content.Context
+import androidx.work.Constraints
+import androidx.work.CoroutineWorker
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import com.boswelja.smartwatchextensions.R
+import com.boswelja.smartwatchextensions.messages.Message
+import com.boswelja.smartwatchextensions.messages.Priority
+import com.boswelja.smartwatchextensions.messages.sendMessage
+import java.util.concurrent.TimeUnit
+
+/**
+ * A [CoroutineWorker] to handle checking for updates and notifying the user.
+ */
+class UpdateCheckWorker(
+    appContext: Context,
+    workerParams: WorkerParameters
+) : CoroutineWorker(appContext, workerParams) {
+    override suspend fun doWork(): Result {
+        val updater = GooglePlayUpdateChecker(applicationContext)
+        if (updater.isNewVersionAvailable()) {
+            val message = Message(
+                icon = Message.Icon.UPDATE,
+                title = applicationContext.getString(R.string.update_available_title),
+                text = applicationContext.getString(R.string.update_available_text),
+                action = Message.Action.INSTALL_UPDATE
+            )
+            applicationContext.sendMessage(message, Priority.HIGH)
+        }
+        return Result.success()
+    }
+
+    companion object {
+        private const val WorkerName = "update-checker"
+
+        /**
+         * Schedule the [UpdateCheckWorker] to be run daily with constraints.
+         */
+        fun schedule(context: Context) {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .build()
+            val request = PeriodicWorkRequestBuilder<UpdateCheckWorker>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
+            WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+                WorkerName,
+                ExistingPeriodicWorkPolicy.REPLACE,
+                request
+            )
+        }
+
+        /**
+         * Cancel any scheduled [UpdateCheckWorker].
+         */
+        fun cancel(context: Context) {
+            WorkManager.getInstance(context).cancelUniqueWork(WorkerName)
+        }
+    }
+}
