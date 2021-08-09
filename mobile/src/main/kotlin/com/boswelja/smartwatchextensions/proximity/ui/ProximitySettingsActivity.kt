7fb317fc701a5f4d4248e818a4d99b9c5fc51d31
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-09 14:27:53 +1200

    Put separation alert settings in a card

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/proximity/ui/ProximitySettingsActivity.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/proximity/ui/ProximitySettingsActivity.kt
@@ -3,9 +3,10 @@ package com.boswelja.smartwatchextensions.proximity.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,20 +16,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.common.ui.AppTheme
+import com.boswelja.smartwatchextensions.common.ui.Card
+import com.boswelja.smartwatchextensions.common.ui.CardHeader
 import com.boswelja.smartwatchextensions.common.ui.SwitchSetting
 import com.boswelja.smartwatchextensions.common.ui.UpNavigationWatchPickerAppBar
 import com.boswelja.smartwatchextensions.watchmanager.WatchManager
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 
 class ProximitySettingsActivity : AppCompatActivity() {
 
-    @ExperimentalCoroutinesApi
-    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -58,36 +60,59 @@ class ProximitySettingsActivity : AppCompatActivity() {
     }
 }
 
-@ExperimentalMaterialApi
-@ExperimentalCoroutinesApi
 @Composable
-fun ProximitySettingsScreen(modifier: Modifier = Modifier) {
+fun ProximitySettingsScreen(
+    modifier: Modifier = Modifier,
+    contentPadding: Dp = 16.dp
+) {
+    Column(
+        modifier = modifier.padding(contentPadding),
+        verticalArrangement = Arrangement.spacedBy(contentPadding)
+    ) {
+        SeparationSettingsCard(Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun SeparationSettingsCard(modifier: Modifier = Modifier) {
     val scope = rememberCoroutineScope()
     val viewModel = viewModel<ProximitySettingsViewModel>()
     val phoneProximityNotiEnabled by viewModel.phoneProximityNotiSetting
         .collectAsState(false, Dispatchers.IO)
     val watchProximityNotiEnabled by viewModel.watchProximityNotiSetting
         .collectAsState(false, Dispatchers.IO)
-    Column(modifier) {
-        SwitchSetting(
-            label = { Text(stringResource(R.string.proximity_phone_noti_title)) },
-            summary = { Text(stringResource(R.string.proximity_phone_noti_summary)) },
-            checked = phoneProximityNotiEnabled,
-            onCheckChanged = { isChecked ->
-                scope.launch {
-                    viewModel.setPhoneProximityNotiEnabled(isChecked)
+
+    Card(
+        modifier = modifier,
+        header = {
+            CardHeader(
+                title = {
+                    Text(stringResource(R.string.separation_alerts_title))
                 }
-            }
-        )
-        SwitchSetting(
-            label = { Text(stringResource(R.string.proximity_watch_noti_title)) },
-            summary = { Text(stringResource(R.string.proximity_watch_noti_summary)) },
-            checked = watchProximityNotiEnabled,
-            onCheckChanged = { isChecked ->
-                scope.launch {
-                    viewModel.setWatchProximityNotiEnabled(isChecked)
+            )
+        }
+    ) {
+        Column {
+            SwitchSetting(
+                label = { Text(stringResource(R.string.proximity_phone_noti_title)) },
+                summary = { Text(stringResource(R.string.proximity_phone_noti_summary)) },
+                checked = phoneProximityNotiEnabled,
+                onCheckChanged = { isChecked ->
+                    scope.launch {
+                        viewModel.setPhoneProximityNotiEnabled(isChecked)
+                    }
                 }
-            }
-        )
+            )
+            SwitchSetting(
+                label = { Text(stringResource(R.string.proximity_watch_noti_title)) },
+                summary = { Text(stringResource(R.string.proximity_watch_noti_summary)) },
+                checked = watchProximityNotiEnabled,
+                onCheckChanged = { isChecked ->
+                    scope.launch {
+                        viewModel.setWatchProximityNotiEnabled(isChecked)
+                    }
+                }
+            )
+        }
     }
 }
