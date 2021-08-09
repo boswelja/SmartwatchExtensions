e0b05c5225c2241aed6980742bf4fd317ec6971b
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-09 15:05:01 +1200

    Drop ProximitySettingsActivity.kt The screen is already in the nav graph

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/proximity/ui/ProximitySettingsScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/proximity/ui/ProximitySettingsScreen.kt
@@ -1,18 +1,13 @@
 package com.boswelja.smartwatchextensions.proximity.ui
 
-import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -20,45 +15,11 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.boswelja.smartwatchextensions.R
-import com.boswelja.smartwatchextensions.common.ui.AppTheme
 import com.boswelja.smartwatchextensions.common.ui.Card
 import com.boswelja.smartwatchextensions.common.ui.CardHeader
 import com.boswelja.smartwatchextensions.common.ui.SwitchSetting
-import com.boswelja.smartwatchextensions.common.ui.UpNavigationWatchPickerAppBar
-import com.boswelja.smartwatchextensions.watchmanager.WatchManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
-class ProximitySettingsActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            val watchManager = remember {
-                WatchManager.getInstance(this)
-            }
-            val selectedWatch by watchManager.selectedWatch.collectAsState(null, Dispatchers.IO)
-            val registeredWatches by watchManager.registeredWatches
-                .collectAsState(emptyList(), Dispatchers.IO)
-
-            AppTheme {
-                Scaffold(
-                    topBar = {
-                        UpNavigationWatchPickerAppBar(
-                            selectedWatch = selectedWatch,
-                            watches = registeredWatches,
-                            onWatchSelected = { watchManager.selectWatchById(it.id) },
-                            onNavigateUp = { finish() }
-                        )
-                    }
-                ) {
-                    ProximitySettingsScreen(Modifier.padding(it))
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun ProximitySettingsScreen(
