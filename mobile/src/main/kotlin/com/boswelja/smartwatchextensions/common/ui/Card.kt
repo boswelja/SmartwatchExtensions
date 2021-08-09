7b5fb461d5b8b2635ed091751fb53938a2ef3847
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-09 23:08:23 +1200

    Create ExpandableCard

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/common/ui/Card.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/common/ui/Card.kt
@@ -1,6 +1,14 @@
 package com.boswelja.smartwatchextensions.common.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,9 +17,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.contentColorFor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,7 +59,11 @@ fun CardHeader(
             }
         }
         if (action != null) {
-            Spacer(Modifier.weight(1f).widthIn(min = 16.dp))
+            Spacer(
+                Modifier
+                    .weight(1f)
+                    .widthIn(min = 16.dp)
+            )
             action()
         }
     }
@@ -78,4 +94,52 @@ fun Card(
             content()
         }
     }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun ExpandableCard(
+    modifier: Modifier = Modifier,
+    title: @Composable () -> Unit,
+    shape: Shape = MaterialTheme.shapes.medium,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    border: BorderStroke? = null,
+    elevation: Dp = 1.dp,
+    expanded: Boolean,
+    toggleExpanded: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Card(
+        modifier = modifier,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        border = border,
+        elevation = elevation,
+        header = {
+            CardHeader(
+                modifier = Modifier.clickable(onClick = toggleExpanded),
+                title = title,
+                action = {
+                    Crossfade(targetState = expanded) {
+                        if (it) {
+                            Icon(Icons.Outlined.ExpandLess, null)
+                        } else {
+                            Icon(Icons.Outlined.ExpandMore, null)
+                        }
+                    }
+                }
+            )
+        },
+        content = {
+            AnimatedVisibility(
+                visible = expanded,
+                enter = expandVertically() + fadeIn(),
+                exit = shrinkVertically() + fadeOut()
+            ) {
+                content()
+            }
+        }
+    )
 }
