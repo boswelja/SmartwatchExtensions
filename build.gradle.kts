7c9f0f089b55ddbb3b5f1c9903a3b5d1edea71eb
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-10 13:44:31 +1200

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha06")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha07")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.9")
         classpath("com.squareup.wire:wire-gradle-plugin:3.7.0")
