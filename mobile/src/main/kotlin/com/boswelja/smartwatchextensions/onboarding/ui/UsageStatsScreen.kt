15fd7215c36c13a68d2b18aee0a229be29796964
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-14 13:09:47 +1200

    Put usage stats info in a card

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/onboarding/ui/UsageStatsScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/onboarding/ui/UsageStatsScreen.kt
@@ -2,8 +2,7 @@ package com.boswelja.smartwatchextensions.onboarding.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.toggleable
@@ -12,7 +11,6 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.ListItem
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -28,11 +26,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.analytics.getAnalytics
+import com.boswelja.smartwatchextensions.common.ui.Card
+import com.boswelja.smartwatchextensions.common.ui.CardHeader
 import com.boswelja.smartwatchextensions.settings.appSettingsStore
 import kotlinx.coroutines.launch
 
@@ -44,13 +43,9 @@ fun UsageStatsScreen(
     onShowPrivacyPolicy: () -> Unit,
     onNavigateTo: (OnboardingDestination) -> Unit
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    val context = LocalContext.current
-    val analytics = getAnalytics()
-    var checkboxChecked by remember { mutableStateOf(true) }
     Column(
         modifier = modifier.padding(contentPadding),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.spacedBy(contentPadding, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
@@ -58,50 +53,67 @@ fun UsageStatsScreen(
             null,
             Modifier.size(180.dp)
         )
-        Text(
-            stringResource(R.string.share_usage_title),
-            style = MaterialTheme.typography.h4,
-            modifier = Modifier.padding(top = contentPadding)
+        UsageStatsCard(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = contentPadding,
+            onShowPrivacyPolicy = onShowPrivacyPolicy
         )
-        Text(
-            stringResource(R.string.share_usage_desc),
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.body1
-        )
-        ListItem(
-            text = { Text(stringResource(R.string.share_usage_title)) },
-            trailing = {
-                Checkbox(
-                    checked = checkboxChecked,
-                    onCheckedChange = null // null recommended for accessibility with screenreaders
-                )
-            },
-            modifier = Modifier
-                .toggleable(
-                    value = checkboxChecked,
-                    onValueChange = {
-                        checkboxChecked = it
-                        coroutineScope.launch {
-                            analytics.setAnalyticsEnabled(it)
-                            context.appSettingsStore.updateData { settings ->
-                                settings.copy(analyticsEnabled = it)
-                            }
-                        }
-                    }
-                )
-                .padding(contentPadding)
-        )
-        OutlinedButton(
-            onClick = onShowPrivacyPolicy,
-            modifier = Modifier.padding(top = contentPadding)
-        ) {
-            Text(stringResource(R.string.about_priv_policy_title))
-        }
-        Spacer(Modifier.height(contentPadding))
         ExtendedFloatingActionButton(
             text = { Text(stringResource(R.string.button_next)) },
             icon = { Icon(Icons.Outlined.NavigateNext, null) },
             onClick = { onNavigateTo(OnboardingDestination.REGISTER_WATCHES) }
         )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun UsageStatsCard(
+    modifier: Modifier = Modifier,
+    contentPadding: Dp,
+    onShowPrivacyPolicy: () -> Unit
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val analytics = getAnalytics()
+    var checkboxChecked by remember { mutableStateOf(true) }
+
+    Card(
+        modifier = modifier,
+        header = {
+            CardHeader(
+                title = { Text(stringResource(R.string.share_usage_title)) }
+            )
+        }
+    ) {
+        Column(Modifier.padding(contentPadding)) {
+            Text(stringResource(R.string.share_usage_desc))
+            ListItem(
+                text = { Text(stringResource(R.string.share_usage_title)) },
+                trailing = {
+                    Checkbox(checked = checkboxChecked, onCheckedChange = null)
+                },
+                modifier = Modifier
+                    .toggleable(
+                        value = checkboxChecked,
+                        onValueChange = {
+                            checkboxChecked = it
+                            coroutineScope.launch {
+                                analytics.setAnalyticsEnabled(it)
+                                context.appSettingsStore.updateData { settings ->
+                                    settings.copy(analyticsEnabled = it)
+                                }
+                            }
+                        }
+                    )
+                    .padding(contentPadding)
+            )
+            OutlinedButton(
+                onClick = onShowPrivacyPolicy,
+                modifier = Modifier.padding(top = contentPadding)
+            ) {
+                Text(stringResource(R.string.about_priv_policy_title))
+            }
+        }
     }
 }
