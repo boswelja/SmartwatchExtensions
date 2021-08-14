8e51b20dd582d647dc3eaab285a1536766cc8ebc
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-14 12:41:21 +1200

    Switch OnboardingActivity.kt to navigation-compose

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/onboarding/ui/OnboardingActivity.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/onboarding/ui/OnboardingActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.FabPosition
@@ -14,61 +13,48 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.NavigateNext
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.common.startActivity
 import com.boswelja.smartwatchextensions.common.ui.AppTheme
 import com.boswelja.smartwatchextensions.common.ui.UpNavigationAppBar
-import com.boswelja.smartwatchextensions.main.ui.MainActivity
 import com.boswelja.smartwatchextensions.watchmanager.ui.register.RegisterWatchScreen
 
 class OnboardingActivity : AppCompatActivity() {
-
-    private var currentDestination by mutableStateOf(Destination.WELCOME)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val navController = rememberNavController()
+
             AppTheme {
                 Scaffold(
                     topBar = {
-                        UpNavigationAppBar(
-                            onNavigateUp = { onBackPressed() }
-                        )
+                        UpNavigationAppBar(onNavigateUp = navController::navigateUp)
                     },
                     floatingActionButtonPosition = FabPosition.End,
                     floatingActionButton = {
                         ExtendedFloatingActionButton(
                             text = { Text(stringResource(R.string.button_next)) },
                             icon = { Icon(Icons.Outlined.NavigateNext, null) },
-                            onClick = { navigateNext() }
+                            onClick = { /* TODO */ }
                         )
                     }
                 ) {
                     OnboardingScreen(
                         modifier = Modifier.fillMaxSize(),
-                        currentDestination = currentDestination
+                        navController = navController
                     )
                 }
-            }
-        }
-    }
-
-    private fun navigateNext() {
-        when (currentDestination) {
-            Destination.WELCOME -> currentDestination = Destination.SHARE_USAGE_STATS
-            Destination.SHARE_USAGE_STATS -> currentDestination = Destination.REGISTER_WATCHES
-            Destination.REGISTER_WATCHES -> {
-                startActivity<MainActivity>()
-                finish()
             }
         }
     }
@@ -77,39 +63,40 @@ class OnboardingActivity : AppCompatActivity() {
     fun OnboardingScreen(
         modifier: Modifier = Modifier,
         contentPadding: Dp = 16.dp,
-        currentDestination: Destination
+        navController: NavHostController
     ) {
-        Crossfade(targetState = currentDestination) {
-            when (it) {
-                Destination.WELCOME -> {
-                    WelcomeScreen(modifier = modifier, contentPadding = contentPadding)
-                }
-                Destination.SHARE_USAGE_STATS -> {
-                    UsageStatsScreen(
-                        modifier = modifier,
-                        contentPadding = contentPadding,
-                        onShowPrivacyPolicy = {
-                            startActivity { intent ->
-                                intent.action = Intent.ACTION_VIEW
-                                intent.data = getString(R.string.privacy_policy_url).toUri()
-                                intent
-                            }
+        NavHost(
+            navController = navController,
+            startDestination = OnboardingDestination.WELCOME.route
+        ) {
+            composable(OnboardingDestination.WELCOME.route) {
+                WelcomeScreen(modifier = modifier, contentPadding = contentPadding)
+            }
+            composable(OnboardingDestination.SHARE_USAGE_STATS.route) {
+                UsageStatsScreen(
+                    modifier = modifier,
+                    contentPadding = contentPadding,
+                    onShowPrivacyPolicy = {
+                        startActivity { intent ->
+                            intent.action = Intent.ACTION_VIEW
+                            intent.data = getString(R.string.privacy_policy_url).toUri()
+                            intent
                         }
-                    )
-                }
-                Destination.REGISTER_WATCHES -> {
-                    RegisterWatchScreen(
-                        modifier = modifier,
-                        contentPadding = contentPadding
-                    )
-                }
+                    }
+                )
+            }
+            composable(OnboardingDestination.REGISTER_WATCHES.route) {
+                RegisterWatchScreen(
+                    modifier = modifier,
+                    contentPadding = contentPadding
+                )
             }
         }
     }
+}
 
-    enum class Destination {
-        WELCOME,
-        SHARE_USAGE_STATS,
-        REGISTER_WATCHES
-    }
+enum class OnboardingDestination(val route: String) {
+    WELCOME("welcome"),
+    SHARE_USAGE_STATS("usage-stats"),
+    REGISTER_WATCHES("register-watches")
 }
