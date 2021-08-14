1fe8013da21cc2c868a61fd263d02a870324d480
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-14 13:15:58 +1200

    Only show the finish button when a watch has been registered

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/onboarding/ui/OnboardingScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/onboarding/ui/OnboardingScreen.kt
@@ -1,6 +1,12 @@
 package com.boswelja.smartwatchextensions.onboarding.ui
 
 import android.content.Intent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExtendedFloatingActionButton
@@ -9,6 +15,10 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -23,6 +33,7 @@ import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.common.startActivity
 import com.boswelja.smartwatchextensions.watchmanager.ui.register.RegisterWatchScreen
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun OnboardingScreen(
     modifier: Modifier = Modifier,
@@ -58,16 +69,30 @@ fun OnboardingScreen(
         }
         composable(OnboardingDestination.REGISTER_WATCHES.route) {
             Box {
+                var finishVisible by rememberSaveable {
+                    mutableStateOf(false)
+                }
                 RegisterWatchScreen(
                     modifier = modifier.padding(bottom = 64.dp),
-                    contentPadding = contentPadding
+                    contentPadding = contentPadding,
+                    onWatchRegistered = {
+                        finishVisible = true
+                    }
                 )
-                ExtendedFloatingActionButton(
-                    modifier = Modifier.padding(contentPadding).align(Alignment.BottomCenter),
-                    text = { Text(stringResource(R.string.button_finish)) },
-                    icon = { Icon(Icons.Outlined.Check, null) },
-                    onClick = onFinished
-                )
+                AnimatedVisibility(
+                    modifier = Modifier.align(Alignment.BottomCenter),
+                    visible = finishVisible,
+                    enter = expandIn() + fadeIn(),
+                    exit = shrinkOut() + fadeOut()
+                ) {
+                    ExtendedFloatingActionButton(
+                        modifier = Modifier
+                            .padding(contentPadding),
+                        text = { Text(stringResource(R.string.button_finish)) },
+                        icon = { Icon(Icons.Outlined.Check, null) },
+                        onClick = onFinished
+                    )
+                }
             }
         }
     }
