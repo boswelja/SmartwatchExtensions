cc449008a014b2bd2d1eb639959280e40461ca80
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-19 21:57:33 +1200

    Improve roundScreenPadding modifier It now takes separate PaddingValues for both round and square screens, and checks isRoundScreen on it's own There's now proper defaults for round and square screen paddings set up

--- wearos/src/main/kotlin/com/boswelja/smartwatchextensions/common/ui/Insets.kt
+++ wearos/src/main/kotlin/com/boswelja/smartwatchextensions/common/ui/Insets.kt
@@ -5,30 +5,47 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
-object InsetDefaults {
-    val RoundScreenInset = 48.dp
+object RoundScreenDefaults {
+    val VerticalPadding = 64.dp
+    val HorizontalPadding = 8.dp
+}
+
+object SquareScreenDefaults {
+    val VerticalPadding = 8.dp
+    val HorizontalPadding = 8.dp
 }
 
 /**
- * Applies the given [PaddingValues] only if the device has a round screen.
- * @param paddingValues See [PaddingValues].
+ * Applies different [PaddingValues] based on [isRoundScreen].
+ * @param roundPaddingValues The [PaddingValues] to be applied on devices with a round screen.
+ * @param squarePaddingValues The [PaddingValues] to be applied on devices with a square screen.
  */
 @Stable
 fun Modifier.roundScreenPadding(
-    isScreenRound: Boolean,
-    paddingValues: PaddingValues
-): Modifier {
-    return if (isScreenRound) {
-        padding(paddingValues)
+    roundPaddingValues: PaddingValues = PaddingValues(
+        vertical = RoundScreenDefaults.VerticalPadding,
+        horizontal = RoundScreenDefaults.HorizontalPadding
+    ),
+    squarePaddingValues: PaddingValues = PaddingValues(
+        vertical = SquareScreenDefaults.VerticalPadding,
+        horizontal = SquareScreenDefaults.HorizontalPadding
+    )
+): Modifier = composed {
+    if (isRoundScreen()) {
+        padding(roundPaddingValues)
     } else {
-        this
+        padding(squarePaddingValues)
     }
 }
 
+/**
+ * Returns the value of [android.content.res.Configuration.isScreenRound].
+ */
 @Composable
-fun isScreenRound(): Boolean {
+fun isRoundScreen(): Boolean {
     return LocalContext.current.resources.configuration.isScreenRound
 }
