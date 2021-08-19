9ac42cb3674c324c4a6191dba68176544912dbe3
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-19 20:45:49 +1200

    Move MainScreen to it's own Composable

--- wearos/src/main/kotlin/com/boswelja/smartwatchextensions/main/ui/MainActivity.kt
+++ wearos/src/main/kotlin/com/boswelja/smartwatchextensions/main/ui/MainActivity.kt
@@ -3,30 +3,39 @@ package com.boswelja.smartwatchextensions.main.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.boswelja.smartwatchextensions.common.ui.AppTheme
 import com.boswelja.smartwatchextensions.extensions.ui.ExtensionsScreen
 import com.boswelja.smartwatchextensions.onboarding.ui.OnboardingScreen
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class MainActivity : ComponentActivity() {
 
-    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel: MainViewModel = viewModel()
-            val isRegistered by viewModel.isRegistered.collectAsState(true, Dispatchers.IO)
             AppTheme {
-                if (isRegistered) {
-                    ExtensionsScreen()
-                } else {
-                    OnboardingScreen()
-                }
+                MainScreen(Modifier.fillMaxSize())
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+    val viewModel: MainViewModel = viewModel()
+    val isRegistered by viewModel.isRegistered.collectAsState(true, Dispatchers.IO)
+
+    if (isRegistered) {
+        ExtensionsScreen()
+    } else {
+        OnboardingScreen()
     }
 }
