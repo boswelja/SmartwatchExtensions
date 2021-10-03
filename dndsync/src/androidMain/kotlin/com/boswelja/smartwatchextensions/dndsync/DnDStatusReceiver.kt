3705ee0e614d91778c31d2c5f28280248364d314
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-10-03 23:20:55 +1300

    Move DnDStatusReceiver.kt into it's feature module

--- dndsync/src/androidMain/kotlin/com/boswelja/smartwatchextensions/dndsync/DnDStatusReceiver.kt
+++ dndsync/src/androidMain/kotlin/com/boswelja/smartwatchextensions/dndsync/DnDStatusReceiver.kt
@@ -2,12 +2,11 @@ package com.boswelja.smartwatchextensions.dndsync
 
 import android.app.NotificationManager
 import android.content.Context
-import androidx.core.content.getSystemService
 import com.boswelja.watchconection.common.message.MessageReceiver
 import com.boswelja.watchconnection.common.message.ReceivedMessage
 
 class DnDStatusReceiver : MessageReceiver<Boolean>(DnDStatusSerializer) {
     override suspend fun onMessageReceived(context: Context, message: ReceivedMessage<Boolean>) {
-        context.getSystemService<NotificationManager>()?.setDnD(message.data)
+        context.getSystemService(NotificationManager::class.java).setDnD(message.data)
     }
 }
