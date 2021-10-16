f298a5e341aedab91de96d7da8fc63a4a7fae604
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-10-16 16:14:27 +1300

    Move CHECK_WATCH_REGISTERED_PATH into devicemanagement/common

--- common/src/main/kotlin/com/boswelja/smartwatchextensions/common/connection/Messages.kt
+++ common/src/main/kotlin/com/boswelja/smartwatchextensions/common/connection/Messages.kt
@@ -4,6 +4,5 @@ package com.boswelja.smartwatchextensions.common.connection
  * Contains definitions for message paths.
  */
 object Messages {
-    const val CHECK_WATCH_REGISTERED_PATH = "/check_watch_registered"
     const val WATCH_REGISTERED_PATH = "/watch_registered"
 }
--- devicemanagement/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/devicemanagement/Paths.kt
+++ devicemanagement/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/devicemanagement/Paths.kt
@@ -3,3 +3,5 @@ package com.boswelja.smartwatchextensions.devicemanagement
 const val LAUNCH_APP = "/launch_app"
 const val RESET_APP = "/reset_app"
 const val REQUEST_UPDATE_CAPABILITIES = "/update_capabilities"
+
+const val CHECK_WATCH_REGISTERED_PATH = "/check_watch_registered"
