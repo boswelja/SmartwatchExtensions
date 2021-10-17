b409129aa1d8cd9f5bd849e171db9b966e8344d3
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-10-17 22:11:45 +1300

    Fix a bug in PackageNameSerializer.kt where the deserialized string may be invalid

--- appmanager/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/appmanager/PackageNameSerializer.kt
+++ appmanager/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/appmanager/PackageNameSerializer.kt
@@ -8,6 +8,12 @@ object PackageNameSerializer : MessageSerializer<String>(
         REQUEST_UNINSTALL_PACKAGE
     )
 ) {
-    override suspend fun deserialize(bytes: ByteArray): String = bytes.decodeToString()
+    override suspend fun deserialize(bytes: ByteArray): String {
+        val decodedString = bytes.decodeToString()
+        check(decodedString.length > 1 && decodedString.contains('.')) {
+            "$decodedString not a valid package name"
+        }
+        return decodedString
+    }
     override suspend fun serialize(data: String): ByteArray = data.encodeToByteArray()
 }
