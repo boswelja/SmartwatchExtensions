96bd49c0f7f9bd7b81de5b542dd7d6f25c8962a6
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-10-25 12:23:06 +1300

    Detekt compliance for messages

--- messages/mobile/src/commonMain/kotlin/com/boswelja/smartwatchextensions/messages/DisplayMessage.kt
+++ messages/mobile/src/commonMain/kotlin/com/boswelja/smartwatchextensions/messages/DisplayMessage.kt
@@ -1,5 +1,15 @@
 package com.boswelja.smartwatchextensions.messages
 
+/**
+ * A data class containing all the data needed to display a Message.
+ * @param id The message ID.
+ * @param sourceUid The device UID that sent this message, or null for the local device.
+ * @param icon The [Message.Icon] to display.
+ * @param title The message title.
+ * @param text The message text.
+ * @param action The [Message.Action] to display.
+ * @param timestamp The timestamp in milliseconds the message was posted at.
+ */
 data class DisplayMessage(
     val id: Long,
     val sourceUid: String?,
