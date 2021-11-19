97b1f928f46ae66f7935937ce8e2ce6922eb2f39
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-11-19 23:50:06 +1300

    Move AppInfo.kt to appmanager/mobile

--- appmanager/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/appmanager/ui/Button.kt
+++ appmanager/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/appmanager/ui/Button.kt
@@ -1,4 +1,4 @@
-package com.boswelja.smartwatchextensions.common.ui
+package com.boswelja.smartwatchextensions.appmanager.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
