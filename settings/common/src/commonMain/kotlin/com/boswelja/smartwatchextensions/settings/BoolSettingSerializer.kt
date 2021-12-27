7cc12604b8c004bd6860340251a5e80b12364f93
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-12-27 14:51:25 +1300

    Switch settings to kotlinx-serialization

--- settings/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/settings/BoolSettingSerializer.kt
+++ settings/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/settings/BoolSettingSerializer.kt
@@ -1,17 +1,22 @@
 package com.boswelja.smartwatchextensions.settings
 
 import com.boswelja.watchconnection.serialization.MessageSerializer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
 
 /**
  * A [MessageSerializer] for handling [BoolSetting].
  */
+@OptIn(ExperimentalSerializationApi::class)
 object BoolSettingSerializer : MessageSerializer<BoolSetting> {
 
     override val messagePaths: Set<String> = setOf(UPDATE_BOOL_PREFERENCE)
 
     override suspend fun deserialize(
         bytes: ByteArray?
-    ): BoolSetting = BoolSetting.ADAPTER.decode(bytes!!)
+    ): BoolSetting = ProtoBuf.decodeFromByteArray(bytes!!)
 
-    override suspend fun serialize(data: BoolSetting): ByteArray = data.encode()
+    override suspend fun serialize(data: BoolSetting): ByteArray = ProtoBuf.encodeToByteArray(data)
 }
