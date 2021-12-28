926b51957853a7c9f9032a3c29a4f18ba62cb45d
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-12-28 16:35:29 +1300

    Prep batterysync/mobile for Compose

--- batterysync/mobile/build.gradle.kts
+++ batterysync/mobile/build.gradle.kts
@@ -26,6 +26,9 @@ kotlin {
                 implementation(libs.sqldelight.android)
                 implementation(libs.androidx.work.ktx)
                 implementation(libs.koin.android)
+                implementation(libs.koin.compose)
+                implementation(libs.bundles.lifecycle)
+                implementation(libs.bundles.compose.mobile)
             }
         }
         val androidTest by getting {
@@ -36,8 +39,36 @@ kotlin {
     }
 }
 
+android {
+    buildFeatures.compose = true
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+}
+
 sqldelight {
     database("BatteryStatsDatabase") {
         packageName = "com.boswelja.smartwatchextensions.batterysync.database"
+    }
+}
+
+// Workaround for https://youtrack.jetbrains.com/issue/KT-38694
+configurations {
+    create("composeCompiler") {
+        isCanBeConsumed = false
+    }
+}
+dependencies {
+    "composeCompiler"("androidx.compose.compiler:compiler:${libs.versions.composeCompiler.get()}")
+}
+android {
+    afterEvaluate {
+        val composeCompilerJar = configurations["composeCompiler"]
+            .resolve()
+            .singleOrNull()
+            ?: error("Missing Compose compiler")
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions.freeCompilerArgs += listOf("-Xuse-ir", "-Xplugin=$composeCompilerJar")
+        }
     }
 }
