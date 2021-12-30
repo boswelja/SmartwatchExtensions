201a36f13c077d2456d3abaec8e4a2a5cb1d0147
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-12-31 00:27:20 +1300

    Consider battery sync being disabled when creating PhoneBatteryComplicationProvider.kt

--- batterysync/wear/src/androidMain/kotlin/com/boswelja/smartwatchextensions/batterysync/PhoneBatteryComplicationProvider.kt
+++ batterysync/wear/src/androidMain/kotlin/com/boswelja/smartwatchextensions/batterysync/PhoneBatteryComplicationProvider.kt
@@ -23,25 +23,41 @@ import org.koin.android.ext.android.inject
  */
 class PhoneBatteryComplicationProvider : SuspendingComplicationDataSourceService() {
 
+    private val batterySyncStateRepository: BatterySyncStateRepository by inject()
     private val batteryStatsRepository: BatteryStatsRepository by inject()
 
+    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+        return createComplicationDataFor(PREVIEW_PERCENT, type)
+    }
+
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
-        val batteryStats = batteryStatsRepository.getPhoneBatteryStats()
-            .map { it.percent }
+        val batterySyncEnabled = batterySyncStateRepository.getBatterySyncState()
+            .map { it.batterySyncEnabled }
             .first()
-        return createComplicationDataFor(batteryStats, request.complicationType)
+        val complicationData = if (batterySyncEnabled) {
+            val batteryStats = batteryStatsRepository.getPhoneBatteryStats()
+                .map { it.percent }
+                .first()
+            createComplicationDataFor(batteryStats, request.complicationType)
+        } else {
+            createDisabledComplicationData(request.complicationType)
+        }
+
+        return complicationData
     }
 
     /**
-     * Create a [ComplicationData] for a complication with given data.
+     * Create a [ComplicationData] for the given battery percent and type.
      * @param percent The battery percent to create [ComplicationData] for.
      * @param type The [ComplicationType] of the required [ComplicationData].
      * @return The created [ComplicationData], or null if type was invalid for this complication.
      */
     private fun createComplicationDataFor(percent: Int, type: ComplicationType): ComplicationData? {
-        val text =
-            if (percent > 0) String.format(getString(R.string.battery_percent), percent)
-            else getString(R.string.battery_percent_unknown)
+        val text = if (percent > 0) {
+            getString(R.string.battery_percent, percent.toString())
+        } else {
+            getString(R.string.battery_percent_unknown)
+        }
         val icon = MonochromaticImage.Builder(
             Icon.createWithResource(this, getBatteryDrawableRes(percent))
         ).build()
@@ -68,8 +84,28 @@ class PhoneBatteryComplicationProvider : SuspendingComplicationDataSourceService
         }
     }
 
-    override fun getPreviewData(type: ComplicationType): ComplicationData? {
-        return createComplicationDataFor(PREVIEW_PERCENT, type)
+    private fun createDisabledComplicationData(type: ComplicationType): ComplicationData? {
+        val text = PlainComplicationText
+            .Builder(getString(R.string.battery_sync_complication_disabled))
+            .build()
+        val icon = MonochromaticImage.Builder(
+            Icon.createWithResource(this, getBatteryDrawableRes(-1))
+        ).build()
+
+        return when (type) {
+            ComplicationType.RANGED_VALUE -> {
+                RangedValueComplicationData.Builder(0f, 0f, 1f, text)
+                    .setMonochromaticImage(icon)
+                    .setText(text)
+                    .build()
+            }
+            ComplicationType.SHORT_TEXT -> {
+                ShortTextComplicationData.Builder(text, text)
+                    .setMonochromaticImage(icon)
+                    .build()
+            }
+            else -> null
+        }
     }
 
     companion object {
