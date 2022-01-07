d8def3834d8616a7ebad0b129dccb7748e411662
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-01-07 23:36:05 +1300

    Update Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("com.squareup.sqldelight") version libs.versions.sqldelight.get() apply false
     id("com.google.gms.google-services") version "4.3.10" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.18.1"
+    id("io.gitlab.arturbosch.detekt") version "1.19.0"
 }
