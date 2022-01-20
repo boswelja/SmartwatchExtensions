86faad4925b82bba75ed66e507130d809b68611c
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-01-20 23:07:36 +1300

    Remove unused import

--- batterysync/common/src/androidMain/kotlin/com/boswelja/smartwatchextensions/batterysync/BatterySyncNotificationHandler.kt
+++ batterysync/common/src/androidMain/kotlin/com/boswelja/smartwatchextensions/batterysync/BatterySyncNotificationHandler.kt
@@ -4,7 +4,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.boswelja.smartwatchextensions.batterysync.common.R
 
