08f749c6032a7be2e233ea8e9c959f77896664b2
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-01-22 17:41:54 +1300

    Optimize some logic inside PhoneLockingAccessibilityService.kt

--- phonelocking/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/phonelocking/PhoneLockingAccessibilityService.kt
+++ phonelocking/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/phonelocking/PhoneLockingAccessibilityService.kt
@@ -2,16 +2,15 @@ package com.boswelja.smartwatchextensions.phonelocking
 
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
-import com.boswelja.smartwatchextensions.devicemanagement.WatchRepository
 import com.boswelja.smartwatchextensions.settings.BoolSettingKeys.PHONE_LOCKING_ENABLED_KEY
 import com.boswelja.smartwatchextensions.settings.WatchSettingsRepository
 import com.boswelja.watchconnection.core.message.MessageClient
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.koin.android.ext.android.inject
@@ -24,7 +23,6 @@ class PhoneLockingAccessibilityService : AccessibilityService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
     private val messageClient: MessageClient by inject()
-    private val watchRepository: WatchRepository by inject()
     private val settingsRepository: WatchSettingsRepository by inject()
 
     override fun onServiceConnected() {
@@ -43,21 +41,18 @@ class PhoneLockingAccessibilityService : AccessibilityService() {
         // This is irrelevant for phone locking
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+    }
+
     private fun tryLockDevice(watchId: String) {
         coroutineScope.launch(Dispatchers.IO) {
-            // Ensure the calling watch is registered
-            val watch = watchRepository.getWatchById(watchId).firstOrNull()
-            if (watch != null) {
-                val phoneLockingEnabledForWatch = settingsRepository.getBoolean(
-                    watch.uid, PHONE_LOCKING_ENABLED_KEY
-                ).first()
-                if (phoneLockingEnabledForWatch) {
-                    withContext(Dispatchers.Main) {
-                        performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)
-                    }
+            val phoneLockingEnabledForWatch = settingsRepository.getBoolean(watchId, PHONE_LOCKING_ENABLED_KEY).first()
+            if (phoneLockingEnabledForWatch) {
+                withContext(Dispatchers.Main) {
+                    performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)
                 }
-            } else {
-                // TODO tell the watch it isn't registered
             }
         }
     }
