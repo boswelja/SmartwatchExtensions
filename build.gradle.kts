e9e0d1320f6f13852a431596bda40ef3c3cec9e1
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-02-07 01:55:36 +1300

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.2.0-beta01" apply false
-    id("com.android.library") version "7.2.0-beta01" apply false
+    id("com.android.application") version "7.3.0-alpha01" apply false
+    id("com.android.library") version "7.3.0-alpha01" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("com.squareup.sqldelight") version libs.versions.sqldelight.get() apply false
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
