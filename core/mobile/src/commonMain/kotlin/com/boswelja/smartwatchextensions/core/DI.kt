326b4969d8ba747905a7fafd88b8ae01566d0ec2
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-02-07 16:47:18 +1300

    Clone settings/mobile tests to core/mobile

--- core/mobile/src/commonMain/kotlin/com/boswelja/smartwatchextensions/core/DI.kt
+++ core/mobile/src/commonMain/kotlin/com/boswelja/smartwatchextensions/core/DI.kt
@@ -1,14 +1,16 @@
-package com.boswelja.smartwatchextensions.core.settings
+package com.boswelja.smartwatchextensions.core
 
+import com.boswelja.smartwatchextensions.core.settings.WatchSettingsDbRepository
+import com.boswelja.smartwatchextensions.core.settings.WatchSettingsRepository
 import com.boswelja.smartwatchextensions.settings.database.WatchSettingsDatabase
 import org.koin.core.parameter.parametersOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 /**
- * A Koin module for providing repositories.
+ * A Koin module for providing core dependencies.
  */
-val settingsModule = module {
+val coreModule = module {
     single<WatchSettingsRepository> { WatchSettingsDbRepository(get(), get(named("database"))) }
     single {
         WatchSettingsDatabase(
