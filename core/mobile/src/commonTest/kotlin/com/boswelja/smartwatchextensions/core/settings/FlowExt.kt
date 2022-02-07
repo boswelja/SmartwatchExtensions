d933188d6ee09e9f773f9c3d1d71a26b0168ca01
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-02-07 22:50:58 +1300

    Workaround for issues with Turbine take 2

--- core/mobile/src/commonTest/kotlin/com/boswelja/smartwatchextensions/core/settings/FlowExt.kt
+++ core/mobile/src/commonTest/kotlin/com/boswelja/smartwatchextensions/core/settings/FlowExt.kt
@@ -2,12 +2,10 @@ package com.boswelja.smartwatchextensions.core.settings
 
 import app.cash.turbine.FlowTurbine
 import app.cash.turbine.test
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope.coroutineContext
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.test.TestCoroutineScheduler
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.withContext
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
@@ -20,11 +18,7 @@ suspend fun <T> Flow<T>.test2(
     timeout: Duration = 1.seconds,
     validate: suspend FlowTurbine<T>.() -> Unit,
 ) {
-    val testScheduler = coroutineContext[TestCoroutineScheduler]
-    return if (testScheduler == null) {
+    withContext(Dispatchers.Default) {
         test(timeout, validate)
-    } else {
-        flowOn(UnconfinedTestDispatcher(testScheduler))
-            .test(timeout, validate)
     }
 }
