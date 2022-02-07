ba99d3611972d59e8854bc87c023c4ce6f1bc35e
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-02-07 16:29:13 +1300

    Clone some common devicemanagement code to core/common

---
+++ core/common/src/commonMain/kotlin/com/boswelja/smartwatchextensions/core/devicemanagement/VersionSerializer.kt
@@ -0,0 +1,17 @@
+package com.boswelja.smartwatchextensions.core.devicemanagement
+
+import com.boswelja.watchconnection.serialization.MessageSerializer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+
+/**
+ * A [MessageSerializer] for handling [Version].
+ */
+@OptIn(ExperimentalSerializationApi::class)
+object VersionSerializer : MessageSerializer<Version> {
+    override val messagePaths: Set<String> = setOf(REQUEST_APP_VERSION)
+    override suspend fun deserialize(bytes: ByteArray?): Version = ProtoBuf.decodeFromByteArray(bytes!!)
+    override suspend fun serialize(data: Version): ByteArray = ProtoBuf.encodeToByteArray(data)
+}
