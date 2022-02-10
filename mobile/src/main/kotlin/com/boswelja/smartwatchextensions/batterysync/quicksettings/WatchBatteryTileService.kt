61c4e3d1fe025604d77a084ad6860f942f5ee65a
Jack Boswell <boswelja@outlook.com>
2022-02-10 21:16:31 +1300

    Fix a bug in WatchTileService.kt causing uninitialized property exceptions (#262)

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/batterysync/quicksettings/WatchBatteryTileService.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/batterysync/quicksettings/WatchBatteryTileService.kt
@@ -14,8 +14,12 @@ import com.boswelja.smartwatchextensions.common.WatchTileService
 import com.boswelja.smartwatchextensions.core.settings.WatchSettingsRepository
 import com.boswelja.smartwatchextensions.main.ui.MainActivity
 import com.boswelja.smartwatchextensions.main.ui.MainActivity.Companion.EXTRA_WATCH_ID
-import com.boswelja.watchconnection.common.Watch
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import org.koin.android.ext.android.inject
 
 /**
@@ -26,10 +30,22 @@ class WatchBatteryTileService : WatchTileService() {
     private val settingsRepository: WatchSettingsRepository by inject()
     private val batteryStatsRepository: BatteryStatsRepository by inject()
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val batteryStats = watch
+        .filterNotNull()
+        .flatMapLatest {
+            batteryStatsRepository.batteryStatsFor(it.uid)
+        }
+        .stateIn(
+            coroutineScope,
+            SharingStarted.Eagerly,
+            null
+        )
+
     override fun onClick() {
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            putExtra(EXTRA_WATCH_ID, watchId)
+            putExtra(EXTRA_WATCH_ID, watch.value?.uid)
         }
         if (isLocked) {
             unlockAndRun {
@@ -40,64 +56,64 @@ class WatchBatteryTileService : WatchTileService() {
         }
     }
 
-    override suspend fun onTileUpdateRequest(watch: Watch?) {
-        if (watch == null) {
-            // Couldn't get watch, set an error
-            updateTile {
-                label = getString(R.string.widget_watch_battery_title)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    subtitle = getString(R.string.watch_status_error)
-                }
-                state = Tile.STATE_UNAVAILABLE
-                icon = Icon.createWithResource(
-                    this@WatchBatteryTileService,
-                    com.boswelja.smartwatchextensions.batterysync.common.R.drawable.battery_unknown
-                )
-            }
-            return
-        }
-
-        val isBatterySyncEnabled = settingsRepository
-            .getBoolean(watch.uid, BATTERY_SYNC_ENABLED_KEY, false).first()
-
-        if (isBatterySyncEnabled) {
-            // TODO Handle null cases
-            val batteryStats = batteryStatsRepository
-                .batteryStatsFor(watch.uid)
-                .first()!!
-
-            updateTile {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    label = getString(
-                        com.boswelja.smartwatchextensions.batterysync.R.string.battery_percent,
-                        batteryStats.percent.toString()
-                    )
-                    subtitle = watch.name
-                } else {
-                    label = getString(
-                        R.string.battery_percent_qs_tile_fallback,
-                        batteryStats.percent.toString(),
-                        watch.name
-                    )
-                }
-                state = Tile.STATE_ACTIVE
-                icon = Icon.createWithResource(
-                    this@WatchBatteryTileService, getBatteryDrawableRes(batteryStats.percent)
-                )
-            }
-        } else {
-            updateTile {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    override suspend fun onTileUpdateRequest() {
+        batteryStats.collect { batteryStats ->
+            val watch = watch.value
+            if (watch == null) {
+                // Couldn't get watch, set an error
+                updateTile {
                     label = getString(R.string.widget_watch_battery_title)
-                    subtitle = getString(com.boswelja.smartwatchextensions.batterysync.R.string.battery_sync_disabled)
-                } else {
-                    label = getString(com.boswelja.smartwatchextensions.batterysync.R.string.battery_sync_disabled)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        subtitle = getString(R.string.watch_status_error)
+                    }
+                    state = Tile.STATE_UNAVAILABLE
+                    icon = Icon.createWithResource(
+                        this@WatchBatteryTileService,
+                        com.boswelja.smartwatchextensions.batterysync.common.R.drawable.battery_unknown
+                    )
                 }
-                state = Tile.STATE_INACTIVE
-                icon = Icon.createWithResource(
-                    this@WatchBatteryTileService,
-                    com.boswelja.smartwatchextensions.batterysync.common.R.drawable.battery_unknown
-                )
+                return@collect
+            }
+
+            val isBatterySyncEnabled = settingsRepository
+                .getBoolean(watch.uid, BATTERY_SYNC_ENABLED_KEY, false).first()
+
+            if (isBatterySyncEnabled) {
+                updateTile {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        label = getString(
+                            com.boswelja.smartwatchextensions.batterysync.R.string.battery_percent,
+                            batteryStats!!.percent.toString()
+                        )
+                        subtitle = watch.name
+                    } else {
+                        label = getString(
+                            R.string.battery_percent_qs_tile_fallback,
+                            batteryStats!!.percent.toString(),
+                            watch.name
+                        )
+                    }
+                    state = Tile.STATE_ACTIVE
+                    icon = Icon.createWithResource(
+                        this@WatchBatteryTileService, getBatteryDrawableRes(batteryStats.percent)
+                    )
+                }
+            } else {
+                updateTile {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        label = getString(R.string.widget_watch_battery_title)
+                        subtitle = getString(
+                            com.boswelja.smartwatchextensions.batterysync.R.string.battery_sync_disabled
+                        )
+                    } else {
+                        label = getString(com.boswelja.smartwatchextensions.batterysync.R.string.battery_sync_disabled)
+                    }
+                    state = Tile.STATE_INACTIVE
+                    icon = Icon.createWithResource(
+                        this@WatchBatteryTileService,
+                        com.boswelja.smartwatchextensions.batterysync.common.R.drawable.battery_unknown
+                    )
+                }
             }
         }
     }
