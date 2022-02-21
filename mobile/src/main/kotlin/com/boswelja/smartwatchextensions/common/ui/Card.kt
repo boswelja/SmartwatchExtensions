86594b423e6859a9d32f6ad4633edd5b23c6b06c
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-02-21 23:22:46 +1300

    Use Material3 for the common card components

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/common/ui/Card.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/common/ui/Card.kt
@@ -1,23 +1,18 @@
 package com.boswelja.smartwatchextensions.common.ui
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProvideTextStyle
-import androidx.compose.material.contentColorFor
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 /**
@@ -45,11 +40,11 @@ fun CardHeader(
             Spacer(Modifier.width(16.dp))
         }
         Column {
-            ProvideTextStyle(MaterialTheme.typography.subtitle1) {
+            ProvideTextStyle(MaterialTheme.typography.labelLarge) {
                 title()
             }
             if (subtitle != null) {
-                ProvideTextStyle(MaterialTheme.typography.subtitle2) {
+                ProvideTextStyle(MaterialTheme.typography.labelMedium) {
                     subtitle()
                 }
             }
@@ -69,33 +64,19 @@ fun CardHeader(
  * A card with a slot for a header.
  * @param modifier [Modifier].
  * @param header The card header Composable.
- * @param shape The card shape.
- * @param backgroundColor The card background color.
- * @param contentColor The card content color.
- * @param border The card border stroke.
- * @param elevation The card elevation.
  * @param dividerVisible Whether the header divider is visible.
  * @param content The card content.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Card(
     modifier: Modifier = Modifier,
     header: @Composable () -> Unit,
-    shape: Shape = MaterialTheme.shapes.medium,
-    backgroundColor: Color = MaterialTheme.colors.surface,
-    contentColor: Color = contentColorFor(backgroundColor),
-    border: BorderStroke? = null,
-    elevation: Dp = 1.dp,
     dividerVisible: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    Card(
-        modifier = modifier,
-        shape = shape,
-        backgroundColor = backgroundColor,
-        contentColor = contentColor,
-        border = border,
-        elevation = elevation
+    androidx.compose.material3.Card(
+        modifier = modifier
     ) {
         Column {
             header()
