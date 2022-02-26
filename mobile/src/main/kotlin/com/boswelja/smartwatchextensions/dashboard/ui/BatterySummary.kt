c718c8f49aa710218c1022ef6e1c434432446000
Jack Boswell <Jack.Boswell@ceridian.com>
2022-02-26 17:57:33 +1300

    Move BatteryIcon into dashboard

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/BatterySummary.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/dashboard/ui/BatterySummary.kt
@@ -2,14 +2,16 @@ package com.boswelja.smartwatchextensions.dashboard.ui
 
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.boswelja.smartwatchextensions.batterysync.BatteryStats
 import com.boswelja.smartwatchextensions.batterysync.R
-import com.boswelja.smartwatchextensions.batterysync.ui.BatteryIcon
+import com.boswelja.smartwatchextensions.batterysync.getBatteryDrawableRes
 
 private const val ICON_WIDTH_PERCENT = 0.33f
 
@@ -42,5 +44,23 @@ fun BatterySummarySmall(
                 style = MaterialTheme.typography.titleLarge
             )
         }
+    )
+}
+
+/**
+ * Displays a battery icon for the given percentage.
+ * @param percent The battery percent to display an icon for.
+ * @param modifier [Modifier].
+ */
+@Composable
+fun BatteryIcon(
+    percent: Int,
+    modifier: Modifier = Modifier
+) {
+    val drawable = getBatteryDrawableRes(percent)
+    Icon(
+        painterResource(drawable),
+        null,
+        modifier
     )
 }
