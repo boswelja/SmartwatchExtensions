6d738672e9f7bcdc6b07b7b7c758d97b68231ea1
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-02-26 15:35:31 +1300

    Remove divider from BatterySyncSettingsScreen.kt

--- batterysync/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/batterysync/ui/BatterySyncSettingsScreen.kt
+++ batterysync/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/batterysync/ui/BatterySyncSettingsScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -52,7 +51,6 @@ fun BatterySyncSettingsScreen(
                 .fillMaxWidth()
                 .padding(16.dp)
         )
-        Divider()
         if (canSyncBattery) {
             BatterySyncSettings(
                 viewModel = viewModel,
