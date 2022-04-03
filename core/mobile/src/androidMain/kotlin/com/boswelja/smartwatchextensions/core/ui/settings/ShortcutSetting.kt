d0fcb609969ee0505b6006aac13ca892bd43ef70
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-04-04 00:58:20 +1200

    Use ListItem for ShortcutSetting.kt

--- core/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/core/ui/settings/ShortcutSetting.kt
+++ core/mobile/src/androidMain/kotlin/com/boswelja/smartwatchextensions/core/ui/settings/ShortcutSetting.kt
@@ -1,14 +1,9 @@
 package com.boswelja.smartwatchextensions.core.ui.settings
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import com.boswelja.smartwatchextensions.core.ui.theme.DisabledAlpha
+import com.boswelja.smartwatchextensions.core.ui.list.ListItem
 
 /**
  * Shortcut setting is used to display an item on a settings screen that links into a deeper settings menu.
@@ -26,30 +21,9 @@ fun ShortcutSetting(
     enabled: Boolean = true,
     summary: (@Composable () -> Unit)? = null
 ) {
-    val textColor = if (enabled) {
-        LocalContentColor.current
-    } else {
-        LocalContentColor.current.copy(alpha = DisabledAlpha)
-    }
-    Column(
-        modifier = Modifier
-            .clickable(enabled = enabled, onClick = onClick)
-            .padding(SettingDefaults.Padding)
-            .then(modifier)
-    ) {
-        CompositionLocalProvider(
-            LocalTextStyle provides SettingDefaults.TitleTextStyle,
-            LocalContentColor provides textColor
-        ) {
-            text()
-        }
-        if (summary != null) {
-            CompositionLocalProvider(
-                LocalTextStyle provides SettingDefaults.SummaryTextStyle,
-                LocalContentColor provides textColor
-            ) {
-                summary()
-            }
-        }
-    }
+    ListItem(
+        text = text,
+        secondaryText = summary,
+        modifier = Modifier.clickable(enabled = enabled, onClick = onClick).then(modifier)
+    )
 }
