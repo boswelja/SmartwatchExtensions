c7747e583923fe937801dfc184e658fe3fa837e2
Jack Boswell <boswelja@outlook.com>
2022-09-19 22:21:00 +1200

    Remove swipe-collapse top app bar (#294)

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/main/ui/MainScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/main/ui/MainScreen.kt
@@ -9,13 +9,10 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults.exitUntilCollapsedScrollBehavior
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -77,12 +74,9 @@ fun MainScreen() {
     val navController = rememberNavController()
     val backStackEntry by navController.currentBackStackEntryAsState()
     val snackbarHostState = remember { SnackbarHostState() }
-    val topBarScrollState = rememberTopAppBarState()
-    val scrollBehavior = exitUntilCollapsedScrollBehavior(state = topBarScrollState)
 
     HarmonizedTheme {
         Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
             topBar = {
                 val showUpButton = backStackEntry?.destination?.route != "dashboard"
@@ -96,8 +90,7 @@ fun MainScreen() {
                         navController.navigate(it) {
                             launchSingleTop = true
                         }
-                    },
-                    scrollBehavior = scrollBehavior
+                    }
                 )
             }
         ) {
