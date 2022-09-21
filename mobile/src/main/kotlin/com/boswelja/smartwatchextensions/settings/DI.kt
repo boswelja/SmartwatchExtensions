6b12019e869d41d8378e9eca657b2b669e8d0f06
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-09-21 21:21:35 +1200

    Remove unused ViewModel

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/settings/DI.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/settings/DI.kt
@@ -2,7 +2,6 @@ package com.boswelja.smartwatchextensions.settings
 
 import android.content.Context
 import com.boswelja.smartwatchextensions.core.settings.appSettingsStore
-import com.boswelja.smartwatchextensions.settings.ui.AboutAppViewModel
 import com.boswelja.smartwatchextensions.settings.ui.AppSettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -12,5 +11,4 @@ import org.koin.dsl.module
  */
 val appSettingsModule = module {
     viewModel { AppSettingsViewModel(get(), get(), get<Context>().appSettingsStore) }
-    viewModel { AboutAppViewModel(get(), get()) }
 }
