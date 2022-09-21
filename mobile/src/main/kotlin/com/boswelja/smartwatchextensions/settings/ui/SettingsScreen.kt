3e8c6b1c074814b9e15bc7012dc313c45b61924c
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-09-21 22:24:30 +1200

    Initial cleanup of settings screen

--- mobile/src/main/kotlin/com/boswelja/smartwatchextensions/settings/ui/SettingsScreen.kt
+++ mobile/src/main/kotlin/com/boswelja/smartwatchextensions/settings/ui/SettingsScreen.kt
@@ -3,14 +3,11 @@ package com.boswelja.smartwatchextensions.settings.ui
 import android.content.Intent
 import android.provider.Settings
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChangeHistory
 import androidx.compose.material.icons.filled.Code
@@ -19,7 +16,6 @@ import androidx.compose.material.icons.filled.Security
 import androidx.compose.material.icons.filled.Watch
 import androidx.compose.material.icons.outlined.Watch
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,105 +25,62 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.boswelja.smartwatchextensions.BuildConfig
 import com.boswelja.smartwatchextensions.R
 import com.boswelja.smartwatchextensions.common.startActivity
 import com.boswelja.smartwatchextensions.core.ui.list.ListItem
 import com.boswelja.smartwatchextensions.core.ui.settings.DialogSetting
-import com.boswelja.smartwatchextensions.core.ui.settings.SettingsHeader
-import com.boswelja.smartwatchextensions.core.ui.settings.ShortcutSetting
 import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.compose.getViewModel
 
 /**
  * A Composable screen for displaying app settings.
- * @param modifier [Modifier].
- * @param contentPadding The screen padding.
  * @param onNavigateTo Called when navigation is requested.
+ * @param modifier [Modifier].
  */
 @Composable
 fun AppSettingsScreen(
-    modifier: Modifier = Modifier,
-    contentPadding: PaddingValues = PaddingValues(),
-    onNavigateTo: (SettingsDestination) -> Unit
-) {
-    LazyColumn(
-        modifier = modifier,
-        contentPadding = contentPadding
-    ) {
-        item {
-            AboutAppHeader(
-                modifier = Modifier
-                    .padding(horizontal = 24.dp, vertical = 16.dp)
-                    .fillMaxWidth()
-            )
-        }
-        item {
-            QSTileSettings()
-        }
-        item {
-            WatchSettings(onNavigateTo = onNavigateTo)
-        }
-        item {
-            AboutAppListShortcuts(onNavigateTo = onNavigateTo)
-        }
-    }
-}
-
-/**
- * A Composable for displaying QS Tile settings.
- * @param modifier [Modifier].
- */
-@Composable
-fun QSTileSettings(
+    onNavigateTo: (SettingsDestination) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val viewModel: AppSettingsViewModel = getViewModel()
     val registeredWatches by viewModel.registeredWatches.collectAsState(emptyList(), Dispatchers.IO)
     val qsTilesWatch by viewModel.qsTilesWatch.collectAsState(null, Dispatchers.IO)
 
-    Column(modifier) {
-        SettingsHeader(
-            text = {
-                Text(stringResource(R.string.category_qstiles))
-            }
-        )
-        DialogSetting(
-            icon = { Icon(Icons.Outlined.Watch, null) },
-            label = { Text(stringResource(R.string.qstiles_selected_watch)) },
-            summary = { Text(qsTilesWatch?.name ?: "") },
-            values = registeredWatches,
-            value = qsTilesWatch,
-            onValueChanged = {
-                viewModel.setQSTilesWatch(it!!)
-            },
-            valueLabel = {
-                Text(it?.name ?: stringResource(R.string.watch_status_error))
-            }
-        )
-    }
-}
-
-/**
- * A Composable for displaying watch settings.
- * @param modifier [Modifier].
- * @param onNavigateTo Called when navigation is requested.
- */
-@Composable
-fun WatchSettings(
-    modifier: Modifier = Modifier,
-    onNavigateTo: (SettingsDestination) -> Unit
-) {
-    Column(modifier) {
-        SettingsHeader(
-            text = {
-                Text(stringResource(R.string.category_watch_settings))
-            }
-        )
-        ShortcutSetting(
-            text = { Text(stringResource(R.string.manage_watches_title)) },
-            onClick = { onNavigateTo(SettingsDestination.WATCH_MANAGER) },
-            modifier = Modifier.fillMaxWidth()
+    val itemModifier = Modifier.fillMaxWidth()
+    LazyColumn(modifier = modifier) {
+        item {
+            AboutAppHeader(
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        item {
+            DialogSetting(
+                icon = { Icon(Icons.Outlined.Watch, null) },
+                label = { Text(stringResource(R.string.qstiles_selected_watch)) },
+                summary = { Text(qsTilesWatch?.name ?: "") },
+                values = registeredWatches,
+                value = qsTilesWatch,
+                onValueChanged = {
+                    viewModel.setQSTilesWatch(it!!)
+                },
+                valueLabel = {
+                    Text(it?.name ?: stringResource(R.string.watch_status_error))
+                },
+                modifier = itemModifier
+            )
+        }
+        item {
+            ListItem(
+                text = { Text(stringResource(R.string.manage_watches_title)) },
+                modifier = Modifier
+                    .clickable {
+                        onNavigateTo(SettingsDestination.WATCH_MANAGER)
+                    }.then(itemModifier)
+            )
+        }
+        aboutAppListShortcuts(
+            onNavigateTo = onNavigateTo,
+            modifier = itemModifier
         )
     }
 }
@@ -139,23 +92,14 @@ fun WatchSettings(
 fun AboutAppHeader(
     modifier: Modifier = Modifier
 ) {
-    Column(
+    Box(
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        contentAlignment = Alignment.TopCenter
     ) {
         Icon(
             imageVector = Icons.Default.Watch,
             contentDescription = null,
             modifier = Modifier.size(64.dp)
-        )
-        Spacer(Modifier.height(16.dp))
-        Text(
-            text = stringResource(R.string.app_name),
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Text(
-            text = BuildConfig.VERSION_NAME,
-            style = MaterialTheme.typography.titleMedium
         )
     }
 }
@@ -163,28 +107,12 @@ fun AboutAppHeader(
 /**
  * Displays a list of shortcuts to app-related information
  */
-@Composable
-fun AboutAppListShortcuts(
+fun LazyListScope.aboutAppListShortcuts(
     onNavigateTo: (SettingsDestination) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val context = LocalContext.current
-    Column(modifier) {
-        ListItem(
-            icon = { Icon(Icons.Default.Code, null) },
-            text = { Text(stringResource(R.string.about_github)) },
-            modifier = Modifier.clickable { onNavigateTo(SettingsDestination.SOURCE) }
-        )
-        ListItem(
-            icon = { Icon(Icons.Default.ChangeHistory, null) },
-            text = { Text(stringResource(R.string.about_changelog)) },
-            modifier = Modifier.clickable { onNavigateTo(SettingsDestination.CHANGELOG) }
-        )
-        ListItem(
-            icon = { Icon(Icons.Default.Security, null) },
-            text = { Text(stringResource(R.string.about_priv_policy_title)) },
-            modifier = Modifier.clickable { onNavigateTo(SettingsDestination.PRIVACY_POLICY) }
-        )
+    item {
+        val context = LocalContext.current
         ListItem(
             icon = { Icon(Icons.Default.Notifications, null) },
             text = { Text(stringResource(R.string.noti_settings_title)) },
@@ -194,7 +122,28 @@ fun AboutAppListShortcuts(
                     it.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
                     it.putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName!!)
                 }
-            }
+            }.then(modifier)
+        )
+    }
+    item {
+        ListItem(
+            icon = { Icon(Icons.Default.Code, null) },
+            text = { Text(stringResource(R.string.about_github)) },
+            modifier = Modifier.clickable { onNavigateTo(SettingsDestination.SOURCE) }.then(modifier)
+        )
+    }
+    item {
+        ListItem(
+            icon = { Icon(Icons.Default.ChangeHistory, null) },
+            text = { Text(stringResource(R.string.about_changelog)) },
+            modifier = Modifier.clickable { onNavigateTo(SettingsDestination.CHANGELOG) }.then(modifier)
+        )
+    }
+    item {
+        ListItem(
+            icon = { Icon(Icons.Default.Security, null) },
+            text = { Text(stringResource(R.string.about_priv_policy_title)) },
+            modifier = Modifier.clickable { onNavigateTo(SettingsDestination.PRIVACY_POLICY) }.then(modifier)
         )
     }
 }
