6c13dc994125a1d9c3cf19bf24be8b7661a74980
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-09-26 14:21:24 +1300

    Fix unresolved import

--- appmanager/common/src/main/kotlin/com/boswelja/smartwatchextensions/appmanager/CacheValidationSerializer.kt
+++ appmanager/common/src/main/kotlin/com/boswelja/smartwatchextensions/appmanager/CacheValidationSerializer.kt
@@ -1,13 +1,12 @@
 package com.boswelja.smartwatchextensions.appmanager
 
-import com.boswelja.watchconnection.serialization.MessageSerializer
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import kotlinx.serialization.protobuf.ProtoBuf
 
 /**
- * A [MessageSerializer] to handle serialization for a cache hash.
+ * A serializer for handling serialization for a cache hash.
  */
 @OptIn(ExperimentalSerializationApi::class)
 object CacheValidationSerializer {
