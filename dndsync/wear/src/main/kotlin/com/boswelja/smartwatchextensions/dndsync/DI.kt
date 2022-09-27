1cb3747f9a5cc1f383cdf394572a6770176254c7
Jack Boswell <boswelja@outlook.com>
2022-09-27 21:22:22 +1300

    (mostly) migrate to the new Koin DSL (#305)
    There are some places where it does not make sense though

--- dndsync/wear/src/main/kotlin/com/boswelja/smartwatchextensions/dndsync/DI.kt
+++ dndsync/wear/src/main/kotlin/com/boswelja/smartwatchextensions/dndsync/DI.kt
@@ -1,11 +1,12 @@
 package com.boswelja.smartwatchextensions.dndsync
 
-import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 /**
  * A Koin module for providing DnD Sync classes.
  */
 val dndSyncModule = module {
-    single<DnDSyncStateRepository> { DnDSyncStateDsRepository(androidContext()) }
+    singleOf(::DnDSyncStateDsRepository) bind DnDSyncStateRepository::class
 }
