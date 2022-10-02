eddc37b4213140b840cd44da2b5a0a27a9404f3b
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-10-02 21:20:00 +1300

    Create RegisterWatchViewModel

--- watchmanager/mobile/src/main/kotlin/com/boswelja/smartwatchextensions/watchmanager/di/DI.kt
+++ watchmanager/mobile/src/main/kotlin/com/boswelja/smartwatchextensions/watchmanager/di/DI.kt
@@ -2,6 +2,7 @@ package com.boswelja.smartwatchextensions.watchmanager.di
 
 import com.boswelja.smartwatchextensions.watchmanager.data.WatchVersionRepositoryImpl
 import com.boswelja.smartwatchextensions.watchmanager.domain.WatchVersionRepository
+import com.boswelja.smartwatchextensions.watchmanager.ui.RegisterWatchViewModel
 import com.boswelja.smartwatchextensions.watchmanager.ui.WatchManagerViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
@@ -12,4 +13,5 @@ val watchManagerModule = module {
     singleOf(::WatchVersionRepositoryImpl) bind WatchVersionRepository::class
 
     viewModelOf(::WatchManagerViewModel)
+    viewModelOf(::RegisterWatchViewModel)
 }
