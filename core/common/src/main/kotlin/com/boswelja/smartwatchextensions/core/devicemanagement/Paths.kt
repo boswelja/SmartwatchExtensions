e0bbc6ae6f13b7434813199e2ab09d9965d42e15
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-10-14 22:56:57 +1300

    Remove unused feature

--- core/common/src/main/kotlin/com/boswelja/smartwatchextensions/core/devicemanagement/Paths.kt
+++ core/common/src/main/kotlin/com/boswelja/smartwatchextensions/core/devicemanagement/Paths.kt
@@ -9,13 +9,3 @@ const val RequestLaunchApp = "/launch_app"
  * Request the target device update it's capabilities.
  */
 const val RequestUpdateCapabilities = "/update_capabilities"
-
-/**
- * Request the source device registration status with the target device.
- */
-const val CheckWatchRegistered = "/check_watch_registered"
-
-/**
- * Notify the target device it is registered with the source device.
- */
-const val ConfirmWatchRegistered = "/watch_registered"
