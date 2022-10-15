48625b52cc5efee05e60b736e1ead055b7a082e5
Jack Boswell <boswelja@outlook.com>
2022-10-15 02:59:28 -0700

    refactor: Update batterysync/wear to not use watchconnectionmanager (#314)
    * Update batterysync/wear to not use watchconnectionmanager
    
    * Fix build error

--- batterysync/wear/src/main/kotlin/com/boswelja/smartwatchextensions/batterysync/domain/usecase/RequestBatteryStatsUpdate.kt
+++ batterysync/wear/src/main/kotlin/com/boswelja/smartwatchextensions/batterysync/domain/usecase/RequestBatteryStatsUpdate.kt
@@ -1,22 +1,31 @@
 package com.boswelja.smartwatchextensions.batterysync.domain.usecase
 
+import android.content.Context
+import androidx.datastore.core.DataStore
 import com.boswelja.smartwatchextensions.batterysync.RequestBatteryStatus
-import com.boswelja.watchconnection.common.message.Message
-import com.boswelja.watchconnection.wear.discovery.DiscoveryClient
-import com.boswelja.watchconnection.wear.message.MessageClient
+import com.boswelja.smartwatchextensions.core.devicemanagement.PhoneState
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 class RequestBatteryStatsUpdate(
-    private val messageClient: MessageClient,
-    private val discoveryClient: DiscoveryClient
+    context: Context,
+    private val phoneStateStore: DataStore<PhoneState>
 ) {
+    private val messageClient = Wearable.getMessageClient(context)
+
     suspend operator fun invoke(): Boolean {
-        val phoneId = discoveryClient.pairedPhone()!!.uid
-        return messageClient.sendMessage(
-            phoneId,
-            Message(
+        val phoneId = phoneStateStore.data.map { it.id }.first()
+        return try {
+            messageClient.sendMessage(
+                phoneId,
                 RequestBatteryStatus,
                 null
             )
-        )
+            true
+        } catch (_: ApiException) {
+            false
+        }
     }
 }
