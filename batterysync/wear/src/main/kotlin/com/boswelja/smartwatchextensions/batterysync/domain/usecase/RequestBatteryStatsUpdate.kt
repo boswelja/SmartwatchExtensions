20d73abafeabedfc1cf30e186d40b1ab0f4e28e5
Jack Boswell <boswelja@outlook.com>
2022-10-15 17:09:36 -0700

    Fix crash when starting battery sync (#317)

--- batterysync/wear/src/main/kotlin/com/boswelja/smartwatchextensions/batterysync/domain/usecase/RequestBatteryStatsUpdate.kt
+++ batterysync/wear/src/main/kotlin/com/boswelja/smartwatchextensions/batterysync/domain/usecase/RequestBatteryStatsUpdate.kt
@@ -1,9 +1,8 @@
 package com.boswelja.smartwatchextensions.batterysync.domain.usecase
 
 import android.content.Context
-import androidx.datastore.core.DataStore
 import com.boswelja.smartwatchextensions.batterysync.RequestBatteryStatus
-import com.boswelja.smartwatchextensions.core.devicemanagement.PhoneState
+import com.boswelja.smartwatchextensions.core.devicemanagement.phoneStateStore
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.wearable.Wearable
 import kotlinx.coroutines.flow.first
@@ -11,9 +10,9 @@ import kotlinx.coroutines.flow.map
 
 class RequestBatteryStatsUpdate(
     context: Context,
-    private val phoneStateStore: DataStore<PhoneState>
 ) {
     private val messageClient = Wearable.getMessageClient(context)
+    private val phoneStateStore = context.phoneStateStore
 
     suspend operator fun invoke(): Boolean {
         val phoneId = phoneStateStore.data.map { it.id }.first()
