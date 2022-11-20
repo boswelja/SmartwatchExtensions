2dc929c249e10063e7560bb28fd4b1a24aefd43c
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-11-20 17:03:09 +1300

    Drop WatchConnectionLib from WatchStatusRepositoryImpl

--- core/mobile/src/main/kotlin/com/boswelja/smartwatchextensions/core/watches/status/WatchStatusRepositoryImpl.kt
+++ core/mobile/src/main/kotlin/com/boswelja/smartwatchextensions/core/watches/status/WatchStatusRepositoryImpl.kt
@@ -1,25 +1,26 @@
 package com.boswelja.smartwatchextensions.core.watches.status
 
-import com.boswelja.watchconnection.core.discovery.DiscoveryClient
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.google.android.gms.wearable.NodeClient
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.tasks.await
 
 internal class WatchStatusRepositoryImpl(
-    private val discoveryClient: DiscoveryClient
+    private val nodeClient: NodeClient
 ) : WatchStatusRepository {
-    @OptIn(ExperimentalCoroutinesApi::class)
-    override fun getStatusFor(watchId: String): Flow<ConnectionMode> {
-        return discoveryClient.connectionModeFor(watchId)
-            .mapLatest {
-                when (it) {
-                    com.boswelja.watchconnection.common.discovery.ConnectionMode.Disconnected ->
-                        ConnectionMode.Disconnected
-                    com.boswelja.watchconnection.common.discovery.ConnectionMode.Internet ->
-                        ConnectionMode.Internet
-                    com.boswelja.watchconnection.common.discovery.ConnectionMode.Bluetooth ->
-                        ConnectionMode.Bluetooth
-                }
-            }
+    override fun getStatusFor(watchId: String): Flow<ConnectionMode> = flow {
+        while (currentCoroutineContext().isActive) {
+            val connectedNodes = nodeClient.connectedNodes.await()
+            val node = connectedNodes.firstOrNull { it.id == watchId }
+            val connectionMode = node?.let {
+                // If NodeClient considers the node to be nearby, assume a bluetooth connection
+                if (it.isNearby) ConnectionMode.Bluetooth else ConnectionMode.Internet
+            } ?: ConnectionMode.Disconnected
+            emit(connectionMode)
+            delay(2000)
+        }
     }
 }
